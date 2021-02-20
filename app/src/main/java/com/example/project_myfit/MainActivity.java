0d559d2f258a9f6062eda35a80c56f9d9e57c5f0
beebeom <leebeebeom@gmail.com>
2021-02-20 11:22:49 +0900

    메인 액티비티 저장

--- app/src/main/java/com/example/project_myfit/MainActivity.java
+++ app/src/main/java/com/example/project_myfit/MainActivity.java
@@ -8,7 +8,7 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
@@ -16,6 +16,8 @@ import com.example.project_myfit.databinding.ActivityMainBinding;
 import com.example.project_myfit.ui.main.database.Category;
 import com.example.project_myfit.ui.main.listfragment.database.Folder;
 import com.example.project_myfit.ui.main.listfragment.database.Size;
+
+import org.jetbrains.annotations.NotNull;
 
 import static com.example.project_myfit.MyFitConstant.FOLDER_ID;
 import static com.example.project_myfit.MyFitConstant.SIZE_ID;
@@ -28,19 +30,21 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        MainActivityViewModel model = new ViewModelProvider(this).get(MainActivityViewModel.class);
-
         //binding
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        //view model
+        MainActivityViewModel model = new ViewModelProvider(this).get(MainActivityViewModel.class);
 
         //toolbar
         setSupportActionBar(binding.toolbar);
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) actionBar.setDisplayShowTitleEnabled(false);
 
-        //navController
-        mNavController = Navigation.findNavController(this, R.id.host_fragment);
+        //navigation controller
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.host_fragment);
+        if (navHostFragment != null) mNavController = navHostFragment.getNavController();
 
         //top level destination
         mAppBarConfiguration = new AppBarConfiguration.Builder(
@@ -57,12 +61,25 @@ public class MainActivity extends AppCompatActivity {
         binding.bottomNav.setBackgroundTintList(null);
         binding.bottomNav.getMenu().getItem(2).setEnabled(false);
 
+        destinationChangeListener(binding, actionBar);
+
+        if (getIntent() != null) {
+            Intent intent = getIntent();
+            if (intent.getIntExtra(SIZE_ID, 0) != 0) {
+                searchSizeClick(model, intent);
+            } else if (intent.getLongExtra(FOLDER_ID, 0) != 0) {
+                searchFolderClick(model, intent);
+            }
+        }
+    }
+
+    private void destinationChangeListener(ActivityMainBinding binding, ActionBar actionBar) {
         mNavController.addOnDestinationChangedListener((controller, destination, arguments) -> {
-            if (destination.getId() == R.id.inputOutputFragment) {
+            if (destination.getId() == R.id.mainFragment) {
                 binding.activityFab.hide();
-                binding.activityFab.setImageResource(R.drawable.icon_save);
+                binding.activityFab.setImageResource(R.drawable.icon_search);
                 binding.activityFab.show();
-                binding.toolbarMainTitle.setVisibility(View.GONE);
+                binding.toolbarMainTitle.setVisibility(View.VISIBLE);
                 if (actionBar != null) actionBar.setDisplayShowTitleEnabled(false);
             } else if (destination.getId() == R.id.listFragment) {
                 binding.activityFab.hide();
@@ -70,39 +87,47 @@ public class MainActivity extends AppCompatActivity {
                 binding.activityFab.show();
                 binding.toolbarMainTitle.setVisibility(View.GONE);
                 if (actionBar != null) actionBar.setDisplayShowTitleEnabled(true);
-            } else if (destination.getId() == R.id.mainFragment) {
+            } else if (destination.getId() == R.id.inputOutputFragment) {
                 binding.activityFab.hide();
-                binding.activityFab.setImageResource(R.drawable.icon_search);
+                binding.activityFab.setImageResource(R.drawable.icon_save);
                 binding.activityFab.show();
-                binding.toolbarMainTitle.setVisibility(View.VISIBLE);
+                binding.toolbarMainTitle.setVisibility(View.GONE);
                 if (actionBar != null) actionBar.setDisplayShowTitleEnabled(false);
             }
         });
+    }
 
-        if (getIntent() != null) {
-            Intent intent = getIntent();
-            if (intent.getIntExtra(SIZE_ID, 0) != 0) {
-                Size size = model.getRepository().getSize(intent.getIntExtra(SIZE_ID, 0));
-                model.setSize(size);
-                Category category = model.getRepository().getCategory(size.getId());
-                Folder folder = model.getRepository().getFolder(size.getId());
-                if (category != null)
-                    model.setCategory(category);
-                else {
-                    category = model.getRepository().getCategory(folder.getFolderId());
-                    model.setCategory(category);
-                    model.setFolder(folder);
-                }
-                mNavController.navigate(R.id.action_mainFragment_to_inputOutputFragment);
-            } else if (intent.getLongExtra(FOLDER_ID, 0) != 0) {
-                Folder folder = model.getRepository().getFolder(intent.getLongExtra(FOLDER_ID, 0));
-                Category category = model.getRepository().getCategory(folder.getFolderId());
-                model.setFolder(folder);
-                model.setCategory(category);
-                mNavController.navigate(R.id.action_mainFragment_to_listFragment);
-            }
+    private void searchSizeClick(@NotNull MainActivityViewModel model, @NotNull Intent intent) {
+        Size size = model.getRepository().getSize(intent.getIntExtra(SIZE_ID, 0));
+        model.setSize(size);
+
+        Category category = model.getRepository().getCategory(size.getId());
+        Folder folder = model.getRepository().getFolder(size.getId());
+        if (category != null) model.setCategory(category);
+        else {
+            category = getCategory(folder, model);
+            model.setCategory(category);
+            model.setFolder(folder);
         }
+        mNavController.navigate(R.id.action_mainFragment_to_inputOutputFragment);
+    }
 
+    private void searchFolderClick(@NotNull MainActivityViewModel model, @NotNull Intent intent) {
+        Folder folder = model.getRepository().getFolder(intent.getLongExtra(FOLDER_ID, 0));
+        Category category = getCategory(folder, model);
+        model.setFolder(folder);
+        model.setCategory(category);
+        mNavController.navigate(R.id.action_mainFragment_to_listFragment);
+    }
+
+    @NotNull
+    private Category getCategory(@NotNull Folder folder, @NotNull MainActivityViewModel model) {
+        Category category = model.getRepository().getCategory(folder.getFolderId());
+        if (category == null) {
+            Folder newFolder = model.getRepository().getFolder(folder.getFolderId());
+            category = getCategory(newFolder, model);
+        }
+        return category;
     }
 
     @Override
