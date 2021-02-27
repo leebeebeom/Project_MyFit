273a51b10d74107116057a2cfb6ff8b564b1ebb2
beebeom <leebeebeom@gmail.com>
2021-02-27 09:34:45 +0900

    드래그 셀렉트, 액션모드 추가

--- app/src/main/java/com/example/project_myfit/searchActivity/adapter/SearchAdapter.java
+++ app/src/main/java/com/example/project_myfit/searchActivity/adapter/SearchAdapter.java
@@ -1,6 +1,7 @@
 package com.example.project_myfit.searchActivity.adapter;
 
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Filter;
 import android.widget.Filterable;
@@ -13,19 +14,34 @@ import com.example.project_myfit.databinding.ItemSearchFolderBinding;
 import com.example.project_myfit.databinding.ItemSearchSizeBinding;
 import com.example.project_myfit.ui.main.listfragment.database.Folder;
 import com.example.project_myfit.ui.main.listfragment.database.Size;
+import com.google.android.material.checkbox.MaterialCheckBox;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+
+import static com.example.project_myfit.MyFitConstant.ACTION_MODE_ON;
 
 public class SearchAdapter extends ListAdapter<Object, RecyclerView.ViewHolder> implements Filterable {
     private List<Object> mOriginList;
     private final SearchAdapterListener mListener;
+    private int mActionModeState;
+    private final HashSet<Integer> mSelectedPosition;
 
     public SearchAdapter(SearchAdapterListener listener) {
         super(new SearchDiffUtil());
         this.mListener = listener;
+        mSelectedPosition = new HashSet<>();
+        setHasStableIds(true);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        if (getCurrentList().get(position) instanceof Folder)
+            return ((Folder) getCurrentList().get(position)).getId();
+        else return ((Size) getCurrentList().get(position)).getId();
     }
 
     public void setItem(List<Object> list) {
@@ -57,19 +73,67 @@ public class SearchAdapter extends ListAdapter<Object, RecyclerView.ViewHolder> 
         if (holder instanceof SearchRecyclerFolderVH) {
             Folder folder = (Folder) getCurrentList().get(position);
 
-            ((SearchRecyclerFolderVH) holder).mFolderBinding.setFolder(folder);
-            ((SearchRecyclerFolderVH) holder).setFolder(folder);
+            if (mActionModeState != ACTION_MODE_ON) {
+                ((SearchRecyclerFolderVH) holder).mFolderBinding.setFolder(folder);
+                ((SearchRecyclerFolderVH) holder).setFolder(folder);
+            }
+
+            MaterialCheckBox checkBox = ((SearchRecyclerFolderVH) holder).mFolderBinding.searchFolderCheckBox;
+            if (mActionModeState == ACTION_MODE_ON) {
+                checkBox.setVisibility(View.VISIBLE);
+                checkBox.setChecked(mSelectedPosition.contains(holder.getLayoutPosition()));
+            } else {
+                checkBox.setVisibility(View.GONE);
+                checkBox.setChecked(false);
+                if (!mSelectedPosition.isEmpty()) mSelectedPosition.clear();
+            }
         } else {
             Size size = (Size) getCurrentList().get(position);
 
-            ((SearchRecyclerSizeVH) holder).mSizeBinding.setSize(size);
-            ((SearchRecyclerSizeVH) holder).setSize(size);
+            if (mActionModeState != ACTION_MODE_ON) {
+                ((SearchRecyclerSizeVH) holder).mSizeBinding.setSize(size);
+                ((SearchRecyclerSizeVH) holder).setSize(size);
+            }
+
+            MaterialCheckBox checkBox = ((SearchRecyclerSizeVH) holder).mSizeBinding.searchSizeCheckBox;
+            if (mActionModeState == ACTION_MODE_ON) {
+                checkBox.setVisibility(View.VISIBLE);
+                checkBox.setChecked(mSelectedPosition.contains(holder.getLayoutPosition()));
+            } else {
+                checkBox.setVisibility(View.GONE);
+                checkBox.setChecked(false);
+                if (!mSelectedPosition.isEmpty()) mSelectedPosition.clear();
+            }
         }
     }
 
     @Override
     public int getItemCount() {
         return getCurrentList().size();
+    }
+
+    public void setActionModeState(int actionModeState) {
+        this.mActionModeState = actionModeState;
+        notifyDataSetChanged();
+    }
+
+    public void selectAll() {
+        for (int i = 0; i < getCurrentList().size(); i++) mSelectedPosition.add(i);
+        notifyDataSetChanged();
+    }
+
+    public void deselectAll() {
+        mSelectedPosition.clear();
+        notifyDataSetChanged();
+    }
+
+    public HashSet<Integer> getSelectedPosition() {
+        return mSelectedPosition;
+    }
+
+    public void setSelectedPosition(int position) {
+        if (!mSelectedPosition.contains(position)) mSelectedPosition.add(position);
+        else mSelectedPosition.remove(position);
     }
 
     private class SearchAdapterFilter extends Filter {
@@ -114,9 +178,9 @@ public class SearchAdapter extends ListAdapter<Object, RecyclerView.ViewHolder> 
             super(folderBinding.getRoot());
             this.mFolderBinding = folderBinding;
 
-            itemView.setOnClickListener(v -> listener.searchAdapterFolderClick(mFolder));
+            itemView.setOnClickListener(v -> listener.searchAdapterFolderClick(mFolder, mFolderBinding.searchFolderCheckBox, getLayoutPosition()));
             itemView.setOnLongClickListener(v -> {
-                listener.searchAdapterFolderLongClick();
+                listener.searchAdapterFolderLongClick(getLayoutPosition());
                 return false;
             });
         }
@@ -134,9 +198,9 @@ public class SearchAdapter extends ListAdapter<Object, RecyclerView.ViewHolder> 
             super(sizeBinding.getRoot());
             this.mSizeBinding = sizeBinding;
 
-            itemView.setOnClickListener(v -> listener.searchAdapterSizeClick(mSize));
+            itemView.setOnClickListener(v -> listener.searchAdapterSizeClick(mSize, mSizeBinding.searchSizeCheckBox, getLayoutPosition()));
             itemView.setOnLongClickListener(v -> {
-                listener.searchAdapterSizeLongClick();
+                listener.searchAdapterSizeLongClick(getLayoutPosition());
                 return false;
             });
         }
@@ -147,12 +211,12 @@ public class SearchAdapter extends ListAdapter<Object, RecyclerView.ViewHolder> 
     }
 
     public interface SearchAdapterListener {
-        void searchAdapterSizeClick(Size size);
+        void searchAdapterSizeClick(Size size, MaterialCheckBox checkBox, int position);
 
-        void searchAdapterSizeLongClick();
+        void searchAdapterSizeLongClick(int position);
 
-        void searchAdapterFolderClick(Folder folder);
+        void searchAdapterFolderClick(Folder folder, MaterialCheckBox checkBox, int position);
 
-        void searchAdapterFolderLongClick();
+        void searchAdapterFolderLongClick(int position);
     }
 }
