0e627a38d2f5211becb1e0a71fa60c8a3bea0f2d
beebeom <leebeebeom@gmail.com>
2021-02-27 09:34:08 +0900

    드래그 셀렉트 리스터, 액션모드 추가

--- app/src/main/java/com/example/project_myfit/searchActivity/adapter/SearchViewPagerAdapter.java
+++ app/src/main/java/com/example/project_myfit/searchActivity/adapter/SearchViewPagerAdapter.java
@@ -8,6 +8,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.project_myfit.databinding.ItemMainRecyclerViewBinding;
+import com.michaelflisar.dragselectrecyclerview.DragSelectTouchListener;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -15,9 +16,11 @@ import java.util.List;
 
 public class SearchViewPagerAdapter extends RecyclerView.Adapter<SearchViewPagerAdapter.SearchViewPagerVH> {
     private final List<SearchAdapter> mSearchAdapterList;
+    private final DragSelectTouchListener mDragSelectListener;
 
-    public SearchViewPagerAdapter(List<SearchAdapter> searchAdapterList) {
+    public SearchViewPagerAdapter(List<SearchAdapter> searchAdapterList, DragSelectTouchListener dragSelectListener) {
         this.mSearchAdapterList = searchAdapterList;
+        this.mDragSelectListener = dragSelectListener;
     }
 
     @NonNull
@@ -43,11 +46,18 @@ public class SearchViewPagerAdapter extends RecyclerView.Adapter<SearchViewPager
             holder.mBinding.mainRecyclerView.setAdapter(mSearchAdapterList.get(2));
         else if (position == 3)
             holder.mBinding.mainRecyclerView.setAdapter(mSearchAdapterList.get(3));
-
+        holder.mBinding.mainRecyclerView.addOnItemTouchListener(mDragSelectListener);
     }
 
     public List<SearchAdapter> getSearchAdapterList() {
         return mSearchAdapterList;
+    }
+
+    public void setActionModeState(int actionModeState) {
+        mSearchAdapterList.get(0).setActionModeState(actionModeState);
+        mSearchAdapterList.get(1).setActionModeState(actionModeState);
+        mSearchAdapterList.get(2).setActionModeState(actionModeState);
+        mSearchAdapterList.get(3).setActionModeState(actionModeState);
     }
 
     public static class SearchViewPagerVH extends RecyclerView.ViewHolder {
