7a05305e8af481e7946b597d0ef08f4671cf7415
beebeom <leebeebeom@gmail.com>
2021-03-14 02:46:04 +0900

    코드 축소 및 테스트 완료

--- app/src/main/java/com/example/project_myfit/dialog/AddCategoryDialog.java
+++ app/src/main/java/com/example/project_myfit/dialog/AddCategoryDialog.java
@@ -2,7 +2,6 @@ package com.example.project_myfit.dialog;
 
 import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -14,6 +13,10 @@ import com.example.project_myfit.databinding.ItemDialogEditTextBinding;
 
 import org.jetbrains.annotations.NotNull;
 
+//TODO 생명주기 안 코드들 메소드 화
+//TODO 변수 인자로 받기
+//반복 코드들 유틸화
+//트리뷰 롱클릭 삭제, 이름변경
 public class AddCategoryDialog extends DialogFragment {
     private AddCategoryConfirmClick mListener;
 
@@ -27,9 +30,10 @@ public class AddCategoryDialog extends DialogFragment {
     @NotNull
     @Override
     public Dialog onCreateDialog(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
-        ItemDialogEditTextBinding binding = DialogUtils.getCategoryBinding(getLayoutInflater(), requireContext());
-        DialogInterface.OnClickListener listener = (dialog, which) -> mListener.addCategoryConfirmClick(String.valueOf(binding.dialogEditText.getText()));
-        return DialogUtils.getEditTextDialog(requireContext(), getString(R.string.add_category), binding, listener);
+        ItemDialogEditTextBinding binding = DialogUtils.getCategoryBinding(getLayoutInflater(), requireContext(), null);
+
+        return DialogUtils.getEditTextDialog(requireContext(), getString(R.string.add_category), binding,
+                (dialog, which) -> mListener.addCategoryConfirmClick(String.valueOf(binding.dialogEditText.getText())));
     }
 
     public interface AddCategoryConfirmClick {
