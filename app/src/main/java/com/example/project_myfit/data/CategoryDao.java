bed27a759aaafb44c5dea719e8bd92897e4dba71
beebeom <leebeebeom@gmail.com>
2021-04-01 10:02:12 +0900

    코드 정리 및 수정

--- app/src/main/java/com/example/project_myfit/data/CategoryDao.java
+++ app/src/main/java/com/example/project_myfit/data/CategoryDao.java
@@ -1,4 +1,4 @@
-package com.example.project_myfit.ui.main.database;
+package com.example.project_myfit.data;
 
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
@@ -16,14 +16,14 @@ public interface CategoryDao {
     @Query("SELECT * FROM Category WHERE parentCategory = :parentCategory AND isDeleted = :isDeleted ORDER BY orderNumber")
     List<Category> getCategoryListByParent(String parentCategory, int isDeleted);
 
-    @Query("SELECT * FROM Category ORDER BY id DESC limit 1")
-    Category getLatestCategory();
-
     @Query("SELECT max(orderNumber) FROM Category")
     int getCategoryLargestOrder();
 
     @Query("SELECT * FROM Category WHERE id = :id")
     Category getCategory(long id);
+
+    @Query("SELECT * FROM Category ORDER BY id DESC limit 1")
+    Category getLatestCategory();
 
     @Insert
     void categoryInsert(Category category);
