5007d3688a4fc2b0c2212363d8075bb7404fd5d0
beebeom <leebeebeom@gmail.com>
2021-04-01 09:50:53 +0900

    코드 정리 및 수정

--- app/src/main/java/com/example/project_myfit/MyFitConstant.java
+++ app/src/main/java/com/example/project_myfit/MyFitConstant.java
@@ -1,10 +1,13 @@
 package com.example.project_myfit;
 
 public class MyFitConstant {
+    //parent category
     public static final String TOP = "TOP";
     public static final String BOTTOM = "BOTTOM";
     public static final String OUTER = "OUTER";
     public static final String ETC = "ETC";
+
+    //size
     public static final String LENGTH = "length";
     public static final String BOTTOM_LENGTH = "bottom_length";
     public static final String SHOULDER = "shoulder";
@@ -21,16 +24,21 @@ public class MyFitConstant {
     public static final String OPTION5 = "option5";
     public static final String OPTION6 = "option6";
 
+    //view type
     public static final int LISTVIEW = 0;
     public static final int GRIDVIEW = 1;
     public static final String VIEW_TYPE = "view type";
 
+    //action mode
+    public static final String ACTION_MODE = "action mode";
     public static final int ACTION_MODE_ON = 1;
     public static final int ACTION_MODE_OFF = 2;
 
+    //request code
     public static final int GET_IMAGE_REQUEST_CODE = 1000;
     public static final int CROP_REQUEST_CODE = 2000;
 
+    //sort
     public static final String SORT = "sort";
     public static final String SORT_LIST = "sort list";
     public static final String SORT_MAIN = "sort main";
@@ -42,18 +50,7 @@ public class MyFitConstant {
     public static final int SORT_NAME = 5;
     public static final int SORT_NAME_REVERSE = 6;
 
-    public static final String FOLDER_TOGGLE = "folder toggle";
-
-    public static final String ALERT_TITLE = "alertTitle";
-    public static final String ID = "id";
-
-    public static final String CATEGORY_NAME = "category name";
-    public static final String FOLDER_NAME = "folder name";
-
-    public static final String AMOUNT = "amount";
-
-    public static final String TREE_VIEW_STATE = "state";
-
+    //dialog tag
     public static final String DELETE_DIALOG = "delete dialog";
     public static final String TREE_DIALOG = "tree dialog";
     public static final String FOLDER_EDIT_DIALOG = "folder edit dialog";
@@ -62,9 +59,18 @@ public class MyFitConstant {
     public static final String CATEGORY_ADD_DIALOG = "category add dialog";
     public static final String MOVE_DIALOG = "move dialog";
     public static final String SORT_DIALOG = "sort dialog";
-    public static final String TREE_ADD_FOLDER = "tree add folder";
+    public static final String TREE_ADD_FOLDER_DIALOG = "tree add folder";
 
-    public static final String ACTION_MODE = "action mode";
+    //key
+    public static final String FOLDER_TOGGLE = "folder toggle";
+
+    public static final String CATEGORY_NAME = "category name";
+    public static final String FOLDER_NAME = "folder name";
+
+    public static final String SELECTED_ITEM_SIZE = "selected item size";
+
+    public static final String TREE_VIEW_STATE = "state";
+
     public static final String CURRENT_ITEM = "current item";
 
     public static final String SIZE_ID = "size id";
@@ -75,4 +81,8 @@ public class MyFitConstant {
 
     public static final String PARENT_CATEGORY = "parent category";
     public static final String IS_SEARCH_VIEW = "is search view";
+
+    //etc
+    public static final String ALERT_TITLE = "alertTitle";
+    public static final String ID = "id";
 }
