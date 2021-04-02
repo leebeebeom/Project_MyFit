aff117a9e8cd033573fd26c1b3dff2da335da27d
beebeom <leebeebeom@gmail.com>
2021-04-03 05:51:02 +0900

    코드 정리 및 수정

--- app/src/main/java/com/example/project_myfit/dialog/TreeHolderFolder.java
+++ app/src/main/java/com/example/project_myfit/dialog/TreeHolderFolder.java
@@ -1,4 +1,4 @@
-package com.example.project_myfit.ui.main.listfragment.treeview;
+package com.example.project_myfit.dialog;
 
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -7,10 +7,10 @@ import android.view.View;
 import androidx.appcompat.widget.LinearLayoutCompat;
 
 import com.example.project_myfit.R;
+import com.example.project_myfit.data.model.Folder;
+import com.example.project_myfit.data.model.Size;
 import com.example.project_myfit.databinding.ItemTreeFolderBinding;
-import com.example.project_myfit.dialog.TreeViewDialog;
-import com.example.project_myfit.ui.main.listfragment.database.Folder;
-import com.example.project_myfit.ui.main.listfragment.database.Size;
+import com.example.project_myfit.util.AdapterUtils;
 import com.unnamed.b.atv.model.TreeNode;
 
 import org.jetbrains.annotations.NotNull;
@@ -27,26 +27,23 @@ public class TreeHolderFolder extends TreeNode.BaseNodeViewHolder<TreeHolderFold
 
     @Override
     public View createNodeView(TreeNode node, @NotNull FolderTreeHolder value) {
+        //checked
         mBinding = ItemTreeFolderBinding.inflate(LayoutInflater.from(context));
         mBinding.setFolder(value.folder);
 
-        int amount = 0;
-        for (Long l : value.folderFolderIdList)
-            if (l == value.folder.getId()) amount++;
-        for (Long l : value.sizeFolderIdList)
-            if (l == value.folder.getId()) amount++;
-        mBinding.amount.setText(String.valueOf(amount));
+        mBinding.amount.setText(String.valueOf(new AdapterUtils(context)
+                .getFolderContentsSize(value.folder, value.folderFolderIdList, value.sizeFolderIdList)));
 
         LinearLayoutCompat.LayoutParams params = (LinearLayoutCompat.LayoutParams) mBinding.arrowIcon.getLayoutParams();
         params.leftMargin = value.margin;
 
-        //if this node is selected folder
+        //이 노드가 선택된 폴더라면 알파
         for (Folder f : value.selectedItemFolder) {
             if (f.getId() == value.folder.getId()) {
                 mBinding.iconLayout.setAlpha(0.5f);
                 mBinding.text.setAlpha(0.5f);
                 isSelected = true;
-                //parent node alpha
+                //부모 노드 알파
                 TreeNode parent = node.getParent();
                 if (parent.getViewHolder() instanceof TreeHolderCategory) {
                     TreeHolderCategory holder = (TreeHolderCategory) parent.getViewHolder();
@@ -61,14 +58,14 @@ public class TreeHolderFolder extends TreeNode.BaseNodeViewHolder<TreeHolderFold
             }
         }
 
-        //if parent node is selected folder
+        //부모 노드가 선택된 폴더라면
         if (node.getParent().getViewHolder() instanceof TreeHolderFolder && ((TreeHolderFolder) node.getParent().getViewHolder()).isSelected) {
             mBinding.iconLayout.setAlpha(0.5f);
             mBinding.text.setAlpha(0.5f);
             isSelected = true;
         }
 
-        //if selected item position is this node
+        //현재위치가 이 노드라면
         if (value.selectedItemSize.size() != 0) {
             Size size = value.selectedItemSize.get(0);
             if (size.getFolderId() == value.folder.getId()) {
@@ -78,6 +75,7 @@ public class TreeHolderFolder extends TreeNode.BaseNodeViewHolder<TreeHolderFold
         }
 
         int margin = (int) context.getResources().getDimension(R.dimen._8sdp);
+        //자식노드 생성
         for (Folder folder : value.allFolderList) {
             if (value.folder.getId() == folder.getFolderId()) {
                 TreeNode treeNode = new TreeNode(new FolderTreeHolder(folder, value.margin + margin, value.listener,
@@ -91,11 +89,12 @@ public class TreeHolderFolder extends TreeNode.BaseNodeViewHolder<TreeHolderFold
             mBinding.iconLayout.setOnClickListener(v -> tView.toggleNode(node));
         else mBinding.arrowIcon.setVisibility(View.INVISIBLE);
 
-        mBinding.addIcon.setOnClickListener(v -> value.listener.treeViewFolderAddClick(mNode, value));
+        mBinding.addIcon.setOnClickListener(v -> value.listener.treeViewFolderFolderAddClick(mNode, value));
         return mBinding.getRoot();
     }
 
     public void setIconClickable() {
+        //checked
         mBinding.arrowIcon.setVisibility(View.VISIBLE);
         mBinding.iconLayout.setOnClickListener(v -> tView.toggleNode(mNode));
     }
@@ -115,15 +114,17 @@ public class TreeHolderFolder extends TreeNode.BaseNodeViewHolder<TreeHolderFold
     }
 
     public static class FolderTreeHolder {
+        //all checked
         private final Folder folder;
         private final int margin;
-        private final TreeViewDialog.TreeViewAddClick listener;
+        private final TreeViewFolderFolderAddListener listener;
         private final List<Folder> selectedItemFolder, allFolderList;
         private final List<Size> selectedItemSize;
         private final List<Long> folderFolderIdList, sizeFolderIdList;
 
-        public FolderTreeHolder(Folder folder, int margin, TreeViewDialog.TreeViewAddClick listener, List<Folder> selectedItemFolder,
+        public FolderTreeHolder(Folder folder, int margin, TreeViewFolderFolderAddListener listener, List<Folder> selectedItemFolder,
                                 List<Size> selectedItemSize, List<Folder> allFolderList, List<Long> folderFolderIdList, List<Long> sizeFolderIdList) {
+            //checked
             this.folder = folder;
             this.margin = margin;
             this.listener = listener;
@@ -137,10 +138,11 @@ public class TreeHolderFolder extends TreeNode.BaseNodeViewHolder<TreeHolderFold
         public Folder getFolder() {
             return folder;
         }
+    }
 
-        public int getMargin() {
-            return margin;
-        }
+    public interface TreeViewFolderFolderAddListener {
+        //checked
+        void treeViewFolderFolderAddClick(TreeNode node, TreeHolderFolder.FolderTreeHolder value);
     }
 }
 
