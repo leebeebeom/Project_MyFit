ff0fa00d54d9c133c16fe94538d73a3d88ea867f
beebeom <leebeebeom@gmail.com>
2021-04-03 02:01:23 +0900

    코드 정리 및 수정

--- app/src/main/java/com/example/project_myfit/util/Sort.java
+++ app/src/main/java/com/example/project_myfit/util/Sort.java
@@ -16,20 +16,23 @@ import static com.example.project_myfit.MyFitConstant.SORT_NAME_REVERSE;
 import static com.example.project_myfit.MyFitConstant.TAG;
 
 public class Sort {
+    //all checked
     public static List<Category> categorySort(int sort, List<Category> categoryList) {
+        //checked
         if (sort == SORT_CREATE)
             categoryList.sort((o1, o2) -> Long.compare(o2.getId(), o1.getId()));
         else if (sort == SORT_CREATE_REVERSE)
             categoryList.sort((o1, o2) -> Long.compare(o1.getId(), o2.getId()));
         else if (sort == SORT_NAME)
-            categoryList.sort((o1, o2) -> o1.getCategory().compareTo(o2.getCategory()));
+            categoryList.sort((o1, o2) -> o1.getCategoryName().compareTo(o2.getCategoryName()));
         else if (sort == SORT_NAME_REVERSE)
-            categoryList.sort((o1, o2) -> o2.getCategory().compareTo(o1.getCategory()));
+            categoryList.sort((o1, o2) -> o2.getCategoryName().compareTo(o1.getCategoryName()));
         else Log.e(TAG, "categorySort: sort가 어디에도 속하지 않음", null);
         return categoryList;
     }
 
     public static List<Folder> folderSort(int sort, List<Folder> folderList) {
+        //checked
         if (sort == SORT_CREATE)
             folderList.sort((o1, o2) -> Long.compare(o2.getId(), o1.getId()));
         else if (sort == SORT_CREATE_REVERSE)
