271da4208d29912e8f72ac05fe3e0f8ae06008cb
beebeom <leebeebeom@gmail.com>
2021-04-05 04:48:52 +0900

    코드 정리 및 수정

--- app/src/main/java/com/example/project_myfit/dialog/CategoryNameEditDialog.java
+++ app/src/main/java/com/example/project_myfit/dialog/CategoryNameEditDialog.java
@@ -13,18 +13,19 @@ import com.example.project_myfit.databinding.ItemDialogEditTextBinding;
 
 import org.jetbrains.annotations.NotNull;
 
+import static com.example.project_myfit.MyFitConstant.CATEGORY;
 import static com.example.project_myfit.MyFitConstant.CATEGORY_NAME;
 
-//테스트 끝
 public class CategoryNameEditDialog extends DialogFragment {
     private ItemDialogEditTextBinding mBinding;
-    private CategoryNameEditConfirmClick mListener;
+    private CategoryNameEditConfirmListener mListener;
 
     public CategoryNameEditDialog() {
     }
 
     @NotNull
     public static CategoryNameEditDialog getInstance(String categoryName) {
+        //checked
         CategoryNameEditDialog categoryNameEditDialog = new CategoryNameEditDialog();
         Bundle bundle = new Bundle();
         bundle.putString(CATEGORY_NAME, categoryName);
@@ -34,30 +35,32 @@ public class CategoryNameEditDialog extends DialogFragment {
 
     @Override
     public void onAttach(@NonNull @NotNull Context context) {
+        //checked
         super.onAttach(context);
-        mListener = (CategoryNameEditConfirmClick) getTargetFragment();
+        mListener = (CategoryNameEditConfirmListener) getTargetFragment();
     }
 
     @NonNull
     @NotNull
     @Override
     public Dialog onCreateDialog(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        //checked
         String categoryName = null;
         if (getArguments() != null) categoryName = getArguments().getString(CATEGORY_NAME);
         if (savedInstanceState != null) categoryName = savedInstanceState.getString(CATEGORY_NAME);
-
-        mBinding = DialogUtils.getCategoryBinding(getLayoutInflater(), requireContext(), categoryName);
+        mBinding = DialogUtils.getBinding(getLayoutInflater(), requireContext(), categoryName, CATEGORY);
         return DialogUtils.getEditTextDialog(requireContext(), getString(R.string.edit_category_name), mBinding,
-                (dialog, which) -> mListener.categoryNameEditConfirmClick(String.valueOf(mBinding.dialogEditText.getText())));
+                (dialog, which) -> mListener.categoryNameEditConfirmClick(String.valueOf(mBinding.dialogEditText.getText()).trim()));
     }
 
     @Override
     public void onSaveInstanceState(@NonNull @NotNull Bundle outState) {
+        //checked
         super.onSaveInstanceState(outState);
         outState.putString(CATEGORY_NAME, String.valueOf(mBinding.dialogEditText.getText()));
     }
 
-    public interface CategoryNameEditConfirmClick {
+    public interface CategoryNameEditConfirmListener {
         void categoryNameEditConfirmClick(String categoryName);
     }
 }
