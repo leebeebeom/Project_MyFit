0276095b3a46bd9eabadf8b450b03272938daecb
beebeom <leebeebeom@gmail.com>
2021-04-11 08:28:34 +0900

    safeArgs 적용

--- app/src/main/java/com/example/project_myfit/dialog/SelectedItemDeleteDialog.java
+++ app/src/main/java/com/example/project_myfit/dialog/SelectedItemDeleteDialog.java
@@ -1,37 +1,39 @@
 package com.example.project_myfit.dialog;
 
 import android.app.Dialog;
-import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavBackStackEntry;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
+import com.example.project_myfit.NavigationViewModel;
 import com.example.project_myfit.R;
 
 import org.jetbrains.annotations.NotNull;
 
-public class SelectedItemDeleteDialog extends DialogFragment {
-    private SelectedItemDeleteConfirmListener mListener;
+import static com.example.project_myfit.MyFitConstant.DIALOG_CONFIRM_CLICK;
+import static com.example.project_myfit.MyFitConstant.SELECTED_ITEM_DELETE_CONFIRM_CLICK;
 
-    @Override
-    public void onAttach(@NonNull @NotNull Context context) {
-        super.onAttach(context);
-        mListener = (SelectedItemDeleteConfirmListener) getTargetFragment();
-    }
+public class SelectedItemDeleteDialog extends DialogFragment {
 
     @NonNull
     @NotNull
     @Override
     public Dialog onCreateDialog(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        NavController navController = NavHostFragment.findNavController(this);
+        NavBackStackEntry navBackStackEntry = navController.getBackStackEntry(R.id.selectedItemDeleteDialog);
+        NavigationViewModel navigationViewModel = new ViewModelProvider(navController.getViewModelStoreOwner(R.id.main_nav_graph)).get(NavigationViewModel.class);
+        navigationViewModel.backStackEntryLiveSetValue(navBackStackEntry);
+
         int selectedItemSize = SelectedItemDeleteDialogArgs.fromBundle(getArguments()).getSelectedItemSize();
+
         String message = selectedItemSize + getString(R.string.selected_item_delete_check);
         return DialogUtils.getConfirmDialog(requireContext(), message,
-                (dialog, which) -> mListener.selectedItemDeleteConfirmClick());
-    }
-
-    public interface SelectedItemDeleteConfirmListener {
-        void selectedItemDeleteConfirmClick();
+                (dialog, which) -> navBackStackEntry.getSavedStateHandle().set(DIALOG_CONFIRM_CLICK, SELECTED_ITEM_DELETE_CONFIRM_CLICK));
     }
 }
