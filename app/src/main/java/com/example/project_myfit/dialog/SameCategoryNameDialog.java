d8cc4e5e369163642660a53828f4ae480734e91d
beebeom <leebeebeom@gmail.com>
2021-04-10 23:54:54 +0900

    safeArgs 적용

--- app/src/main/java/com/example/project_myfit/dialog/SameCategoryNameDialog.java
+++ app/src/main/java/com/example/project_myfit/dialog/SameCategoryNameDialog.java
@@ -12,21 +12,8 @@ import com.example.project_myfit.R;
 
 import org.jetbrains.annotations.NotNull;
 
-import static com.example.project_myfit.MyFitConstant.CATEGORY_NAME;
-import static com.example.project_myfit.MyFitConstant.PARENT_CATEGORY;
-
 public class SameCategoryNameDialog extends DialogFragment {
     private SameCategoryNameConfirmListener mListener;
-
-    @NotNull
-    public static SameCategoryNameDialog getInstance(String categoryName, String parentCategory) {
-        SameCategoryNameDialog sameCategoryNameDialog = new SameCategoryNameDialog();
-        Bundle bundle = new Bundle();
-        bundle.putString(CATEGORY_NAME, categoryName);
-        bundle.putString(PARENT_CATEGORY, parentCategory);
-        sameCategoryNameDialog.setArguments(bundle);
-        return sameCategoryNameDialog;
-    }
 
     @Override
     public void onAttach(@NonNull @NotNull Context context) {
@@ -38,8 +25,8 @@ public class SameCategoryNameDialog extends DialogFragment {
     @NotNull
     @Override
     public Dialog onCreateDialog(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
-        String categoryName = getArguments() != null ? getArguments().getString(CATEGORY_NAME) : null;
-        String parentCategory = getArguments() != null ? getArguments().getString(PARENT_CATEGORY) : null;
+        String categoryName = SameCategoryNameDialogArgs.fromBundle(getArguments()).getCategoryName();
+        String parentCategory = SameCategoryNameDialogArgs.fromBundle(getArguments()).getParentCategory();
 
         return DialogUtils.getConfirmDialog(requireContext(), getString(R.string.same_category_name),
                 (dialog, which) -> mListener.SameCategoryNameConfirmClick(categoryName, parentCategory));
