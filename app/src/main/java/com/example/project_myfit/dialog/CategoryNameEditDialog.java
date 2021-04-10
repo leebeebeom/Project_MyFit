646f40fbd2ce39849ef5f89e12b78ea33f62184c
beebeom <leebeebeom@gmail.com>
2021-04-11 04:25:55 +0900

    리스너 삭제

--- app/src/main/java/com/example/project_myfit/dialog/CategoryNameEditDialog.java
+++ app/src/main/java/com/example/project_myfit/dialog/CategoryNameEditDialog.java
@@ -1,14 +1,19 @@
 package com.example.project_myfit.dialog;
 
 import android.app.Dialog;
-import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
+import com.example.project_myfit.NavigationViewModel;
 import com.example.project_myfit.R;
+import com.example.project_myfit.data.Repository;
+import com.example.project_myfit.data.model.Category;
 import com.example.project_myfit.databinding.ItemDialogEditTextBinding;
 
 import org.jetbrains.annotations.NotNull;
@@ -18,33 +23,32 @@ import static com.example.project_myfit.MyFitConstant.CATEGORY_NAME;
 
 public class CategoryNameEditDialog extends DialogFragment {
     private ItemDialogEditTextBinding mBinding;
-    private CategoryNameEditConfirmListener mListener;
-
-    @Override
-    public void onAttach(@NonNull @NotNull Context context) {
-        super.onAttach(context);
-        mListener = (CategoryNameEditConfirmListener) getTargetFragment();
-    }
 
     @NonNull
     @NotNull
     @Override
     public Dialog onCreateDialog(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
-        String categoryName = CategoryNameEditDialogArgs.fromBundle(getArguments()).getCategoryName();
-        categoryName = savedInstanceState != null ? savedInstanceState.getString(CATEGORY_NAME) : categoryName;
+        Repository repository = new Repository(requireContext());
+        long categoryId = CategoryNameEditDialogArgs.fromBundle(getArguments()).getCategoryId();
+        Category category = repository.getCategory(categoryId);
+        String categoryName = savedInstanceState == null ? category.getCategoryName() : savedInstanceState.getString(CATEGORY_NAME);
+
+        NavController navController = NavHostFragment.findNavController(this);
+        NavigationViewModel navigationViewModel = new ViewModelProvider(navController.getViewModelStoreOwner(R.id.main_nav_graph))
+                .get(NavigationViewModel.class);
+        navigationViewModel.backStackEntryLiveSetValue(navController.getBackStackEntry(R.id.categoryNameEditDialog));
 
         mBinding = DialogUtils.getBinding(getLayoutInflater(), requireContext(), categoryName, CATEGORY);
         return DialogUtils.getEditTextDialog(requireContext(), getString(R.string.edit_category_name), mBinding,
-                (dialog, which) -> mListener.categoryNameEditConfirmClick(String.valueOf(mBinding.dialogEditText.getText()).trim()));
+                (dialog, which) -> {
+                    category.setCategoryName(String.valueOf(mBinding.dialogEditText.getText()).trim());
+                    repository.categoryUpdate(category);
+                });
     }
 
     @Override
     public void onSaveInstanceState(@NonNull @NotNull Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putString(CATEGORY_NAME, String.valueOf(mBinding.dialogEditText.getText()));
-    }
-
-    public interface CategoryNameEditConfirmListener {
-        void categoryNameEditConfirmClick(String categoryName);
     }
 }
