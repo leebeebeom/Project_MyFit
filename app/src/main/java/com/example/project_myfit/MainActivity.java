a40e7c6446170c3a084adf0746731c5aa7fb3994
beebeom <leebeebeom@gmail.com>
2021-04-11 09:35:46 +0900

    model 지역변수, navController 전역변수, 주석 제거, TODO 추가

--- app/src/main/java/com/example/project_myfit/MainActivity.java
+++ app/src/main/java/com/example/project_myfit/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
@@ -17,25 +18,10 @@ import org.jetbrains.annotations.NotNull;
 import static com.example.project_myfit.MyFitConstant.FOLDER_ID;
 import static com.example.project_myfit.MyFitConstant.SIZE_ID;
 
-//TODO 디자인 패턴은 일단 나중에
-
-//TODO 생명주기 안 코드들 메소드 화
-
-//TODO 코드 최대한 간결하고 직관적으로
-
-//TODO 변수 엥간하면 인자로 받기
-
-//TODO 반복 코드들 유틸화
-
-//TODO 트리뷰 롱클릭으로 삭제, 이름변경
+//TODO 트리뷰 롱클릭으로 삭제, 이름변경?
 //TODO 탭레이아웃 뱃지 텍스트 사이즈
 
-//TODO 어댑터 셀렉트 포지션 포지션말고 아이디로
-
-//TODO 트리뷰 폴더 추가시 [리스트뷰]랑 같은 순서로 추가되게(가능?)
-//TODO 트리뷰 폴더 추가시 [서치뷰]랑 같은 순서로 추가되게(가능?)
-
-//TODO 엔드아이콘 클릭하면 키보드 보이게
+//TODO 서치뷰 엔드아이콘 클릭하면 키보드 보이게
 
 //TODO 서치뷰에서 폴더 추가시 뱃지 변경
 
@@ -45,7 +31,7 @@ import static com.example.project_myfit.MyFitConstant.SIZE_ID;
 
 public class MainActivity extends AppCompatActivity {
     private AppBarConfiguration mAppBarConfiguration;
-    private MainActivityViewModel mModel;
+    private NavController mNavControl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,20 +39,19 @@ public class MainActivity extends AppCompatActivity {
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        mModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+        MainActivityViewModel model = new ViewModelProvider(this).get(MainActivityViewModel.class);
 
         setSupportActionBar(binding.toolbar);
         ActionBar actionBar = getSupportActionBar();
-        //타이틀 안보이게(커스텀 타이틀 있음)
         if (actionBar != null) actionBar.setDisplayShowTitleEnabled(false);
 
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.host_fragment);
-        if (navHostFragment != null) mModel.setNavController(navHostFragment.getNavController());
+        if (navHostFragment != null) mNavControl = navHostFragment.getNavController();
 
         mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.mainFragment, R.id.favoriteFragment, R.id.settingFragment).build();
 
-        NavigationUI.setupActionBarWithNavController(this, mModel.getNavController(), mAppBarConfiguration);
-        NavigationUI.setupWithNavController(binding.bottomNav, mModel.getNavController());
+        NavigationUI.setupActionBarWithNavController(this, mNavControl, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(binding.bottomNav, mNavControl);
 
         //바텀네비게이션 뷰 그림자 제거
         binding.bottomNav.setBackgroundTintList(null);
@@ -76,15 +61,22 @@ public class MainActivity extends AppCompatActivity {
         //프래그먼트 변경 리스너
         destinationChangeListener(binding, actionBar);
 
+        //TODO getIntent 제거, navigation 이용
+        //sizeId, folderId 받은 후 존재하면 이동
         if (getIntent() != null)
             if (getIntent().getLongExtra(SIZE_ID, 0) != 0)
-                mModel.searchViewSizeClick(getIntent().getLongExtra(SIZE_ID, 0));
+                //TODO 삭제시 서치뷰 복귀
+                model.searchViewSizeClick(getIntent().getLongExtra(SIZE_ID, 0));
             else if (getIntent().getLongExtra(FOLDER_ID, 0) != 0)
-                mModel.searchViewFolderClick(getIntent().getLongExtra(FOLDER_ID, 0));
+                //TODO UP버튼 클릭시 서치뷰 복귀
+                //TODO 이름 변경시 종료됨
+                //TODO 네비게이션으로 뒤로 이동 후 백버튼 누르면 다시 서치뷰로 북귀함 and 어플 종료됨
+                //TODO 다른 폴더로 이동 후 back버튼 클릭시 다시 복귀됨(복귀 안되게)
+                model.searchViewFolderClick(getIntent().getLongExtra(FOLDER_ID, 0));
     }
 
     private void destinationChangeListener(ActivityMainBinding binding, ActionBar actionBar) {
-        mModel.getNavController().addOnDestinationChangedListener((controller, destination, arguments) -> {
+        mNavControl.addOnDestinationChangedListener((controller, destination, arguments) -> {
             if (destination.getId() == R.id.mainFragment) {
                 //메인 프래그먼트
                 fabChange(binding, R.drawable.icon_search);
@@ -112,6 +104,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onSupportNavigateUp() {
-        return NavigationUI.navigateUp(mModel.getNavController(), mAppBarConfiguration) || super.onSupportNavigateUp();
+        return NavigationUI.navigateUp(mNavControl, mAppBarConfiguration) || super.onSupportNavigateUp();
     }
 }
