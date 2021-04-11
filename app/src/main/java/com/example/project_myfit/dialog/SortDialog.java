ae8e9be1c7e9fe16a5cf684927b1227000f34f3c
beebeom <leebeebeom@gmail.com>
2021-04-12 01:32:38 +0900

    safeArgs 적용

--- app/src/main/java/com/example/project_myfit/dialog/SortDialog.java
+++ app/src/main/java/com/example/project_myfit/dialog/SortDialog.java
@@ -1,7 +1,6 @@
 package com.example.project_myfit.dialog;
 
 import android.app.Dialog;
-import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -13,12 +12,12 @@ import androidx.fragment.app.DialogFragment;
 
 import com.example.project_myfit.R;
 import com.example.project_myfit.databinding.SortViewBinding;
-import com.example.project_myfit.main.MainFragment;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.radiobutton.MaterialRadioButton;
 
 import org.jetbrains.annotations.NotNull;
 
+import static com.example.project_myfit.MyFitConstant.MAIN_FRAGMENT;
 import static com.example.project_myfit.MyFitConstant.SORT;
 import static com.example.project_myfit.MyFitConstant.SORT_BRAND;
 import static com.example.project_myfit.MyFitConstant.SORT_BRAND_REVERSE;
@@ -30,20 +29,15 @@ import static com.example.project_myfit.MyFitConstant.SORT_NAME_REVERSE;
 
 public class SortDialog extends DialogFragment {
     private int mCheckedItem;
-    private SortConfirmListener mListener;
-
-    @Override
-    public void onAttach(@NonNull @NotNull Context context) {
-        super.onAttach(context);
-        mListener = (SortConfirmListener) getTargetFragment();
-    }
 
     @NonNull
     @NotNull
     @Override
     public Dialog onCreateDialog(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
-        mCheckedItem = SortDialogArgs.fromBundle(getArguments()).getSort();
-        mCheckedItem = savedInstanceState != null ? savedInstanceState.getInt(SORT) : mCheckedItem;
+        mCheckedItem = savedInstanceState == null ? SortDialogArgs.fromBundle(getArguments()).getSort() : savedInstanceState.getInt(SORT);
+        String fragmentType = SortDialogArgs.fromBundle(getArguments()).getFragmentType();
+
+        DialogUtils dialogUtils = new DialogUtils(requireContext(), getLayoutInflater(), this).setBackStack(R.id.sortDialog);
 
         SortViewBinding binding = SortViewBinding.inflate(getLayoutInflater());
         MaterialRadioButton[] sortButtons = {binding.sortCustom, binding.sortCreate, binding.sortCreateReverse,
@@ -52,21 +46,22 @@ public class SortDialog extends DialogFragment {
         sortButtons[mCheckedItem].setChecked(true);
 
         //메인프래그먼트에서 실행 시 브랜드 삭제
-        if (getTargetFragment() instanceof MainFragment) {
+        if (fragmentType.equals(MAIN_FRAGMENT)) {
             binding.sortBrand.setVisibility(View.GONE);
             binding.sortBrandReverse.setVisibility(View.GONE);
         }
 
-        AlertDialog dialog = new MaterialAlertDialogBuilder(requireContext(), R.style.myAlertDialog)
+        AlertDialog alertDialog = new MaterialAlertDialogBuilder(requireContext(), R.style.myAlertDialog)
                 .setTitle(R.string.sort)
                 .setView(binding.getRoot())
                 .setNegativeButton(R.string.cancel, null)
-                .setPositiveButton(R.string.confirm, (dialog1, which) -> mListener.sortConfirmClick(mCheckedItem))
+                .setPositiveButton(R.string.confirm, (dialog, which) -> dialogUtils.sortConfirmClick(mCheckedItem))
                 .show();
-        Window window = dialog.getWindow();
-        DialogUtils.setLayout(requireContext(), window);
-        DialogUtils.setTextSize(requireContext(), dialog);
-        return dialog;
+
+        Window window = alertDialog.getWindow();
+        dialogUtils.setLayout(window);
+        dialogUtils.setTextSize(alertDialog);
+        return alertDialog;
     }
 
     private void addCheckListener(@NotNull MaterialRadioButton[] buttons) {
@@ -125,9 +120,5 @@ public class SortDialog extends DialogFragment {
     public void onSaveInstanceState(@NonNull @NotNull Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putInt(SORT, mCheckedItem);
-    }
-
-    public interface SortConfirmListener {
-        void sortConfirmClick(int sort);
     }
 }
