0e928235a15745ac50932ec2b8033476756e9e41
beebeom <leebeebeom@gmail.com>
2021-04-12 03:01:24 +0900

    safeArgs 적용

--- app/src/main/java/com/example/project_myfit/dialog/ItemMoveDialog.java
+++ app/src/main/java/com/example/project_myfit/dialog/ItemMoveDialog.java
@@ -1,52 +1,38 @@
 package com.example.project_myfit.dialog;
 
 import android.app.Dialog;
-import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 
 import com.example.project_myfit.R;
 
 import org.jetbrains.annotations.NotNull;
 
-import static com.example.project_myfit.MyFitConstant.FOLDER_ID;
-import static com.example.project_myfit.MyFitConstant.SELECTED_ITEM_SIZE;
-
 public class ItemMoveDialog extends DialogFragment {
-    private ItemMoveConfirmListener mListener;
-
-    @NotNull
-    public static ItemMoveDialog getInstance(int selectedItemSize, long folderId) {
-        ItemMoveDialog itemMoveDialog = new ItemMoveDialog();
-        Bundle bundle = new Bundle();
-        bundle.putInt(SELECTED_ITEM_SIZE, selectedItemSize);
-        bundle.putLong(FOLDER_ID, folderId);
-        itemMoveDialog.setArguments(bundle);
-        return itemMoveDialog;
-    }
-
-    @Override
-    public void onAttach(@NonNull @NotNull Context context) {
-        super.onAttach(context);
-        mListener = (ItemMoveConfirmListener) getTargetFragment();
-    }
 
     @NonNull
     @NotNull
     @Override
     public Dialog onCreateDialog(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
-        int amount = getArguments() != null ? getArguments().getInt(SELECTED_ITEM_SIZE) : 0;
-        long folderId = getArguments() != null ? getArguments().getLong(FOLDER_ID) : 0;
+        int selectedItemSize = ItemMoveDialogArgs.fromBundle(getArguments()).getSelectedItemSize();
+        long folderId = ItemMoveDialogArgs.fromBundle(getArguments()).getFolderId();
 
-        String message = amount + getString(R.string.item_move_check);
-        return DialogUtils.getConfirmDialog(requireContext(), message,
-                (dialog, which) -> mListener.itemMoveConfirmClick(folderId));
-    }
+        DialogUtils dialogUtils = new DialogUtils(requireContext(), getLayoutInflater(), this).setBackStack(R.id.itemMoveDialog);
 
-    public interface ItemMoveConfirmListener {
-        void itemMoveConfirmClick(long folderId);
+
+        AlertDialog alertDialog = dialogUtils.getConfirmDialog();
+        String message = selectedItemSize + getString(R.string.item_move_check);
+        alertDialog.setMessage(message);
+
+        Button positiveButton = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
+        positiveButton.setOnClickListener(v -> dialogUtils.itemMoveConfirmClick(folderId));
+
+        return alertDialog;
     }
 }
