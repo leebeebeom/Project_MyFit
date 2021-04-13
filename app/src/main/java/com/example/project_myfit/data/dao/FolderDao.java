4aa94b1a6c76a6ce22bc43605c4bfcca6071da15
beebeom <leebeebeom@gmail.com>
2021-04-14 06:54:47 +0900

    순서 정리

--- app/src/main/java/com/example/project_myfit/data/dao/FolderDao.java
+++ app/src/main/java/com/example/project_myfit/data/dao/FolderDao.java
@@ -12,23 +12,38 @@ import java.util.List;
 
 @Dao
 public interface FolderDao {
+    @Query("SELECT * FROM Folder WHERE isDeleted = 0 AND parentIsDeleted = 0")
+    LiveData<List<Folder>> getAllFolderLive();
+
     @Query("SELECT * FROM Folder WHERE parentId = :parentId AND isDeleted = 0 AND parentIsDeleted = 0 ORDER BY orderNumber")
     LiveData<List<Folder>> getFolderLiveByParentId(long parentId);
+
+    @Query("SELECT * FROM Folder WHERE isDeleted = 0 AND parentIsDeleted = 0")
+    List<Folder> getAllFolder();
 
     @Query("SELECT * FROM Folder WHERE parentCategory = :parentCategory AND isDeleted = 0 AND parentIsDeleted = 0 ORDER BY orderNumber")
     List<Folder> getFolderListByParentCategory(String parentCategory);
 
+    @Query("SELECT * FROM Folder WHERE parentId = :parentId AND isDeleted = 0 AND parentIsDeleted = 0")
+    List<Folder> getFolderListByParentId(long parentId);
+
+    @Query("SELECT folderName FROM Folder WHERE isDeleted = 0 AND parentIsDeleted = 0 AND parentCategory = :parentCategory")
+    List<String> getFolderNameListByParentCategory(String parentCategory);
+
     @Query("SELECT parentId FROM Folder WHERE parentCategory = :parentCategory AND isDeleted = 0 AND parentIsDeleted = 0")
-    List<Long> getFolderParentIdByParentCategory(String parentCategory);
+    List<Long> getFolderParentIdListByParentCategory(String parentCategory);
 
     @Query("SELECT folderName FROM Folder WHERE isDeleted = 0 AND parentIsDeleted = 0 ORDER BY folderName")
     List<String> getFolderNameList();
 
-    @Query("SELECT max(orderNumber) FROM Folder")
-    int getFolderLargestOrder();
-
     @Query("SELECT * FROM Folder WHERE id = :id")
     Folder getFolder(long id);
+
+    @Query("SELECT * FROM Folder ORDER BY id DESC limit 1")
+    Folder getLatestFolder();
+
+    @Query("SELECT max(orderNumber) FROM Folder")
+    int getFolderLargestOrder();
 
     @Insert
     void folderInsert(Folder folder);
@@ -38,19 +53,4 @@ public interface FolderDao {
 
     @Update
     void folderUpdate(List<Folder> folderList);
-
-    @Query("SELECT * FROM Folder WHERE parentId = :parentId AND isDeleted = 0 AND parentIsDeleted = 0")
-    List<Folder> getFolderListByParentId(long parentId);
-
-    @Query("SELECT * FROM Folder WHERE isDeleted = 0 AND parentIsDeleted = 0")
-    LiveData<List<Folder>> getAllFolderLive();
-
-    @Query("SELECT * FROM Folder WHERE isDeleted = 0 AND parentIsDeleted = 0")
-    List<Folder> getAllFolder();
-
-    @Query("SELECT folderName FROM Folder WHERE isDeleted = 0 AND parentIsDeleted = 0 AND parentCategory = :parentCategory")
-    List<String> getFolderNameListByParentCategory(String parentCategory);
-
-    @Query("SELECT * FROM Folder ORDER BY id DESC limit 1")
-    Folder getLatestFolder();
 }
