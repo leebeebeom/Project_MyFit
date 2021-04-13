3f8fa3f2d83dc12b5aa23a944bb544460ac50e79
beebeom <leebeebeom@gmail.com>
2021-04-14 03:31:10 +0900

    isDeleted, parentIsDeleted 인자 제거

---
+++ app/src/main/java/com/example/project_myfit/data/dao/SizeDao.java
@@ -0,0 +1,59 @@
+package com.example.project_myfit.data.dao;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.project_myfit.data.model.Size;
+
+import java.util.List;
+
+@Dao
+public interface SizeDao {
+    @Query("SELECT * FROM Size WHERE parentId = :folderId AND isDeleted = 0 AND parentIsDeleted = 0 ORDER BY orderNumber DESC")
+    LiveData<List<Size>> getSizeLiveByFolderId(long folderId);
+
+    @Query("SELECT parentId FROM Size WHERE parentCategory = :parentCategory AND isDeleted = 0 AND parentIsDeleted = 0")
+    List<Long> getSizeFolderIdByParent(String parentCategory);
+
+    @Query("SELECT * FROM size WHERE isDeleted = 0 AND parentIsDeleted = 0 ORDER BY name")
+    LiveData<List<Size>> getAllSizeLive();
+
+    @Query("SELECT * FROM Size WHERE parentCategory = :parentCategory AND isDeleted = 0 AND parentIsDeleted = 0 ORDER BY name")
+    List<Size> getAllSizeListByParent(String parentCategory);
+
+    @Query("SELECT brand FROM Size WHERE isDeleted = 0 AND parentIsDeleted = 0 ORDER BY brand")
+    List<String> getSizeBrandList();
+
+    @Query("SELECT name FROM Size WHERE isDeleted = 0 AND parentIsDeleted = 0 ORDER BY brand")
+    List<String> getSizeNameList();
+
+    @Query("SELECT * FROM Size WHERE id = :id")
+    Size getSize(long id);
+
+    @Query("SELECT max(orderNumber) FROM Size")
+    int getSizeLargestOrder();
+
+    @Insert
+    void sizeInsert(Size size);
+
+    @Update
+    void sizeUpdate(Size size);
+
+    @Update
+    void sizeUpdate(List<Size> sizeList);
+
+    @Query("SELECT * FROM Size WHERE isDeleted = 0 AND parentIsDeleted = 0 ORDER BY name")
+    List<Size> getAllSize();
+
+    @Query("SELECT * FROM Size WHERE parentId = :folderId AND isDeleted = 0 AND parentIsDeleted = 0 ")
+    List<Size> getSizeListByFolderId(long folderId);
+
+    @Query("SELECT * FROM Size WHERE isDeleted = 0 AND parentIsDeleted = 0")
+    LiveData<List<Size>> getSizeLiveForSearch();
+
+    @Query("SELECT * FROM Size WHERE isDeleted = 0 AND parentIsDeleted = 0")
+    List<Size> getAllSizeForSearch();
+}
