fe54e002bedcdd797c53a241434a17caf8924d36
beebeom <leebeebeom@gmail.com>
2021-04-14 02:56:27 +0900

    unUsed 삭제

--- app/src/main/java/com/example/project_myfit/MyFitConstant.java
+++ app/src/main/java/com/example/project_myfit/MyFitConstant.java
@@ -59,51 +59,25 @@ public class MyFitConstant {
     public static final int SORT_NAME = 5;
     public static final int SORT_NAME_REVERSE = 6;
 
-    //dialog tag
-    public static final String DELETE_DIALOG = "delete dialog";
-    public static final String TREE_DIALOG = "tree dialog";
-    public static final String FOLDER_EDIT_DIALOG = "folder edit dialog";
-    public static final String FOLDER_ADD_DIALOG = "folder add dialog";
-    public static final String CATEGORY_EDIT_DIALOG = "category edit dialog";
-    public static final String CATEGORY_ADD_DIALOG = "category add dialog";
-    public static final String MOVE_DIALOG = "move dialog";
-    public static final String SORT_DIALOG = "sort dialog";
-    public static final String TREE_ADD_FOLDER_DIALOG = "tree add folder";
-    public static final String SAME_CATEGORY_NAME_DIALOG = "same category name dialog";
-
-    //key
-    public static final String DIALOG_CONFIRM_CLICK = "dialog confirm click";
-    public static final String CATEGORY_NAME_EDIT_CONFIRM_CLICK = "category name edit confirm click";
+    //dialog key
+    public static final String ADD_CONFIRM_CLICK = "add confirm click";
+    public static final String NAME_EDIT_CONFIRM_CLICK = "name edit confirm click";
     public static final String SELECTED_ITEM_DELETE_CONFIRM_CLICK = "selected item deleted confirm click";
     public static final String SORT_CONFIRM_CLICK = "sort confirm click";
+    public static final String ITEM_MOVE_CONFIRM_CLICK = "item move confirm click";
+
+    public static final String NAME_EDIT_NAME = "name edit name";
 
     public static final String FOLDER_TOGGLE = "folder toggle";
 
-    public static final String CATEGORY_NAME = "category name";
-    public static final String FOLDER_NAME = "folder name";
-
-    public static final String SELECTED_ITEM_SIZE = "selected item size";
-
     public static final String TREE_VIEW_STATE = "state";
-
-    public static final String CURRENT_ITEM = "current item";
 
     public static final String SIZE_ID = "size id";
     public static final String FOLDER_ID = "folder id";
 
-    public static final String IS_TREE_VIEW = "is tree view";
-    public static final String IS_PARENT_NAME = "is parent name";
-
-    public static final String PARENT_CATEGORY = "parent category";
-    public static final String IS_SEARCH_VIEW = "is search view";
-
-    public static final String POPUP_IS_SHOWING = "popup is showing";
-
     //etc
     public static final String ALERT_TITLE = "alertTitle";
     public static final String ID = "id";
-
-    public static final String TAG = "에러";
 
     public static final int DOWN = 0;
     public static final int UP = 1;
