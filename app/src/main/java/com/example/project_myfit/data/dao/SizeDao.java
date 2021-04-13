6b2efb02da51913f0773745a3fc2116451d06c7d
beebeom <leebeebeom@gmail.com>
2021-04-14 07:29:10 +0900

    순서 정리

--- app/src/main/java/com/example/project_myfit/data/dao/SizeDao.java
+++ app/src/main/java/com/example/project_myfit/data/dao/SizeDao.java
@@ -12,22 +12,28 @@ import java.util.List;
 
 @Dao
 public interface SizeDao {
-    @Query("SELECT * FROM Size WHERE parentId = :parentId AND isDeleted = 0 AND parentIsDeleted = 0 ORDER BY orderNumber DESC")
-    LiveData<List<Size>> getSizeLiveByParentId(long parentId);
-
-    @Query("SELECT parentId FROM Size WHERE parentCategory = :parentCategory AND isDeleted = 0 AND parentIsDeleted = 0")
-    List<Long> getSizeParentIdByParentCategory(String parentCategory);
-
     @Query("SELECT * FROM size WHERE isDeleted = 0 AND parentIsDeleted = 0 ORDER BY name")
     LiveData<List<Size>> getAllSizeLive();
 
+    @Query("SELECT * FROM Size WHERE parentId = :parentId AND isDeleted = 0 AND parentIsDeleted = 0 ORDER BY orderNumber DESC")
+    LiveData<List<Size>> getSizeLiveByParentId(long parentId);
+
+    @Query("SELECT * FROM Size WHERE isDeleted = 0 AND parentIsDeleted = 0 ORDER BY name")
+    List<Size> getAllSizeList();
+
+    @Query("SELECT * FROM Size WHERE parentId = :parentId AND isDeleted = 0 AND parentIsDeleted = 0 ")
+    List<Size> getSizeListByParentId(long parentId);
+
     @Query("SELECT * FROM Size WHERE parentCategory = :parentCategory AND isDeleted = 0 AND parentIsDeleted = 0 ORDER BY name")
-    List<Size> getAllSizeListByParentCategory(String parentCategory);
+    List<Size> getSizeListByParentCategory(String parentCategory);
+
+    @Query("SELECT parentId FROM Size WHERE parentCategory = :parentCategory AND isDeleted = 0 AND parentIsDeleted = 0")
+    List<Long> getSizeParentIdListByParentCategory(String parentCategory);
 
     @Query("SELECT brand FROM Size WHERE isDeleted = 0 AND parentIsDeleted = 0 ORDER BY brand")
     List<String> getSizeBrandList();
 
-    @Query("SELECT name FROM Size WHERE isDeleted = 0 AND parentIsDeleted = 0 ORDER BY brand")
+    @Query("SELECT name FROM Size WHERE isDeleted = 0 AND parentIsDeleted = 0 ORDER BY name")
     List<String> getSizeNameList();
 
     @Query("SELECT * FROM Size WHERE id = :id")
@@ -44,16 +50,4 @@ public interface SizeDao {
 
     @Update
     void sizeUpdate(List<Size> sizeList);
-
-    @Query("SELECT * FROM Size WHERE isDeleted = 0 AND parentIsDeleted = 0 ORDER BY name")
-    List<Size> getAllSize();
-
-    @Query("SELECT * FROM Size WHERE parentId = :parentId AND isDeleted = 0 AND parentIsDeleted = 0 ")
-    List<Size> getSizeListByParentId(long parentId);
-
-    @Query("SELECT * FROM Size WHERE isDeleted = 0 AND parentIsDeleted = 0")
-    LiveData<List<Size>> getSizeLiveForSearch();
-
-    @Query("SELECT * FROM Size WHERE isDeleted = 0 AND parentIsDeleted = 0")
-    List<Size> getAllSizeForSearch();
 }
