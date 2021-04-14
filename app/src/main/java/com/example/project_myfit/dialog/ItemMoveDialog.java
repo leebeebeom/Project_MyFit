c34a3d98d07c4225399060e290857615529923c3
beebeom <leebeebeom@gmail.com>
2021-04-15 03:11:29 +0900

    정리끝

--- app/src/main/java/com/example/project_myfit/dialog/ItemMoveDialog.java
+++ app/src/main/java/com/example/project_myfit/dialog/ItemMoveDialog.java
@@ -16,21 +16,28 @@ import org.jetbrains.annotations.NotNull;
 
 public class ItemMoveDialog extends DialogFragment {
 
+    private int mSelectedItemSize;
+    private long mParentId;
+
+    @Override
+    public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mSelectedItemSize = ItemMoveDialogArgs.fromBundle(getArguments()).getSelectedItemSize();
+        mParentId = ItemMoveDialogArgs.fromBundle(getArguments()).getParentId();
+    }
+
     @NonNull
     @NotNull
     @Override
     public Dialog onCreateDialog(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
-        int selectedItemSize = ItemMoveDialogArgs.fromBundle(getArguments()).getSelectedItemSize();
-        long folderId = ItemMoveDialogArgs.fromBundle(getArguments()).getFolderId();
-        String message = selectedItemSize + getString(R.string.item_move_check);
+        String message = mSelectedItemSize + getString(R.string.item_move_check);
 
-        DialogUtils dialogUtils = new DialogUtils(requireContext(), getLayoutInflater(), this).setBackStack(R.id.itemMoveDialog);
-
+        DialogUtils dialogUtils = new DialogUtils(requireContext(), getLayoutInflater(), this).backStackLiveSetValue(R.id.itemMoveDialog);
 
         AlertDialog alertDialog = dialogUtils.getConfirmDialog(message);
 
         Button positiveButton = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
-        positiveButton.setOnClickListener(v -> dialogUtils.itemMoveConfirmClick(folderId));
+        positiveButton.setOnClickListener(v -> dialogUtils.itemMoveConfirmClick(mParentId));
 
         return alertDialog;
     }
