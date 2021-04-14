4d50f52ed21ce3d42ec7565e40f12b75b224d941
beebeom <leebeebeom@gmail.com>
2021-04-15 02:53:33 +0900

    folderId -> parentId

---
+++ app/src/main/java/com/example/project_myfit/dialog/DeleteConfirmDialog.java
@@ -0,0 +1,38 @@
+package com.example.project_myfit.dialog;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+
+import com.example.project_myfit.R;
+
+import org.jetbrains.annotations.NotNull;
+
+public class DeleteConfirmDialog extends DialogFragment {
+
+    private long mSizeId;
+
+    @Override
+    public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mSizeId = DeleteConfirmDialogArgs.fromBundle(getArguments()).getSizeId();
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public Dialog onCreateDialog(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        DialogUtils dialogUtils = new DialogUtils(requireContext(), getLayoutInflater(), this).backStackLiveSetValue(R.id.deleteConfirmDialog);
+
+        AlertDialog alertDialog = dialogUtils.getConfirmDialog(getString(R.string.delete_check));
+        Button positiveButton = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
+        positiveButton.setOnClickListener(v -> dialogUtils.deletedConfirmClick(mSizeId));
+        return alertDialog;
+    }
+}
