a3fc69c43f0e2ec1bf40184c46f2f184a6104ce9
beebeom <leebeebeom@gmail.com>
2021-04-15 03:02:41 +0900

    정리끝

--- app/src/main/java/com/example/project_myfit/dialog/GoBackDialog.java
+++ app/src/main/java/com/example/project_myfit/dialog/GoBackDialog.java
@@ -1,36 +1,30 @@
 package com.example.project_myfit.dialog;
 
 import android.app.Dialog;
-import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 
 import com.example.project_myfit.R;
 
 import org.jetbrains.annotations.NotNull;
 
-//테스트 끝
 public class GoBackDialog extends DialogFragment {
-    private GoBackConfirmClick mListener;
-
-    @Override
-    public void onAttach(@NonNull @NotNull Context context) {
-        super.onAttach(context);
-        mListener = (GoBackConfirmClick) getTargetFragment();
-    }
 
     @NonNull
     @NotNull
     @Override
     public Dialog onCreateDialog(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
-        return DialogUtils.getDialog(requireContext(), getString(R.string.go_back_check),
-                (dialog, which) -> mListener.goBackConfirmClick());
-    }
+        DialogUtils dialogUtils = new DialogUtils(requireContext(), getLayoutInflater(), this);
 
-    public interface GoBackConfirmClick {
-        void goBackConfirmClick();
+        AlertDialog alertDialog = dialogUtils.getConfirmDialog(getString(R.string.go_back_check));
+        Button positiveButton = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
+        positiveButton.setOnClickListener(v -> dialogUtils.goBackConfirmClick());
+        return alertDialog;
     }
 }
