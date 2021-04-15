a0265c1235f6ffb97c8462a4f10f64c385713689
beebeom <leebeebeom@gmail.com>
2021-04-15 12:38:13 +0900

    정리 끝

--- app/src/main/java/com/example/project_myfit/util/Sort.java
+++ app/src/main/java/com/example/project_myfit/util/Sort.java
@@ -5,17 +5,15 @@ import com.example.project_myfit.data.model.Folder;
 
 import java.util.List;
 
-import static com.example.project_myfit.MyFitConstant.SORT_BRAND;
-import static com.example.project_myfit.MyFitConstant.SORT_BRAND_REVERSE;
-import static com.example.project_myfit.MyFitConstant.SORT_CREATE;
-import static com.example.project_myfit.MyFitConstant.SORT_CREATE_REVERSE;
-import static com.example.project_myfit.MyFitConstant.SORT_NAME;
-import static com.example.project_myfit.MyFitConstant.SORT_NAME_REVERSE;
+import static com.example.project_myfit.util.MyFitConstant.SORT_BRAND;
+import static com.example.project_myfit.util.MyFitConstant.SORT_BRAND_REVERSE;
+import static com.example.project_myfit.util.MyFitConstant.SORT_CREATE;
+import static com.example.project_myfit.util.MyFitConstant.SORT_CREATE_REVERSE;
+import static com.example.project_myfit.util.MyFitConstant.SORT_NAME;
+import static com.example.project_myfit.util.MyFitConstant.SORT_NAME_REVERSE;
 
 public class Sort {
-    //all checked
     public static List<Category> categorySort(int sort, List<Category> categoryList) {
-        //checked
         if (sort == SORT_CREATE)
             categoryList.sort((o1, o2) -> Long.compare(o2.getId(), o1.getId()));
         else if (sort == SORT_CREATE_REVERSE)
@@ -28,7 +26,6 @@ public class Sort {
     }
 
     public static List<Folder> folderSort(int sort, List<Folder> folderList) {
-        //checked
         if (sort == SORT_CREATE)
             folderList.sort((o1, o2) -> Long.compare(o2.getId(), o1.getId()));
         else if (sort == SORT_CREATE_REVERSE)
