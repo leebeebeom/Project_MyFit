9ebd8b03575f7f72d17a10032b3f9aab6f40ffec
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-04-19 00:49:30 +0900

    클래스 생성

---
+++ app/src/main/java/com/example/project_myfit/dialog/SearchDialog/SearchSameNameAddDialog.java
@@ -0,0 +1,56 @@
+package com.example.project_myfit.dialog.SearchDialog;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+
+import com.example.project_myfit.R;
+import com.example.project_myfit.dialog.DialogUtils;
+import com.example.project_myfit.dialog.SameNameAddDialogArgs;
+
+import org.jetbrains.annotations.NotNull;
+
+import static com.example.project_myfit.util.MyFitConstant.CATEGORY;
+
+public class SearchSameNameAddDialog extends DialogFragment {
+    private String mItemType;
+    private String mParentCategory;
+    private long mParentId;
+    private String mNewName;
+
+    @Override
+    public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mItemType = SameNameAddDialogArgs.fromBundle(getArguments()).getItemType();
+        mParentCategory = SameNameAddDialogArgs.fromBundle(getArguments()).getParentCategory();
+        mParentId = SameNameAddDialogArgs.fromBundle(getArguments()).getParentId();
+        mNewName = SameNameAddDialogArgs.fromBundle(getArguments()).getNewName();
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public Dialog onCreateDialog(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        DialogUtils dialogUtils = new DialogUtils(requireContext(), getLayoutInflater(), this, R.id.search_nav_gragh).backStackLiveSetValue(R.id.searchSameNameAddDialog);
+
+        AlertDialog alertDialog;
+        if (mItemType.equals(CATEGORY))
+            alertDialog = dialogUtils.getConfirmDialog(getString(R.string.same_category_name_add));
+        else alertDialog = dialogUtils.getConfirmDialog(getString(R.string.same_folder_name_add));
+
+        Button positiveButton = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
+        positiveButton.setOnClickListener(v -> {
+            if (mItemType.equals(CATEGORY))
+                dialogUtils.sameNameCategoryAddConfirmClick(mNewName, mParentCategory, true);
+            else dialogUtils.sameNameFolderAddConfirmClick(mNewName, mParentCategory, mParentId, true);
+        });
+
+        return alertDialog;
+    }
+}
