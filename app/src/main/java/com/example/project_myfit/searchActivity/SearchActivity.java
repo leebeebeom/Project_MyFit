37bb9b5738c644c3581a2a7f7bdfb311c03057cf
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-04-22 02:29:25 +0900

    combineLive 추가

--- app/src/main/java/com/example/project_myfit/searchActivity/SearchActivity.java
+++ app/src/main/java/com/example/project_myfit/searchActivity/SearchActivity.java
@@ -3,21 +3,20 @@ package com.example.project_myfit.searchActivity;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.project_myfit.R;
+import com.example.project_myfit.data.model.Folder;
 import com.example.project_myfit.data.model.Size;
 import com.example.project_myfit.databinding.ActivitySearchBinding;
 import com.example.project_myfit.searchActivity.adapter.AutoCompleteAdapter;
-import com.example.project_myfit.util.Sort;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static com.example.project_myfit.util.MyFitConstant.SORT_BRAND;
-import static com.example.project_myfit.util.MyFitConstant.SORT_NAME;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -39,17 +38,58 @@ public class SearchActivity extends AppCompatActivity {
     }
 
     private void setAutoCompleteLive(@NotNull SearchViewModel model, AutoCompleteAdapter autoCompleteAdapter) {
-        model.getAllSizeLive().observe(this, sizeList -> {
+        LiveData<List<Folder>> folderLiveData = model.getAllFolderLive();
+        LiveData<List<Size>> sizeLiveData = model.getAllSizeLive();
+
+        MediatorLiveData<Object> mediatorLiveData = new MediatorLiveData<>();
+        mediatorLiveData.addSource(folderLiveData, mediatorLiveData::setValue);
+        mediatorLiveData.addSource(sizeLiveData, mediatorLiveData::setValue);
+
+        mediatorLiveData.observe(this, o -> {
             List<String> autoCompleteList = new ArrayList<>();
-            Sort.sizeSort(SORT_BRAND, sizeList);
-            for (Size size : sizeList)
-                if (!autoCompleteList.contains(size.getBrand()))
-                    autoCompleteList.add(size.getBrand());
-            Sort.sizeSort(SORT_NAME, sizeList);
-            for (Size size : sizeList)
-                if (!autoCompleteList.contains(size.getName()))
-                    autoCompleteList.add(size.getName());
+
+            if (o instanceof List<?>){
+                if (((List<?>) o).get(0) instanceof Folder)
+                    folderChangedValue(sizeLiveData, (List<?>) o, autoCompleteList);
+                else if (((List<?>) o).get(0) instanceof Size)
+                    sizeChangedValue(folderLiveData, (List<?>) o, autoCompleteList);
+            }
+            autoCompleteList.sort(String::compareTo);
             autoCompleteAdapter.setItem(autoCompleteList);
         });
+    }
+
+    private void folderChangedValue(LiveData<List<Size>> sizeLiveData, @NotNull List<?> objects, List<String> autoCompleteList) {
+        for (Object o : objects)
+            if (o instanceof Folder && !autoCompleteList.contains(((Folder) o).getFolderName().trim()))
+                autoCompleteList.add(((Folder) o).getFolderName().trim());
+
+        if (sizeLiveData.getValue() != null) {
+            List<Size> sizeList = sizeLiveData.getValue();
+            for (Size size : sizeList) {
+                if (!autoCompleteList.contains(size.getBrand().trim()))
+                    autoCompleteList.add(size.getBrand().trim());
+                if (!autoCompleteList.contains(size.getName().trim()))
+                    autoCompleteList.add(size.getName().trim());
+            }
+        }
+    }
+
+    private void sizeChangedValue(@NotNull LiveData<List<Folder>> folderLiveData, List<?> objects, List<String> autoCompleteList) {
+        if (folderLiveData.getValue() != null) {
+            List<Folder> folderList = folderLiveData.getValue();
+            for (Folder folder : folderList)
+                if (!autoCompleteList.contains(folder.getFolderName().trim()))
+                    autoCompleteList.add(folder.getFolderName().trim());
+        }
+
+        for (Object o : objects) {
+            if (o instanceof Size) {
+                if (!autoCompleteList.contains(((Size) o).getBrand().trim()))
+                    autoCompleteList.add(((Size) o).getBrand().trim());
+                if (!autoCompleteList.contains(((Size) o).getName().trim()))
+                    autoCompleteList.add(((Size) o).getName().trim());
+            }
+        }
     }
 }
