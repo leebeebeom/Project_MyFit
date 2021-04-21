f516647e9c9c76ac8a008b4a55a90197cb9b6691
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-04-22 02:39:06 +0900

    정리 및 수정

--- app/src/main/java/com/example/project_myfit/searchActivity/adapter/SearchDiffUtil.java
+++ app/src/main/java/com/example/project_myfit/searchActivity/adapter/SearchDiffUtil.java
@@ -22,13 +22,11 @@ public class SearchDiffUtil extends DiffUtil.ItemCallback<Object> {
     public boolean areContentsTheSame(@NonNull @NotNull Object oldItem, @NonNull @NotNull Object newItem) {
         if (oldItem instanceof Folder && newItem instanceof Folder)
             return ((Folder) oldItem).getFolderName().equals(((Folder) newItem).getFolderName()) &&
-                    ((Folder) oldItem).getDummy() == ((Folder) newItem).getDummy() &&
-                    ((Folder) oldItem).getParentId() == ((Folder) newItem).getParentId();
+                    ((Folder) oldItem).getDummy() == ((Folder) newItem).getDummy();
         else if (oldItem instanceof Size && newItem instanceof Size)
             return ((Size) oldItem).getBrand().equals(((Size) newItem).getBrand()) &&
                     ((Size) oldItem).getName().equals(((Size) newItem).getName()) &&
-                    String.valueOf(((Size) oldItem).getImageUri()).equals(String.valueOf(((Size) newItem).getImageUri())) &&
-                    ((Size) oldItem).getParentId() == ((Size) newItem).getParentId();
+                    String.valueOf(((Size) oldItem).getImageUri()).equals(String.valueOf(((Size) newItem).getImageUri()));
         return false;
     }
 }
