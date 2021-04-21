74994ff903e425a7a6bf46327d6b56e68a17f894
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-04-22 02:37:44 +0900

    정리 및 수정

--- app/src/main/java/com/example/project_myfit/searchActivity/adapter/SearchDiffUtil.java
+++ app/src/main/java/com/example/project_myfit/searchActivity/adapter/SearchDiffUtil.java
@@ -3,8 +3,8 @@ package com.example.project_myfit.searchActivity.adapter;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.DiffUtil;
 
-import com.example.project_myfit.ui.main.listfragment.database.Folder;
-import com.example.project_myfit.ui.main.listfragment.database.Size;
+import com.example.project_myfit.data.model.Folder;
+import com.example.project_myfit.data.model.Size;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -13,10 +13,6 @@ public class SearchDiffUtil extends DiffUtil.ItemCallback<Object> {
     public boolean areItemsTheSame(@NonNull @NotNull Object oldItem, @NonNull @NotNull Object newItem) {
         if (oldItem instanceof Folder && newItem instanceof Folder)
             return ((Folder) oldItem).getId() == ((Folder) newItem).getId();
-        else if (oldItem instanceof Folder && newItem instanceof Size)
-            return false;
-        else if (oldItem instanceof Size && newItem instanceof Folder)
-            return false;
         else if (oldItem instanceof Size && newItem instanceof Size)
             return ((Size) oldItem).getId() == ((Size) newItem).getId();
         return false;
@@ -25,14 +21,14 @@ public class SearchDiffUtil extends DiffUtil.ItemCallback<Object> {
     @Override
     public boolean areContentsTheSame(@NonNull @NotNull Object oldItem, @NonNull @NotNull Object newItem) {
         if (oldItem instanceof Folder && newItem instanceof Folder)
-            return ((Folder) oldItem).getFolderName().equals(((Folder) newItem).getFolderName());
-        else if (oldItem instanceof Folder && newItem instanceof Size)
-            return false;
-        else if (oldItem instanceof Size && newItem instanceof Folder)
-            return false;
+            return ((Folder) oldItem).getFolderName().equals(((Folder) newItem).getFolderName()) &&
+                    ((Folder) oldItem).getDummy() == ((Folder) newItem).getDummy() &&
+                    ((Folder) oldItem).getParentId() == ((Folder) newItem).getParentId();
         else if (oldItem instanceof Size && newItem instanceof Size)
             return ((Size) oldItem).getBrand().equals(((Size) newItem).getBrand()) &&
-                    ((Size) oldItem).getName().equals(((Size) newItem).getName());
+                    ((Size) oldItem).getName().equals(((Size) newItem).getName()) &&
+                    String.valueOf(((Size) oldItem).getImageUri()).equals(String.valueOf(((Size) newItem).getImageUri())) &&
+                    ((Size) oldItem).getParentId() == ((Size) newItem).getParentId();
         return false;
     }
 }
