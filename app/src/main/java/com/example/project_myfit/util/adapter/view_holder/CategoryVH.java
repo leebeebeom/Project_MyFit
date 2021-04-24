4a211ab8b44dc5ddd02e4c417c3d39c82366bfd5
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-04-25 05:06:59 +0900

    카테고리 뷰홀더 분리

---
+++ app/src/main/java/com/example/project_myfit/util/adapter/view_holder/CategoryVH.java
@@ -0,0 +1,39 @@
+package com.example.project_myfit.util.adapter.view_holder;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.project_myfit.data.model.Category;
+import com.example.project_myfit.databinding.ItemMainRecyclerCategoryBinding;
+import com.google.android.material.checkbox.MaterialCheckBox;
+
+import org.jetbrains.annotations.NotNull;
+
+public class CategoryVH extends RecyclerView.ViewHolder {
+    public final ItemMainRecyclerCategoryBinding mBinding;
+    private Category mCategory;
+
+    public CategoryVH(@NotNull ItemMainRecyclerCategoryBinding binding, CategoryAdapterListener listener) {
+        super(binding.getRoot());
+        this.mBinding = binding;
+
+        itemView.setOnClickListener(v -> listener.onCategoryCardViewClick(mCategory, mBinding.itemMainCheckBox));
+
+        itemView.setOnLongClickListener(v -> {
+            listener.onCategoryCardViewLongClick(getLayoutPosition());
+            return false;
+        });
+    }
+
+    public void setCategory(Category category) {
+        this.mCategory = category;
+        mBinding.setCategory(category);
+    }
+
+    public interface CategoryAdapterListener {
+        void onCategoryCardViewClick(Category category, MaterialCheckBox checkBox);
+
+        void onCategoryCardViewLongClick(int position);
+
+        void onCategoryDragHandleTouch(RecyclerView.ViewHolder viewHolder);
+    }
+}
