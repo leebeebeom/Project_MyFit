25fcf39db90f606e14a9886436314ae3ffa920e6
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-04-26 01:47:25 +0900

    정리 및 수정

--- app/src/main/java/com/example/project_myfit/util/adapter/view_holder/FolderGridVH.java
+++ app/src/main/java/com/example/project_myfit/util/adapter/view_holder/FolderGridVH.java
@@ -3,22 +3,21 @@ package com.example.project_myfit.util.adapter.view_holder;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.project_myfit.data.model.Folder;
-import com.example.project_myfit.databinding.ItemListRecyclerFolderBinding;
-import com.google.android.material.checkbox.MaterialCheckBox;
+import com.example.project_myfit.databinding.ItemFolderGridBinding;
 
 import org.jetbrains.annotations.NotNull;
 
 public class FolderGridVH extends RecyclerView.ViewHolder {
-    private final ItemListRecyclerFolderBinding mBinding;
+    private final ItemFolderGridBinding mBinding;
     private Folder mFolder;
 
-    public FolderGridVH(@NotNull ItemListRecyclerFolderBinding binding, FolderGridVHListener listener) {
+    public FolderGridVH(@NotNull ItemFolderGridBinding binding, FolderVHListener listener) {
         super(binding.getRoot());
         this.mBinding = binding;
 
         itemView.setOnClickListener(v -> {
             if (mFolder.getId() != -1)
-                listener.onFolderItemViewClick(mFolder, mBinding.itemListFolderCheckBox);
+                listener.onFolderItemViewClick(mFolder, mBinding.cbItemFolderGrid);
         });
 
         itemView.setOnLongClickListener(v -> {
@@ -33,15 +32,7 @@ public class FolderGridVH extends RecyclerView.ViewHolder {
         mBinding.setFolder(folder);
     }
 
-    public ItemListRecyclerFolderBinding getBinding() {
+    public ItemFolderGridBinding getBinding() {
         return mBinding;
-    }
-
-    public interface FolderGridVHListener {
-        void onFolderItemViewClick(Folder folder, MaterialCheckBox checkBox);
-
-        void onFolderItemViewLongClick(int position);
-
-        void onFolderDragHandleTouch(RecyclerView.ViewHolder holder);
     }
 }
