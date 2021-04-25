104fb6ed8f89c8eca21e547601f096884b85ac43
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-04-26 02:00:51 +0900

    정리 및 수정

---
+++ app/src/main/java/com/example/project_myfit/util/KeyboardUtil.java
@@ -0,0 +1,19 @@
+package com.example.project_myfit.util;
+
+import android.content.Context;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+import org.jetbrains.annotations.NotNull;
+
+public class KeyboardUtil {
+    public static void keyboardShow(@NotNull Context context, @NotNull View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.showSoftInputFromInputMethod(view.getWindowToken(), 0);
+    }
+
+    public static void keyboardHide(@NotNull Context context, @NotNull View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromInputMethod(view.getWindowToken(), 0);
+    }
+}
