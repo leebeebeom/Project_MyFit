5382559f6a134021e4c5ad1936fb7e21dc75a9d6
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-04-26 23:07:56 +0900

    정리 및 수정

--- app/src/main/java/com/example/project_myfit/search/main/adapter/SearchDiffUtil.java
+++ app/src/main/java/com/example/project_myfit/search/main/adapter/SearchDiffUtil.java
@@ -1,4 +1,4 @@
-package com.example.project_myfit.search_activity.adapter;
+package com.example.project_myfit.search.main.adapter;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.DiffUtil;
