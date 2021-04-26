5f825fc10397132c81bfe621c4e5df49d9f7fc63
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-04-26 23:15:26 +0900

    정리 및 수정

--- app/src/main/java/com/example/project_myfit/recyclebin/main/RecycleBinMainFragment.java
+++ app/src/main/java/com/example/project_myfit/recyclebin/main/RecycleBinMainFragment.java
@@ -1,4 +1,4 @@
-package com.example.project_myfit.recycle_bin_activity.fragment.main;
+package com.example.project_myfit.recyclebin.main;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -7,7 +7,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.PopupWindow;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -16,15 +15,12 @@ import androidx.fragment.app.Fragment;
 
 import com.example.project_myfit.R;
 import com.example.project_myfit.databinding.FragmentRecycleBinMainBinding;
-import com.example.project_myfit.databinding.PopupMenuRecycleBinMainBinding;
 
 import org.jetbrains.annotations.NotNull;
 
 public class RecycleBinMainFragment extends Fragment {
 
     private FragmentRecycleBinMainBinding mBinding;
-    private PopupMenuRecycleBinMainBinding mPopupMenuBinding;
-    private PopupWindow mPopupWindow;
 
     @Override
     public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
@@ -38,25 +34,18 @@ public class RecycleBinMainFragment extends Fragment {
     public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         mBinding = FragmentRecycleBinMainBinding.inflate(inflater);
         View view = mBinding.getRoot();
-        mPopupMenuBinding = PopupMenuRecycleBinMainBinding.inflate(inflater);
-        mPopupWindow = new PopupWindow(mPopupMenuBinding.getRoot(), ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        mPopupWindow.setOutsideTouchable(true);
-
         return view;
     }
 
     @Override
     public void onCreateOptionsMenu(@NonNull @NotNull Menu menu, @NonNull @NotNull MenuInflater inflater) {
-        inflater.inflate(R.menu.trash_main_menu, menu);
+        inflater.inflate(R.menu.menu_recycle_bin_main, menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull @NotNull MenuItem item) {
-        if (item.getItemId() == R.id.menuTrashMainSearch) {
+        if (item.getItemId() == R.id.menu_recycle_bin_search) {
             Toast.makeText(requireContext(), "검색 클릭", Toast.LENGTH_SHORT).show();
-            return true;
-        } else if (item.getItemId() == R.id.menuTrashMainPopup) {
-            mPopupWindow.showAsDropDown(requireActivity().findViewById(R.id.menuTrashMainPopup));
             return true;
         }
         return false;
