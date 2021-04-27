aa318d6f6e6699d7a398b84e761a928903f9a71c
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-04-28 02:25:02 +0900

    up버튼 추가

--- app/src/main/java/com/example/project_myfit/recyclebin/RecycleBinActivity.java
+++ app/src/main/java/com/example/project_myfit/recyclebin/RecycleBinActivity.java
@@ -1,12 +1,23 @@
 package com.example.project_myfit.recyclebin;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
+import com.example.project_myfit.R;
 import com.example.project_myfit.databinding.ActivityRecycleBinBinding;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.android.material.textview.MaterialTextView;
 
 public class RecycleBinActivity extends AppCompatActivity {
+
+    private NavController mNavController;
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +27,32 @@ public class RecycleBinActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.toolBarRecycleBin);
         if (getSupportActionBar() != null) getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.host_fragment_recycle_bin);
+        if (navHostFragment != null) mNavController = navHostFragment.getNavController();
+
+        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.recycleBinMain).build();
+
+        setDestinationChangeListener();
+    }
+
+    private void setDestinationChangeListener() {
+        MaterialTextView materialTextView = findViewById(R.id.tv_recycle_bin_title);
+        TextInputLayout autoCompleteLayout = findViewById(R.id.ac_tv_recycle_bin_search_layout);
+
+        mNavController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+            if (destination.getId() == R.id.recycleBinMain) {
+                materialTextView.setVisibility(View.VISIBLE);
+                autoCompleteLayout.setVisibility(View.GONE);
+            } else if (destination.getId() == R.id.recycleBinSearch) {
+                materialTextView.setVisibility(View.GONE);
+                autoCompleteLayout.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(mNavController, mAppBarConfiguration) || super.onSupportNavigateUp();
     }
 }
