db1a999817597c24075edb963942017d845d73fc
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-04-28 00:32:45 +0900

    네이밍 수정

--- app/src/main/java/com/example/project_myfit/data/dao/FolderDao.java
+++ app/src/main/java/com/example/project_myfit/data/dao/FolderDao.java
@@ -13,40 +13,43 @@ import java.util.List;
 @Dao
 public interface FolderDao {
     @Query("SELECT * FROM Folder WHERE isDeleted = 0 AND parentIsDeleted = 0 ORDER BY folderName")
-    LiveData<List<Folder>> getAllFolderLive();
-
-    @Query("SELECT * FROM Folder WHERE isDeleted = 1")
-    LiveData<List<Folder>> getAllDeletedFolderLive();
+    LiveData<List<Folder>> getFolderLive();
 
     @Query("SELECT * FROM Folder WHERE parentId = :parentId AND isDeleted = 0 AND parentIsDeleted = 0 ORDER BY orderNumber")
-    LiveData<List<Folder>> getFolderLiveByParentId(long parentId);
+    LiveData<List<Folder>> getFolderLive(long parentId);
+
+    @Query("SELECT * FROM Folder WHERE isDeleted = 1")
+    LiveData<List<Folder>> getDeletedFolderLive();
 
     @Query("SELECT * FROM Folder WHERE isDeleted = 0 AND parentIsDeleted = 0 ORDER BY orderNumber")
-    List<Folder> getAllFolderList();
+    List<Folder> getFolderList();
 
     @Query("SELECT * FROM Folder WHERE parentCategory = :parentCategory AND isDeleted = 0 AND parentIsDeleted = 0 ORDER BY orderNumber")
-    List<Folder> getFolderListByParentCategory(String parentCategory);
+    List<Folder> getFolderList(String parentCategory);
 
     @Query("SELECT * FROM Folder WHERE parentId = :parentId AND isDeleted = 0 AND parentIsDeleted = 0")
-    List<Folder> getFolderListByParentId(long parentId);
+    List<Folder> getFolderList(long parentId);
 
     @Query("SELECT folderName FROM Folder WHERE isDeleted =0 AND parentIsDeleted = 0")
-    LiveData<List<String>> getAllFolderNameLive();
+    LiveData<List<String>> getFolderNameLive();
 
-    @Query("SELECT folderName FROM Folder WHERE parentId = :parentId AND isDeleted =0 AND parentIsDeleted = 0")
-    LiveData<List<String>> getFolderNameLiveByParentId(long parentId);
+    @Query("SELECT folderName FROM Folder WHERE isDeleted =1 OR parentIsDeleted =1")
+    LiveData<List<String>> getDeletedFolderNameLive();
 
     @Query("SELECT folderName FROM Folder WHERE isDeleted = 0 AND parentIsDeleted = 0 AND parentId = :parentId")
-    List<String> getFolderNameListByParentId(long parentId);
+    List<String> getFolderNameList(long parentId);
 
     @Query("SELECT parentId FROM Folder WHERE parentCategory = :parentCategory AND isDeleted = 0 AND parentIsDeleted = 0")
-    List<Long> getFolderParentIdListByParentCategory(String parentCategory);
+    List<Long> getFolderParentIdList(String parentCategory);
+
+    @Query("SELECT parentId FROM Folder WHERE isDeleted AND parentIsDeleted = 1")
+    List<Long> getParentDeletedFolderParentIdList();
 
     @Query("SELECT * FROM Folder WHERE id = :id")
     Folder getFolder(long id);
 
     @Query("SELECT * FROM Folder WHERE id = :id")
-    LiveData<Folder> getFolderLive(long id);
+    LiveData<Folder> getSingleFolderLive(long id);
 
     @Query("SELECT * FROM Folder ORDER BY id DESC limit 1")
     Folder getLatestFolder();
