59aeb7ca0fe1780c8ca2936a107636a8d3d8008c
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-04-28 02:30:42 +0900

    toggle 추가

--- app/src/main/java/com/example/project_myfit/util/KeyboardUtil.java
+++ app/src/main/java/com/example/project_myfit/util/KeyboardUtil.java
@@ -7,13 +7,18 @@ import android.view.inputmethod.InputMethodManager;
 import org.jetbrains.annotations.NotNull;
 
 public class KeyboardUtil {
-    public static void keyboardShow(@NotNull Context context, @NotNull View view) {
+    public static void show(@NotNull Context context, @NotNull View view) {
         InputMethodManager inputMethodManager = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
         inputMethodManager.showSoftInputFromInputMethod(view.getWindowToken(), 0);
     }
 
-    public static void keyboardHide(@NotNull Context context, @NotNull View view) {
+    public static void hide(@NotNull Context context, @NotNull View view) {
         InputMethodManager inputMethodManager = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    public static void toggle(@NotNull Context context, View view){
+        InputMethodManager inputMethodManager = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.toggleSoftInput(0,0);
     }
 }
