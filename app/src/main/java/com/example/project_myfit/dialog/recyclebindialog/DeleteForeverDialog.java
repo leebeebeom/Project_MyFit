91210d173ccb74d75a092bc32d6f897e7c2cbf55
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-04-29 04:47:03 +0900

    휴지통 다이얼로그 생성

---
+++ app/src/main/java/com/example/project_myfit/dialog/recyclebindialog/DeleteForeverDialog.java
@@ -0,0 +1,41 @@
+package com.example.project_myfit.dialog.recyclebindialog;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+
+import com.example.project_myfit.R;
+import com.example.project_myfit.dialog.DialogUtils;
+
+import org.jetbrains.annotations.NotNull;
+
+public class DeleteForeverDialog extends DialogFragment {
+    private int mSelectedItemSize;
+
+    @Override
+    public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mSelectedItemSize = DeleteForeverDialogArgs.fromBundle(getArguments()).getSelectedItemSize();
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public Dialog onCreateDialog(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        DialogUtils dialogUtils = new DialogUtils(requireContext(), getLayoutInflater(), this, R.id.nav_graph_recycle_bin)
+                .backStackLiveSetValue(R.id.deleteForeverDialog);
+
+        AlertDialog alertDialog = dialogUtils.getConfirmDialog(mSelectedItemSize + getString(R.string.dialog_message_delete_forever));
+
+        Button positiveButton = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
+        positiveButton.setOnClickListener(v -> dialogUtils.deleteForever());
+
+        return alertDialog;
+    }
+}
---
+++ app/src/main/java/com/example/project_myfit/dialog/recyclebindialog/RestoreDialog.java
@@ -0,0 +1,39 @@
+package com.example.project_myfit.dialog.recyclebindialog;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+
+import com.example.project_myfit.R;
+import com.example.project_myfit.dialog.DialogUtils;
+
+import org.jetbrains.annotations.NotNull;
+
+public class RestoreDialog extends DialogFragment {
+    private int mSelectedItemSize;
+
+    @Override
+    public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mSelectedItemSize = RestoreDialogArgs.fromBundle(getArguments()).getSelectedItemSize();
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public Dialog onCreateDialog(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        DialogUtils dialogUtils = new DialogUtils(requireContext(), getLayoutInflater(), this, R.id.nav_graph_recycle_bin)
+                .backStackLiveSetValue(R.id.restoreDialog);
+
+        AlertDialog alertDialog = dialogUtils.getConfirmDialog(mSelectedItemSize + getString(R.string.dialog_message_restore));
+        Button positiveButton = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
+        positiveButton.setOnClickListener(v -> dialogUtils.restore());
+        return alertDialog;
+    }
+}
