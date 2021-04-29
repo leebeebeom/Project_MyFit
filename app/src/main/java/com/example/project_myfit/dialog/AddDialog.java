c0a868c4cbbea16a26f5eb24df185dffd90fbf54
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-04-30 02:59:19 +0900

    메소드 캡슐화

--- app/src/main/java/com/example/project_myfit/dialog/AddDialog.java
+++ app/src/main/java/com/example/project_myfit/dialog/AddDialog.java
@@ -1,11 +1,8 @@
 package com.example.project_myfit.dialog;
 
 import android.app.Dialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
@@ -15,6 +12,7 @@ import androidx.fragment.app.DialogFragment;
 
 import com.example.project_myfit.R;
 import com.example.project_myfit.databinding.ItemDialogEditTextBinding;
+import com.example.project_myfit.util.KeyboardUtil;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -41,17 +39,26 @@ public class AddDialog extends DialogFragment {
         DialogUtils dialogUtils = new DialogUtils(requireContext(), getLayoutInflater(), this, R.id.nav_graph_main).backStackLiveSetValue(R.id.addDialog);
 
         ItemDialogEditTextBinding binding = dialogUtils.getBinding(null, mItemType);
+        AlertDialog alertDialog = getDialog(dialogUtils, binding);
+        Button positiveButton = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
+        positiveClick(dialogUtils, binding, positiveButton);
+        dialogUtils.imeClick(binding, positiveButton);
+        return alertDialog;
+    }
+
+    @NotNull
+    private AlertDialog getDialog(DialogUtils dialogUtils, ItemDialogEditTextBinding binding) {
         AlertDialog alertDialog;
         if (mItemType.equals(CATEGORY))
             alertDialog = dialogUtils.getEditTextDialog(binding, getString(R.string.all_add_category), null);
         else
             alertDialog = dialogUtils.getEditTextDialog(binding, getString(R.string.all_create_folder), null);
+        return alertDialog;
+    }
 
-        Button positiveButton = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
-
+    private void positiveClick(DialogUtils dialogUtils, ItemDialogEditTextBinding binding, @NotNull Button positiveButton) {
         positiveButton.setOnClickListener(v -> {
-            InputMethodManager inputMethodManager = (InputMethodManager) requireContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-            inputMethodManager.hideSoftInputFromWindow(v.getWindowToken(), 0);
+            KeyboardUtil.hide(requireContext(), v);
 
             String newName = String.valueOf(binding.etDialog.getText()).trim();
 
@@ -59,12 +66,5 @@ public class AddDialog extends DialogFragment {
                 dialogUtils.addCategory(newName, mParentCategory, false);
             else dialogUtils.addFolder(newName, mParentId, mParentCategory, false);
         });
-
-        binding.etDialog.setOnEditorActionListener((v, actionId, event) -> {
-            if (actionId == EditorInfo.IME_ACTION_DONE && positiveButton.isEnabled())
-                positiveButton.callOnClick();
-            return false;
-        });
-        return alertDialog;
     }
 }
