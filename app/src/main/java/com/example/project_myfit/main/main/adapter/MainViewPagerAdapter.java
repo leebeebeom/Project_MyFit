bdaf8e988b652628b9eb37161b26cde620579652
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-03 01:21:13 +0900

    내부 코드 수정

--- app/src/main/java/com/example/project_myfit/main/main/adapter/MainViewPagerAdapter.java
+++ app/src/main/java/com/example/project_myfit/main/main/adapter/MainViewPagerAdapter.java
@@ -8,64 +8,25 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.project_myfit.data.model.Category;
 import com.example.project_myfit.databinding.ItemRecyclerViewBinding;
-import com.example.project_myfit.main.main.MainViewModel;
-import com.example.project_myfit.util.Sort;
+import com.example.project_myfit.util.DragSelectImpl;
 import com.example.project_myfit.util.adapter.viewholder.ViewPagerVH;
-import com.michaelflisar.dragselectrecyclerview.DragSelectTouchListener;
 
 import org.jetbrains.annotations.NotNull;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.example.project_myfit.util.MyFitConstant.BOTTOM;
-import static com.example.project_myfit.util.MyFitConstant.ETC;
-import static com.example.project_myfit.util.MyFitConstant.OUTER;
-import static com.example.project_myfit.util.MyFitConstant.TOP;
 
 public class MainViewPagerAdapter extends RecyclerView.Adapter<ViewPagerVH> {
     private final CategoryAdapter[] mCategoryAdapterArray;
     private final ViewPagerVH.ViewPagerAutoScrollListener mListener;
-    private final DragSelectTouchListener mDragSelectListener;
+    private final DragSelectImpl[] mDragSelectListenerArray;
     private final ItemTouchHelper[] mTouchHelperArray;
 
-    public MainViewPagerAdapter(CategoryAdapter[] adapterList, DragSelectTouchListener dragSelectListener,
+    public MainViewPagerAdapter(CategoryAdapter[] adapterList, DragSelectImpl[] dragSelectListenerArray,
                                 ItemTouchHelper[] touchHelperList, ViewPagerVH.ViewPagerAutoScrollListener listener) {
         this.mCategoryAdapterArray = adapterList;
-        this.mDragSelectListener = dragSelectListener;
+        this.mDragSelectListenerArray = dragSelectListenerArray;
         this.mTouchHelperArray = touchHelperList;
         this.mListener = listener;
         setHasStableIds(true);
-    }
-
-    public void setItem(int sort, List<Category> categoryList, MainViewModel model) {
-        List<Category> topList = new ArrayList<>();
-        List<Category> bottomList = new ArrayList<>();
-        List<Category> outerList = new ArrayList<>();
-        List<Category> etcList = new ArrayList<>();
-        for (Category category : Sort.categorySort(sort, categoryList)) {
-            switch (category.getParentCategory()) {
-                case TOP:
-                    topList.add(category);
-                    break;
-                case BOTTOM:
-                    bottomList.add(category);
-                    break;
-                case OUTER:
-                    outerList.add(category);
-                    break;
-                case ETC:
-                    etcList.add(category);
-                    break;
-            }
-        }
-
-        mCategoryAdapterArray[0].submitList(sort, topList, model.getFolderParentIdList(TOP), model.getSizeParentIdList(TOP));
-        mCategoryAdapterArray[1].submitList(sort, bottomList, model.getFolderParentIdList(BOTTOM), model.getSizeParentIdList(BOTTOM));
-        mCategoryAdapterArray[2].submitList(sort, outerList, model.getFolderParentIdList(OUTER), model.getSizeParentIdList(OUTER));
-        mCategoryAdapterArray[3].submitList(sort, etcList, model.getFolderParentIdList(ETC), model.getSizeParentIdList(ETC));
     }
 
     @Override
@@ -83,15 +44,13 @@ public class MainViewPagerAdapter extends RecyclerView.Adapter<ViewPagerVH> {
 
     @Override
     public void onBindViewHolder(@NonNull @NotNull ViewPagerVH holder, int position) {
-        if (holder.getBinding().rvItemRv.getAdapter() == null) {
+        if (holder.getBinding().rv.getAdapter() == null) {
             mCategoryAdapterArray[position].setViewPagerVH(holder);
+            holder.getBinding().rv.setAdapter(mCategoryAdapterArray[position]);
 
-            holder.getBinding().rvItemRv.setAdapter(mCategoryAdapterArray[position]);
-
-            mTouchHelperArray[position].attachToRecyclerView(holder.getBinding().rvItemRv);
-            holder.getBinding().rvItemRv.addOnItemTouchListener(mDragSelectListener);
-
-            holder.getBinding().tvItemRvNoDataLayout.setVisibility(mCategoryAdapterArray[position].getItemCount() == 0 ? View.VISIBLE : View.GONE);
+            mTouchHelperArray[position].attachToRecyclerView(holder.getBinding().rv);
+            holder.getBinding().rv.addOnItemTouchListener(mDragSelectListenerArray[position].setRecyclerView(holder.getBinding().rv));
+            holder.getBinding().layoutNoData.setVisibility(mCategoryAdapterArray[position].getItemCount() == 0 ? View.VISIBLE : View.GONE);
         }
     }
 
