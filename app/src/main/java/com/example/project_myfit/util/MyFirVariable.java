17059ca27b350547dbb61f21173a5139ebc30fbc
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-03 00:07:57 +0900

    static 변수 생성

---
+++ app/src/main/java/com/example/project_myfit/util/MyFirVariable.java
@@ -0,0 +1,11 @@
+package com.example.project_myfit.util;
+
+import androidx.appcompat.view.ActionMode;
+
+public class MyFirVariable {
+    public static ActionMode actionMode;
+    public static boolean isActionModeOn;
+    public static boolean isKeyboardShowing;
+    public static boolean isDragSelecting;
+
+}
