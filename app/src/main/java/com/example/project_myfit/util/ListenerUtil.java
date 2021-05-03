cd4f5e717e9411996f6fe6023ffb8ddb543a4c31
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-03 09:05:12 +0900

    최근검색, 오토컴플리트 리스너 추가

--- app/src/main/java/com/example/project_myfit/util/ListenerUtil.java
+++ app/src/main/java/com/example/project_myfit/util/ListenerUtil.java
@@ -1,13 +1,20 @@
 package com.example.project_myfit.util;
 
+import android.content.Context;
+import android.text.TextUtils;
 import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.core.widget.NestedScrollView;
 
+import com.example.project_myfit.data.Repository;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.MaterialAutoCompleteTextView;
+import com.google.android.material.textfield.TextInputLayout;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 import static com.example.project_myfit.util.MyFitConstant.DOWN;
 import static com.example.project_myfit.util.MyFitConstant.STOP;
@@ -65,4 +72,36 @@ public class ListenerUtil {
         } else if (event.getRawY() < 2000 && event.getRawY() > 250)
             MyFitVariable.scrollEnable = false;
     }
+
+    public void recentSearchClick(String word, @NotNull MaterialAutoCompleteTextView autoCompleteTextView, @NotNull TextInputLayout textInputLayout, Context context, int type) {
+        autoCompleteTextView.setText(word);
+        autoCompleteTextView.setSelection(word.length());
+        autoCompleteTextView.dismissDropDown();
+        textInputLayout.setEndIconVisible(false);
+        CommonUtil.keyBoardHide(context, autoCompleteTextView);
+        Repository.getRecentSearchRepository(context).overLapRecentSearchReInsert(word, type);
+    }
+
+    public void autoCompleteImeClick(@NotNull MaterialAutoCompleteTextView autoCompleteTextView, Context context, List<String> recentSearchStringList, int type) {
+        CommonUtil.keyBoardHide(context, autoCompleteTextView);
+
+        autoCompleteTextView.dismissDropDown();
+
+        String word = autoCompleteTextView.getText().toString().trim();
+        //검색어 중복시 지우고 맨 위로
+        if (!TextUtils.isEmpty(word)) {
+            if (recentSearchStringList.contains(word))
+                Repository.getRecentSearchRepository(context).overLapRecentSearchReInsert(word, type);
+            else Repository.getRecentSearchRepository(context).recentSearchInsert(word, type);
+        }
+    }
+
+    public void autoCompleteEndIconClick(MaterialAutoCompleteTextView autoCompleteTextView, @NotNull TextInputLayout textInputLayout, Context context) {
+        textInputLayout.setEndIconOnClickListener(v -> {
+            textInputLayout.setEndIconVisible(false);
+            autoCompleteTextView.setText("");
+            autoCompleteTextView.postDelayed(() -> CommonUtil.keyBoardShow(context, autoCompleteTextView), 50);
+        });
+    }
 }
+
