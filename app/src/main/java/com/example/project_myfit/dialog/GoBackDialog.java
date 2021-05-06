e775acdbaab01425e47fdfd5899b2fc38c32b54e
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-06 14:17:50 +0900

    구조 변경

--- app/src/main/java/com/example/project_myfit/dialog/GoBackDialog.java
+++ app/src/main/java/com/example/project_myfit/dialog/GoBackDialog.java
@@ -14,22 +14,27 @@ import com.example.project_myfit.R;
 
 import org.jetbrains.annotations.NotNull;
 
-import static com.example.project_myfit.util.MyFitConstant.GO_BACK_CONFIRM;
-
 public class GoBackDialog extends DialogFragment {
+
+    private int mNavGraphId;
+
+    @Override
+    public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mNavGraphId = GoBackDialogArgs.fromBundle(getArguments()).getNavGraphId();
+    }
 
     @NonNull
     @NotNull
     @Override
     public Dialog onCreateDialog(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
-        DialogUtil dialogUtil = new DialogUtil(requireContext(), this, R.id.nav_graph_main)
-                .backStackLiveSetValue(R.id.goBackDialog);
+        DialogUtil dialogUtil = new DialogUtil(requireContext(), this, mNavGraphId)
+                .setValueBackStackLive(R.id.goBackDialog);
 
         AlertDialog alertDialog = dialogUtil.getConfirmDialog(getString(R.string.dialog_message_go_back));
 
         Button positiveButton = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
-        positiveButton.setOnClickListener(v ->
-                dialogUtil.getBackStackEntry().getSavedStateHandle().set(GO_BACK_CONFIRM, null));
+        positiveButton.setOnClickListener(v -> dialogUtil.goBackConfirm());
         return alertDialog;
     }
 }
