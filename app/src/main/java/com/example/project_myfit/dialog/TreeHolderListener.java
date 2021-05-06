5034dcfd73ab2e190fdcf1028d1047e55bc529b1
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-07 01:33:14 +0900

    트리뷰 리스너 분리

---
+++ app/src/main/java/com/example/project_myfit/dialog/TreeHolderListener.java
@@ -0,0 +1,7 @@
+package com.example.project_myfit.dialog;
+
+import com.unnamed.b.atv.model.TreeNode;
+
+public interface TreeHolderListener {
+    void addFolderIconClick(TreeNode node, long parentId);
+}
