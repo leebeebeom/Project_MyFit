c56d6991f968cc8fa571a1432836f2c2e1fd686b
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-06 15:00:41 +0900

    구조 변경

--- app/src/main/java/com/example/project_myfit/dialog/RecentSearchDeleteAllDialog.java
+++ app/src/main/java/com/example/project_myfit/dialog/RecentSearchDeleteAllDialog.java
@@ -1,4 +1,4 @@
-package com.example.project_myfit.dialog.searchdialog;
+package com.example.project_myfit.dialog;
 
 import android.app.Dialog;
 import android.content.DialogInterface;
@@ -11,19 +11,25 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 
 import com.example.project_myfit.R;
-import com.example.project_myfit.dialog.DialogUtil;
 
 import org.jetbrains.annotations.NotNull;
 
-public class
-RecentSearchDeleteAllDialog extends DialogFragment {
+public class RecentSearchDeleteAllDialog extends DialogFragment {
+
+    private int mNavGraphId;
+
+    @Override
+    public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mNavGraphId = RecentSearchDeleteAllDialogArgs.fromBundle(getArguments()).getNavGraphId();
+    }
 
     @NonNull
     @NotNull
     @Override
     public Dialog onCreateDialog(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
-        DialogUtil dialogUtil = new DialogUtil(requireContext(), this, R.id.nav_graph_search)
-                .backStackLiveSetValue(R.id.recentSearchDeleteAllDialog);
+        DialogUtil dialogUtil = new DialogUtil(requireContext(), this, mNavGraphId)
+                .setValueBackStackLive(R.id.recentSearchDeleteAllDialog);
 
         AlertDialog alertDialog = dialogUtil.getConfirmDialog(getString(R.string.dialog_message_recent_search_delete_all));
 
