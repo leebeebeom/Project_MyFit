7e8c651d37c49e4b93ae0c33693c678e0850f641
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-06 14:44:12 +0900

    구조 변경

--- app/src/main/java/com/example/project_myfit/dialog/ItemMoveDialog.java
+++ app/src/main/java/com/example/project_myfit/dialog/ItemMoveDialog.java
@@ -18,24 +18,26 @@ public class ItemMoveDialog extends DialogFragment {
 
     private int mSelectedItemSize;
     private long mParentId;
+    private int mNavGraphId;
 
     @Override
     public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mSelectedItemSize = ItemMoveDialogArgs.fromBundle(getArguments()).getSelectedItemSize();
         mParentId = ItemMoveDialogArgs.fromBundle(getArguments()).getParentId();
+        mNavGraphId = ItemMoveDialogArgs.fromBundle(getArguments()).getNavGraphId();
     }
 
     @NonNull
     @NotNull
     @Override
     public Dialog onCreateDialog(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
-        DialogUtil dialogUtil = new DialogUtil(requireContext(), this, R.id.nav_graph_main).backStackLiveSetValue(R.id.itemMoveDialog);
+        DialogUtil dialogUtil = new DialogUtil(requireContext(), this, mNavGraphId).setValueBackStackLive(R.id.itemMoveDialog);
 
         AlertDialog alertDialog = dialogUtil.getConfirmDialog(mSelectedItemSize + getString(R.string.dialog_message_item_move));
 
         Button positiveButton = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
-        positiveButton.setOnClickListener(v -> dialogUtil.itemMove(mParentId, false));
+        positiveButton.setOnClickListener(v -> dialogUtil.itemMove(mParentId));
         return alertDialog;
     }
 }
