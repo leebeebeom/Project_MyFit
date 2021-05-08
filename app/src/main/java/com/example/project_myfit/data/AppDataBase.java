74f21bf196f2c107f8f2783bdf8908dc4e34ac60
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-09 02:33:45 +0900

    parentCategory 타입 변경, 마이그레이션 추가

--- app/src/main/java/com/example/project_myfit/data/AppDataBase.java
+++ app/src/main/java/com/example/project_myfit/data/AppDataBase.java
@@ -18,27 +18,79 @@ import com.example.project_myfit.data.model.Category;
 import com.example.project_myfit.data.model.Folder;
 import com.example.project_myfit.data.model.RecentSearch;
 import com.example.project_myfit.data.model.Size;
-import com.example.project_myfit.util.MyFitConstant;
+import com.example.project_myfit.util.Constant;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Database(entities = {Category.class, Size.class, Folder.class, RecentSearch.class}, version = 2, exportSchema = false)
+@Database(entities = {Category.class, Size.class, Folder.class, RecentSearch.class}, version = 1, exportSchema = false)
 @TypeConverters({Converters.class})
 public abstract class AppDataBase extends RoomDatabase {
     private static final Migration MIGRATION_2_1 = new Migration(2, 1) {
         @Override
         public void migrate(@NonNull @NotNull SupportSQLiteDatabase database) {
+            database.execSQL("CREATE TABLE Category_new(" +
+                    "id INTEGER NOT NULL PRIMARY KEY," +
+                    "parentCategory INTEGER NOT NULL DEFAULT 0," +
+                    "orderNumber INTEGER NOT NULL," +
+                    "isDeleted INTEGER NOT NULL," +
+                    "dummy INTEGER NOT NULL," +
+                    "categoryName TEXT," +
+                    "parentId INTEGER NOT NULL)");
+            database.execSQL("INSERT INTO Category_new (id, orderNumber, isDeleted, dummy, categoryName, parentId)" +
+                    "SELECT id, orderNumber, isDeleted, dummy ,categoryName, parentId FROM Category");
+            database.execSQL("DROP TABLE Category");
+            database.execSQL("ALTER TABLE Category_new RENAME TO Category");
+
+            database.execSQL("CREATE TABLE Folder_new(" +
+                    "id INTEGER NOT NULL PRIMARY KEY," +
+                    "parentCategory INTEGER NOT NULL DEFAULT 0," +
+                    "parentId INTEGER NOT NULL," +
+                    "folderName TEXT," +
+                    "orderNumber INTEGER NOT NULL, " +
+                    "isDeleted INTEGER NOT NULL," +
+                    "dummy INTEGER NOT NULL," +
+                    "isParentDeleted INTEGER NOT NULL)");
+            database.execSQL("INSERT INTO Folder_new (id, parentId, folderName, orderNumber, isDeleted, dummy ,isParentDeleted)" +
+                    "SELECT id, parentId, folderName, orderNumber, isDeleted, dummy, parentIsDeleted FROM Folder");
+            database.execSQL("DROP TABLE Folder");
+            database.execSQL("ALTER TABLE Folder_new RENAME TO Folder");
+
+            database.execSQL("CREATE TABLE Size_new(" +
+                    "id INTEGER NOT NULL PRIMARY KEY," +
+                    "orderNumber INTEGER NOT NULL," +
+                    "createdTime TEXT," +
+                    "modifiedTime TEXT," +
+                    "imageUri TEXT," +
+                    "brand TEXT," +
+                    "name TEXT," +
+                    "size TEXT," +
+                    "link TEXT," +
+                    "memo TEXT," +
+                    "parentCategory INTEGER NOT NULL DEFAULT 0," +
+                    "parentId INTEGER NOT NULL," +
+                    "isFavorite INTEGER NOT NULL," +
+                    "isDeleted INTEGER NOT NULL," +
+                    "isParentDeleted INTEGER NOT NULL," +
+                    "sizeMap TEXT)");
+            database.execSQL("INSERT INTO Size_new(id, orderNumber, createdTime, " +
+                    "modifiedTime, imageUri, brand, name, size, link, memo, parentId, isFavorite, isDeleted, isParentDeleted, sizeMap)" +
+                    "SELECT id, orderNumber, createdTime, modifiedTime, imageUri, brand, name, size, link, memo, parentId," +
+                    "isFavorite, isDeleted, parentIsDeleted, sizeMap FROM Size");
+            database.execSQL("DROP TABLE Size");
+            database.execSQL("ALTER TABLE Size_new RENAME TO Size");
+
             database.execSQL("CREATE TABLE RecentSearch_new(" +
-                    "id INTEGER PRIMARY KEY NOT NULL," +
+                    "id INTEGER NOT NULL PRIMARY KEY," +
                     "word TEXT," +
                     "date TEXT," +
-                    "type INTERGER NOT NULL DEFAULT 0," +
-                    "parentCategory TEXT DEFAULT NULL)");
-            database.execSQL("INSERT INTO RecentSearch_new(id, word, date, type)" +
-                    "SELECT id, word, date, type FROM RecentSearch");
+                    "type INTEGER NOT NULL," +
+                    "parentCategory INTEGER NOT NULL DEFAULT 0," +
+                    "parentId INTEGER NOT NULL)");
+            database.execSQL("INSERT INTO RecentSearch_new(id, word, date, type, parentId)" +
+                    "SELECT id, word, date, type, parentId FROM RecentSearch");
             database.execSQL("DROP TABLE RecentSearch");
             database.execSQL("ALTER TABLE RecentSearch_new RENAME TO RecentSearch");
         }
@@ -60,26 +112,26 @@ public abstract class AppDataBase extends RoomDatabase {
                         public void onCreate(@NonNull SupportSQLiteDatabase db) {
                             super.onCreate(db);
                             List<Category> categoryList = new ArrayList<>();
-                            categoryList.add(new Category(1, "반팔", MyFitConstant.TOP, 1));
-                            categoryList.add(new Category(2, "긴팔", MyFitConstant.TOP, 2));
-                            categoryList.add(new Category(3, "니트", MyFitConstant.TOP, 3));
-                            categoryList.add(new Category(4, "후드", MyFitConstant.TOP, 4));
-                            categoryList.add(new Category(5, "셔츠", MyFitConstant.TOP, 5));
-                            categoryList.add(new Category(6, "청바지", MyFitConstant.BOTTOM, 6));
-                            categoryList.add(new Category(7, "슬랙스", MyFitConstant.BOTTOM, 7));
-                            categoryList.add(new Category(8, "반바지", MyFitConstant.BOTTOM, 8));
-                            categoryList.add(new Category(9, "트랙 팬츠", MyFitConstant.BOTTOM, 9));
-                            categoryList.add(new Category(10, "Ma-1", MyFitConstant.OUTER, 10));
-                            categoryList.add(new Category(11, "패딩", MyFitConstant.OUTER, 11));
-                            categoryList.add(new Category(12, "야상", MyFitConstant.OUTER, 12));
-                            categoryList.add(new Category(13, "후드 집업", MyFitConstant.OUTER, 13));
-                            categoryList.add(new Category(14, "신발", MyFitConstant.ETC, 14));
-                            categoryList.add(new Category(15, "안경", MyFitConstant.ETC, 15));
-                            categoryList.add(new Category(16, "목걸이", MyFitConstant.ETC, 16));
-                            categoryList.add(new Category(17, "기타", MyFitConstant.ETC, 17));
+                            categoryList.add(new Category(1, "반팔", Constant.ParentCategory.TOP.ordinal(), 1));
+                            categoryList.add(new Category(2, "긴팔", Constant.ParentCategory.TOP.ordinal(), 2));
+                            categoryList.add(new Category(3, "니트", Constant.ParentCategory.TOP.ordinal(), 3));
+                            categoryList.add(new Category(4, "후드", Constant.ParentCategory.TOP.ordinal(), 4));
+                            categoryList.add(new Category(5, "셔츠", Constant.ParentCategory.TOP.ordinal(), 5));
+                            categoryList.add(new Category(6, "청바지", Constant.ParentCategory.BOTTOM.ordinal(), 6));
+                            categoryList.add(new Category(7, "슬랙스", Constant.ParentCategory.BOTTOM.ordinal(), 7));
+                            categoryList.add(new Category(8, "반바지", Constant.ParentCategory.BOTTOM.ordinal(), 8));
+                            categoryList.add(new Category(9, "트랙 팬츠", Constant.ParentCategory.BOTTOM.ordinal(), 9));
+                            categoryList.add(new Category(10, "Ma-1", Constant.ParentCategory.OUTER.ordinal(), 10));
+                            categoryList.add(new Category(11, "패딩", Constant.ParentCategory.OUTER.ordinal(), 11));
+                            categoryList.add(new Category(12, "야상", Constant.ParentCategory.OUTER.ordinal(), 12));
+                            categoryList.add(new Category(13, "후드 집업", Constant.ParentCategory.OUTER.ordinal(), 13));
+                            categoryList.add(new Category(14, "신발", Constant.ParentCategory.ETC.ordinal(), 14));
+                            categoryList.add(new Category(15, "안경", Constant.ParentCategory.ETC.ordinal(), 15));
+                            categoryList.add(new Category(16, "목걸이", Constant.ParentCategory.ETC.ordinal(), 16));
+                            categoryList.add(new Category(17, "기타", Constant.ParentCategory.ETC.ordinal(), 17));
                             new Repository(context).getCategoryRepository().insertCategory(categoryList);
                         }
-                    }).addMigrations(MIGRATION_1_2)
+                    }).addMigrations(MIGRATION_2_1)
                     .build();
         }
         return sInstance;
