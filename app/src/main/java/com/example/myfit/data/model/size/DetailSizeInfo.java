51294aafc11a245955af34cfb8254160024c3857
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-12 02:22:49 +0900

    DetailSizeInfo 생성

---
+++ app/src/main/java/com/example/myfit/data/model/size/DetailSizeInfo.java
@@ -0,0 +1,53 @@
+package com.example.myfit.data.model.size;
+
+public class DetailSizeInfo {
+    private String firstInfo, secondInfo, thirdInfo, fourthInfo, fifthInfo, sixthInfo;
+
+    public String getFirstInfo() {
+        return firstInfo;
+    }
+
+    public void setFirstInfo(String firstInfo) {
+        this.firstInfo = firstInfo;
+    }
+
+    public String getSecondInfo() {
+        return secondInfo;
+    }
+
+    public void setSecondInfo(String secondInfo) {
+        this.secondInfo = secondInfo;
+    }
+
+    public String getThirdInfo() {
+        return thirdInfo;
+    }
+
+    public void setThirdInfo(String thirdInfo) {
+        this.thirdInfo = thirdInfo;
+    }
+
+    public String getFourthInfo() {
+        return fourthInfo;
+    }
+
+    public void setFourthInfo(String fourthInfo) {
+        this.fourthInfo = fourthInfo;
+    }
+
+    public String getFifthInfo() {
+        return fifthInfo;
+    }
+
+    public void setFifthInfo(String fifthInfo) {
+        this.fifthInfo = fifthInfo;
+    }
+
+    public String getSixthInfo() {
+        return sixthInfo;
+    }
+
+    public void setSixthInfo(String sixthInfo) {
+        this.sixthInfo = sixthInfo;
+    }
+}
