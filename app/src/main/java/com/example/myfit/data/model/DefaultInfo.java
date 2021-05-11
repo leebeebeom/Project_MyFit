8b2b300e781700b448138f68b851d5682f64edaf
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-12 02:12:25 +0900

    setId 삭제

--- app/src/main/java/com/example/myfit/data/model/DefaultInfo.java
+++ app/src/main/java/com/example/myfit/data/model/DefaultInfo.java
@@ -20,10 +20,6 @@ public class DefaultInfo {
         return id;
     }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
     public byte getParentCategoryIndex() {
         return parentCategoryIndex;
     }
