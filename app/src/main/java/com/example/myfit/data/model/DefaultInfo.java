2803413f18e8241fa94fc59ec1df0de91405add6
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-12 00:43:32 +0900

    id setter 추가

--- app/src/main/java/com/example/myfit/data/model/DefaultInfo.java
+++ app/src/main/java/com/example/myfit/data/model/DefaultInfo.java
@@ -20,6 +20,10 @@ public class DefaultInfo {
         return id;
     }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
     public byte getParentCategoryIndex() {
         return parentCategoryIndex;
     }
