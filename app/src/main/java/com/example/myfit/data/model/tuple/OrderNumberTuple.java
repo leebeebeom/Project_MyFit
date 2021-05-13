ba3a0fa7c7e2bf29cd6ebfc100801b7e140a5126
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-13 09:03:42 +0900

    클래스 생성

---
+++ app/src/main/java/com/example/myfit/data/model/tuple/OrderNumberTuple.java
@@ -0,0 +1,18 @@
+package com.example.myfit.data.model.tuple;
+
+public class OrderNumberTuple {
+    private long id;
+    private int orderNumber;
+
+    public long getId() {
+        return id;
+    }
+
+    public int getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(int orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+}
