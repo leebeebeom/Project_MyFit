86276ce429c809ac663f1c8ae01af08e22fad841
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-13 09:04:13 +0900

    파라미터 순서 변경

--- app/src/main/java/com/example/myfit/data/model/ModelFactory.java
+++ app/src/main/java/com/example/myfit/data/model/ModelFactory.java
@@ -10,25 +10,24 @@ import org.jetbrains.annotations.NotNull;
 public class ModelFactory {
     @NotNull
     @Contract("_, _, _ -> new")
-    public static Category makeCategory(byte parentIndex, int orderNumber, String categoryName) {
+    public static Category makeCategory(String categoryName, byte parentIndex, int orderNumber) {
         BaseInfo defaultCategoryInfo = createDefaultInfo(parentIndex, orderNumber);
         return new Category(defaultCategoryInfo, categoryName);
     }
 
     @NotNull
     @Contract("_, _, _, _ -> new")
-    public static Folder makeFolder(byte parentIndex, int orderNumber, String folderName, long parentId) {
+    public static Folder makeFolder(String folderName, long parentId, byte parentIndex, int orderNumber) {
         BaseInfo defaultFolderInfo = createDefaultInfo(parentIndex, orderNumber);
         return new Folder(defaultFolderInfo, folderName, parentId);
     }
 
     @NotNull
     @Contract("_, _, _ -> new")
-    public static Size makeSize(byte parentIndex, int orderNumber, long parentId) {
+    public static Size makeSize(long parentId, byte parentIndex, int orderNumber) {
         BaseInfo defaultSizeInfo = createDefaultInfo(parentIndex, orderNumber);
         return new Size(defaultSizeInfo, parentId);
     }
-
 
     @NotNull
     @Contract(value = "_, _ -> new", pure = true)
