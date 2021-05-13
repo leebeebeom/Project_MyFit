337873f481f35f83c376c23753d60696e99de2c4
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-13 09:03:52 +0900

    클래스 생성

---
+++ app/src/main/java/com/example/myfit/data/model/tuple/ParentIdTuple.java
@@ -0,0 +1,17 @@
+package com.example.myfit.data.model.tuple;
+
+public class ParentIdTuple {
+    private long id, parentId;
+
+    public long getId() {
+        return id;
+    }
+
+    public long getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(long parentId) {
+        this.parentId = parentId;
+    }
+}
