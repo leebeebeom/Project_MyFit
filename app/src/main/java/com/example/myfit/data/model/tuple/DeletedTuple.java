c8a148b4eee6a1b641a93c321e70c50399d0e47a
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-16 04:03:44 +0900

    deletedTime 추가

--- app/src/main/java/com/example/myfit/data/model/tuple/DeletedTuple.java
+++ app/src/main/java/com/example/myfit/data/model/tuple/DeletedTuple.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 public class DeletedTuple {
     private long id;
     private boolean isDeleted;
+    private long deletedTime;
 
     public void setId(long id) {
         this.id = id;
@@ -22,17 +23,26 @@ public class DeletedTuple {
         isDeleted = deleted;
     }
 
+    public long getDeletedTime() {
+        return deletedTime;
+    }
+
+    public void setDeletedTime(long deletedTime) {
+        this.deletedTime = deletedTime;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof DeletedTuple)) return false;
         DeletedTuple that = (DeletedTuple) o;
         return getId() == that.getId() &&
-                isDeleted() == that.isDeleted();
+                isDeleted() == that.isDeleted() &&
+                getDeletedTime() == that.getDeletedTime();
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getId(), isDeleted());
+        return Objects.hash(getId(), isDeleted(), getDeletedTime());
     }
 }
