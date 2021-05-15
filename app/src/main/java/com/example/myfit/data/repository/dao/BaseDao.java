3e07bd56b9d5623f6a4eb0a383315e2ed09480dc
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-15 13:53:12 +0900

    기능 구현

--- app/src/main/java/com/example/myfit/data/repository/dao/BaseDao.java
+++ app/src/main/java/com/example/myfit/data/repository/dao/BaseDao.java
@@ -11,26 +11,91 @@ import androidx.room.Query;
 import androidx.room.Update;
 
 import com.example.myfit.data.model.BaseModel;
-import com.example.myfit.data.model.category.Category;
-import com.example.myfit.data.model.tuple.DeletedTuple;
+import com.example.myfit.data.model.tuple.BaseTuple;
+import com.example.myfit.data.model.tuple.CategoryFolderTuple;
 import com.example.myfit.data.model.tuple.ParentDeletedTuple;
-import com.example.myfit.data.model.tuple.ParentIdTuple;
 
-import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 @Dao
-public abstract class BaseDao<T extends BaseModel> {
+public abstract class BaseDao<T extends BaseModel, R extends BaseTuple> {
+
+    //to searchView
+    @Query("SELECT(SELECT name FROM Category WHERE isDeleted = 0) + " +
+            "(SELECT name FROM Folder WHERE isDeleted = 0 AND isParentDeleted = 0) + " +
+            "(SELECT name FROM Size WHERE isDeleted = 0 AND isParentDeleted = 0) + " +
+            "(SELECT brand FROM Size WHERE isDeleted = 0 AND isParentDeleted = 0)")
+    public abstract LiveData<List<String>> getAutoCompleteWordsLive();
+
+    //to recycleBin search
+    @Query("SELECT(SELECT name FROM Category WHERE isDeleted = 1) + " +
+            "(SELECT name FROM Folder WHERE isDeleted = 1 OR isParentDeleted = 1) + " +
+            "(SELECT name FROM Size WHERE isDeleted = 1 OR isParentDeleted = 1) + " +
+            "(SELECT brand FROM Size WHERE isDeleted = 1 OR isParentDeleted = 1)")
+    public abstract LiveData<List<String>> getDeletedAutoCompleteWordsLive();
+
+    @NotNull
+    protected LiveData<int[]> getContentsSizesLive(LiveData<List<R>> tuplesLive, boolean isParentDeleted) {
+        return Transformations.switchMap(tuplesLive, items -> {
+            long[] itemIds = getItemIds(items);
+            return getContentsSizesLiveByParentIds(itemIds, isParentDeleted);
+        });
+    }
+
+    protected long[] getItemIds(@NotNull List<R> tuples) {
+        return tuples.stream().mapToLong(BaseTuple::getId).toArray();
+    }
+
+    protected void setItemsContentsSize(List<CategoryFolderTuple> items, int[] contentsSizes) {
+        try {
+            int itemsSize = items.size();
+            for (int i = 0; i < itemsSize; i++)
+                items.get(i).setContentsSize(contentsSizes[i]);
+        } catch (NullPointerException e) {
+            logE(e);
+        }
+    }
+
+    protected List<LinkedList<R>> getClassifiedListByParentIndex(List<R> tuples) {
+        List<LinkedList<R>> classifiedList = Arrays.asList(new LinkedList<>(), new LinkedList<>(), new LinkedList<>(), new LinkedList<>());
+
+        try {
+            tuples.forEach(tuple -> classifiedList.get(tuple.getParentIndex()).add(tuple));
+        } catch (NullPointerException e) {
+            logE(e);
+        }
+        return classifiedList;
+    }
+
+    protected void logE(NullPointerException e) {
+        Log.e("에러", "Null Pointer Exception" + e, e);
+    }
+
+    protected void setParentDeletedTuples(@NotNull List<ParentDeletedTuple> parentDeletedTuples, boolean isParentDeleted) {
+        parentDeletedTuples.forEach(parentDeletedTuple -> parentDeletedTuple.setParentDeleted(isParentDeleted));
+    }
+
+    @Query("SELECT((SELECT COUNT(parentId) FROM Folder WHERE parentId IN (:parentIds) AND isDeleted = 0 AND isParentDeleted = :isParentDeleted) + " +
+            "(SELECT COUNT(parentId) FROM Size WHERE parentId IN(:parentIds) AND isDeleted =0 AND isParentDeleted = :isParentDeleted))")
+    protected abstract LiveData<int[]> getContentsSizesLiveByParentIds(long[] parentIds, boolean isParentDeleted);
+
+    @Query("SELECT((SELECT COUNT(parentId) FROM Folder WHERE parentId IN (:parentIds) AND isDeleted = 0 AND isParentDeleted = :isParentDeleted) + " +
+            "(SELECT COUNT(parentId) FROM Size WHERE parentId IN(:parentIds) AND isDeleted =0 AND isParentDeleted = :isParentDeleted))")
+    protected abstract int[] getContentsSizesByParentIds(long[] parentIds, boolean isParentDeleted);
+
+    @Query("SELECT((SELECT COUNT(parentId) FROM Folder WHERE parentId = :parentId AND isDeleted = 0 AND isParentDeleted = :isParentDeleted) + " +
+            "(SELECT COUNT(parentId) FROM Size WHERE parentId = :parentId AND isDeleted =0 AND isParentDeleted = :isParentDeleted))")
+    protected abstract int getContentsSizeByParentId(long parentId, boolean isParentDeleted);
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     public abstract long insert(T item);
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     public abstract long[] insert(T[] items);
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    public abstract long[] insert(List<T> items);
 
     @Update(onConflict = OnConflictStrategy.REPLACE)
     public abstract void update(T item);
@@ -38,70 +103,10 @@ public abstract class BaseDao<T extends BaseModel> {
     @Update(onConflict = OnConflictStrategy.REPLACE)
     public abstract void update(List<T> items);
 
-    @Update(onConflict = OnConflictStrategy.REPLACE)
-    public abstract void update(T[] items);
+    public interface BaseDaoInterFace {
+        LiveData<List<String>> getAutoCompleteWordsLive();
 
-    @Update(onConflict = OnConflictStrategy.REPLACE)
-    public abstract void updateDeletedTuples(DeletedTuple[] deletedTuples);
-
-    @Update(onConflict = OnConflictStrategy.REPLACE)
-    public abstract void updateParentDeletedTuples(List<ParentDeletedTuple> parentDeletedTuples);
-
-    @Update(onConflict = OnConflictStrategy.REPLACE)
-    public abstract void updateParentIdTuples(ParentIdTuple[] parentIdTuples);
-
-    protected <R extends Category> void clearItemsList(@NotNull List<List<R>> itemsList) {
-        if (!itemsList.isEmpty()) itemsList.clear();
+        LiveData<List<String>> getDeletedAutoCompleteWordsLive();
     }
 
-    protected <R extends Category> void setItemsContentsSize(List<R> items, int[] contentsSizes) {
-        try {
-            int itemsSize = items.size();
-            for (int i = 0; i < itemsSize; i++)
-                items.get(i).setContentsSize(contentsSizes[i]);
-        } catch (NullPointerException e) {
-            logError(e);
-        }
-    }
-
-    protected void sortByParentIndex(List<T> items, List<List<T>> itemsList) {
-        try {
-            items.forEach(category -> itemsList.get(category.getParentIndex()).add(category));
-        } catch (NullPointerException e) {
-            logError(e);
-        }
-    }
-
-    protected void logError(NullPointerException e) {
-        Log.e("에러", "setCategoryContentsSize: Null Pointer Excapter" + e, null);
-    }
-
-    @Query("SELECT((SELECT COUNT(parentId) FROM Folder WHERE parentId IN (:parentIds) AND isDeleted = 0 AND isParentDeleted = 0) + " +
-            "(SELECT COUNT(parentId) FROM Size WHERE parentId IN(:parentIds) AND isDeleted =0 AND isParentDeleted = 0))")
-    protected abstract LiveData<int[]> getContentsSizesLiveByParentIds(long[] parentIds);
-
-    @Query("SELECT((SELECT COUNT(parentId) FROM Folder WHERE parentId IN (:parentIds) AND isDeleted = 0 AND isParentDeleted = 0) + " +
-            "(SELECT COUNT(parentId) FROM Size WHERE parentId IN(:parentIds) AND isDeleted =0 AND isParentDeleted = 0))")
-    protected abstract int[] getContentsSizesByParentIds(long[] parentIds);
-
-    @Query("SELECT((SELECT COUNT(parentId) FROM Folder WHERE parentId = :parentId AND isDeleted = 0 AND isParentDeleted = 0) + " +
-            "(SELECT COUNT(parentId) FROM Size WHERE parentId = :parentId AND isDeleted =0 AND isParentDeleted = 0))")
-    protected abstract LiveData<Integer> getContentsSizeLiveByParentId(long parentId);
-
-    @Query("SELECT((SELECT COUNT(parentId) FROM Folder WHERE parentId = :parentId AND isDeleted = 0 AND isParentDeleted = 0) + " +
-            "(SELECT COUNT(parentId) FROM Size WHERE parentId = :parentId AND isDeleted =0 AND isParentDeleted = 0))")
-    protected abstract int getContentsSizeByParentId(long parentId);
-
-    @NotNull
-    @Contract("_ -> new")
-    protected LiveData<int[]> getContentsSizesLive(LiveData<List<T>> itemsLive) {
-        return Transformations.switchMap(itemsLive, items -> {
-            long[] itemsIds = getItemsIds(items);
-            return getContentsSizesLiveByParentIds(itemsIds);
-        });
-    }
-
-    protected long[] getItemsIds(@NotNull List<T> items) {
-        return items.stream().mapToLong(BaseModel::getId).toArray();
-    }
 }
