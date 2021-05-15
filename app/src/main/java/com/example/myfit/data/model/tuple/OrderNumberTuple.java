384d45e66b6c4e7cc5baa352ab9d675cf336d677
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-15 13:51:10 +0900

    게터세터 추가

--- app/src/main/java/com/example/myfit/data/model/tuple/OrderNumberTuple.java
+++ app/src/main/java/com/example/myfit/data/model/tuple/OrderNumberTuple.java
@@ -4,9 +4,13 @@ public class OrderNumberTuple {
     private long id;
     private int orderNumber;
 
-    public long getId() {
-        return id;
+    public void setId(long id) {
+        this.id = id;
     }
+
+    public long getId() {
+    return id;
+}
 
     public int getOrderNumber() {
         return orderNumber;
