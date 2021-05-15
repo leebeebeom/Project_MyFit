0e39a4ff121fd1f8dae08941e41f5e70e135f71a
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-15 13:51:22 +0900

    게터세터 추가

--- app/src/main/java/com/example/myfit/data/model/tuple/ParentIdTuple.java
+++ app/src/main/java/com/example/myfit/data/model/tuple/ParentIdTuple.java
@@ -3,6 +3,10 @@ package com.example.myfit.data.model.tuple;
 public class ParentIdTuple {
     private long id, parentId;
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
     public long getId() {
         return id;
     }
