9a8f4e47bd37ddedd6e441b17715cab40ea855c2
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-15 13:50:55 +0900

    클래스 생성

---
+++ app/src/main/java/com/example/myfit/data/model/tuple/CategoryFolderTuple.java
@@ -0,0 +1,13 @@
+package com.example.myfit.data.model.tuple;
+
+public class CategoryFolderTuple extends BaseTuple {
+    private int contentsSize;
+
+    public int getContentsSize() {
+        return contentsSize;
+    }
+
+    public void setContentsSize(int contentsSize) {
+        this.contentsSize = contentsSize;
+    }
+}
