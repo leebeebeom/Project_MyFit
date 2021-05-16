61d5141d7c938d38650cef56a3cdfec7980617b2
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-17 02:51:09 +0900

    대폭 수정

--- app/src/main/java/com/example/myfit/data/repository/dao/FolderDao.java
+++ app/src/main/java/com/example/myfit/data/repository/dao/FolderDao.java
@@ -8,13 +8,10 @@ import androidx.room.Query;
 import androidx.room.Transaction;
 import androidx.room.Update;
 
-import com.example.myfit.data.AppDataBase;
 import com.example.myfit.data.model.ModelFactory;
 import com.example.myfit.data.model.folder.Folder;
-import com.example.myfit.data.model.folder.FolderDeletedRelation;
 import com.example.myfit.data.model.tuple.CategoryFolderTuple;
 import com.example.myfit.data.model.tuple.DeletedTuple;
-import com.example.myfit.data.model.tuple.ParentDeletedTuple;
 import com.example.myfit.data.model.tuple.ParentIdTuple;
 import com.example.myfit.util.constant.SortValue;
 
@@ -24,201 +21,139 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
-import javax.inject.Inject;
-
 @Dao
 public abstract class FolderDao extends BaseDao<Folder, CategoryFolderTuple> {
-    private final AppDataBase appDataBase;
-    private SizeDao sizeDao;
-
-    @Inject
-    public FolderDao(@NotNull AppDataBase appDataBase) {
-        this.appDataBase = appDataBase;
-    }
 
     //to recycleBin
-    public LiveData<List<List<CategoryFolderTuple>>> getDeletedClassifiedFolderTuplesLive() {
-        LiveData<List<CategoryFolderTuple>> folderTuplesLive = this.getDeletedFolderTuplesLive();
-        LiveData<int[]> contentsSizesLive = super.getContentsSizesLive(folderTuplesLive, true);
+    public LiveData<List<List<CategoryFolderTuple>>> getDeletedClassifiedTuplesLive() {
+        LiveData<List<CategoryFolderTuple>> deletedTuplesLive = this.getTuplesLive(true);
+        LiveData<int[]> contentsSizesLive = super.getContentsSizesLive(deletedTuplesLive, true);
 
-        return this.getClassifiedFolderTuplesLive(folderTuplesLive, contentsSizesLive, SortValue.SORT_DELETED.getValue());
+        return super.getClassifiedTuplesLive(deletedTuplesLive, contentsSizesLive, SortValue.SORT_DELETED.getValue());
     }
 
-    @Query("SELECT id, parentIndex, orderNumber, name, contentsSize FROM Folder WHERE isDeleted = 1 AND isParentDeleted = 0")
-    protected abstract LiveData<List<CategoryFolderTuple>> getDeletedFolderTuplesLive();
-
-    @NotNull
-    private LiveData<List<List<CategoryFolderTuple>>> getClassifiedFolderTuplesLive(LiveData<List<CategoryFolderTuple>> folderTuplesLive,
-                                                                                    LiveData<int[]> contentsSizesLive,
-                                                                                    int sort) {
-        return Transformations.map(contentsSizesLive, contentsSizes -> {
-            List<CategoryFolderTuple> folderTuples = folderTuplesLive.getValue();
-            super.setContentsSize(folderTuples, contentsSizes);
-            List<List<CategoryFolderTuple>> classifiedList = super.getClassifiedListByParentIndex(folderTuples);
-            return super.orderCategoryFolderTuplesList(sort, classifiedList);
-        });
-    }
-
+    @Query("SELECT id, parentIndex, orderNumber, name, contentsSize FROM Folder WHERE isDeleted = :isDeleted")
+    protected abstract LiveData<List<CategoryFolderTuple>> getTuplesLive(boolean isDeleted);
 
     //to list
-    public LiveData<List<CategoryFolderTuple>> getFolderTuplesLiveByParentId(long parentId, int sort) {
-        LiveData<List<CategoryFolderTuple>> folderTuplesLive = this.getFolderTuplesLiveByParentId(parentId);
-        LiveData<int[]> contentsSizesLive = super.getContentsSizesLive(folderTuplesLive, false);
+    public LiveData<List<CategoryFolderTuple>> getTuplesLiveByParentId(long parentId, int sort) {
+        LiveData<List<CategoryFolderTuple>> tuplesLive = this.getTuplesLiveByParentId(parentId);
+        LiveData<int[]> contentsSizesLive = super.getContentsSizesLive(tuplesLive, false);
 
-        return this.getFolderTuplesLive(folderTuplesLive, contentsSizesLive, sort);
+        return this.getOrderedTuplesLive(tuplesLive, contentsSizesLive, sort);
     }
 
     @Query("SELECT id, parentIndex, orderNumber, name, contentsSize FROM Folder WHERE parentId = :parentId AND isDeleted = 0 AND isParentDeleted = 0")
-    protected abstract LiveData<List<CategoryFolderTuple>> getFolderTuplesLiveByParentId(long parentId);
+    protected abstract LiveData<List<CategoryFolderTuple>> getTuplesLiveByParentId(long parentId);
 
     @NotNull
-    private LiveData<List<CategoryFolderTuple>> getFolderTuplesLive(LiveData<List<CategoryFolderTuple>> folderTuplesLive,
-                                                                    LiveData<int[]> contentsSizesLive,
-                                                                    int sort) {
+    private LiveData<List<CategoryFolderTuple>> getOrderedTuplesLive(LiveData<List<CategoryFolderTuple>> tuplesLive,
+                                                                     LiveData<int[]> contentsSizesLive,
+                                                                     int sort) {
         return Transformations.map(contentsSizesLive, contentsSizes -> {
-            List<CategoryFolderTuple> folderTuples = folderTuplesLive.getValue();
-            super.setContentsSize(folderTuples, contentsSizes);
-            return super.orderCategoryFolderTuples(sort, folderTuples);
+            List<CategoryFolderTuple> tuples = tuplesLive.getValue();
+            super.setContentsSize(tuples, contentsSizes);
+            super.orderTuples(sort, tuples);
+            return tuples;
         });
     }
 
     //to searchView
-    public LiveData<List<List<CategoryFolderTuple>>> getSearchFolderTuplesListLive(String keyWord) {
-        LiveData<List<CategoryFolderTuple>> folderTuplesLive = getSearchFolderTuplesLive(false, keyWord);
-        LiveData<int[]> contentsSizesLive = super.getContentsSizesLive(folderTuplesLive, false);
+    public LiveData<List<List<CategoryFolderTuple>>> getSearchTuplesListLive(String keyWord) {
+        LiveData<List<CategoryFolderTuple>> searchTuplesLive = this.getSearchTuplesLive(false, keyWord);
+        LiveData<int[]> contentsSizesLive = super.getContentsSizesLive(searchTuplesLive, false);
 
-        return this.getClassifiedFolderTuplesLive(folderTuplesLive, contentsSizesLive, SortValue.SORT_NAME.getValue());
+        return this.getClassifiedTuplesLive(searchTuplesLive, contentsSizesLive, SortValue.SORT_NAME.getValue());
     }
 
     //to recycleBin search
     public LiveData<List<List<CategoryFolderTuple>>> getDeletedSearchFolderTuplesListLive(String keyWord) {
-        LiveData<List<CategoryFolderTuple>> folderTuplesLive = getSearchFolderTuplesLive(true, keyWord);
-        LiveData<int[]> contentsSizesLive = super.getContentsSizesLive(folderTuplesLive, true);
+        LiveData<List<CategoryFolderTuple>> deletedSearchTuplesLive = this.getSearchTuplesLive(true, keyWord);
+        LiveData<int[]> contentsSizesLive = super.getContentsSizesLive(deletedSearchTuplesLive, true);
 
-        return this.getClassifiedFolderTuplesLive(folderTuplesLive, contentsSizesLive, SortValue.SORT_NAME.getValue());
+        return this.getClassifiedTuplesLive(deletedSearchTuplesLive, contentsSizesLive, SortValue.SORT_NAME.getValue());
     }
 
-
     @Query("SELECT id, parentIndex, orderNumber, name, contentsSize FROM Folder WHERE isDeleted = :isDeleted AND isParentDeleted = 0 AND name LIKE :keyWord")
-    protected abstract LiveData<List<CategoryFolderTuple>> getSearchFolderTuplesLive(boolean isDeleted, String keyWord);
+    protected abstract LiveData<List<CategoryFolderTuple>> getSearchTuplesLive(boolean isDeleted, String keyWord);
 
     //to treeView (disposable)
     @Transaction
-    public List<CategoryFolderTuple> getFolderTuplesByParentIndex(byte parentIndex, int sort) {
-        List<CategoryFolderTuple> folderTuples = this.getFolderTuplesByParentIndex(parentIndex);
-        long[] folderIds = super.getItemIds(folderTuples);
-        int[] folderContentsSize = getContentsSizesByParentIds(folderIds);
-        super.setContentsSize(folderTuples, folderContentsSize);
-        return super.orderCategoryFolderTuples(sort, folderTuples);
+    public List<CategoryFolderTuple> getTuplesByParentIndex(byte parentIndex, int sort) {
+        List<CategoryFolderTuple> tuples = this.getTuplesByParentIndex(parentIndex);
+        long[] ids = super.getItemIds(tuples);
+        int[] contentsSizes = getContentsSizesByParentIds(ids);
+        super.setContentsSize(tuples, contentsSizes);
+        super.orderTuples(sort, tuples);
+        return tuples;
     }
 
     @Query("SELECT id, parentIndex, orderNumber, name, contentsSize FROM Folder WHERE parentIndex = :parentIndex AND isDeleted = 0 AND isParentDeleted = 0")
-    protected abstract List<CategoryFolderTuple> getFolderTuplesByParentIndex(byte parentIndex);
+    protected abstract List<CategoryFolderTuple> getTuplesByParentIndex(byte parentIndex);
 
     @Transaction
     //to treeView
-    public CategoryFolderTuple getFolderTupleById(long id) {
-        CategoryFolderTuple folderTuple = this.getFolderTupleById2(id);
+    public CategoryFolderTuple getTupleById(long id) {
+        CategoryFolderTuple tuple = this.getTupleById2(id);
         int contentsSize = getContentsSizeByParentId(id);
-        folderTuple.setContentsSize(contentsSize);
-        return folderTuple;
+        tuple.setContentsSize(contentsSize);
+        return tuple;
     }
 
     @Query("SELECT id, parentIndex, orderNumber, name, contentsSize FROM Folder WHERE id = :id AND isDeleted = 0 AND isParentDeleted = 0")
-    protected abstract CategoryFolderTuple getFolderTupleById2(long id);
+    protected abstract CategoryFolderTuple getTupleById2(long id);
 
     //to list
     @Query("SELECT * FROM Folder WHERE id = :id")
-    public abstract LiveData<Folder> getFolderLiveById(long id);
+    public abstract LiveData<Folder> getSingleLiveById(long id);
 
-    public long insertFolder(String name, long parentId, byte parentIndex) {
-        int orderNumber = this.getFolderLargestOrder() + 1;
+    @Transaction
+    public long insert(String name, long parentId, byte parentIndex) {
+        int orderNumber = this.getLargestOrderNumber() + 1;
         Folder folder = ModelFactory.makeFolder(name, parentId, parentIndex, orderNumber);
         return insert(folder);
     }
 
     @Query("SELECT max(orderNumber) FROM Folder")
-    protected abstract int getFolderLargestOrder();
-
-    public void updateFolder(long id, String name) {
-        CategoryFolderTuple folderTuple = this.getFolderTupleById(id);
-        folderTuple.setName(name);
-        this.updateTuple(folderTuple);
+    protected abstract int getLargestOrderNumber();
+    @Transaction
+    public void update(long id, String name) {
+        CategoryFolderTuple tuple = this.getTupleById2(id);
+        tuple.setName(name);
+        this.update(tuple);
     }
 
+    @Update(onConflict = OnConflictStrategy.REPLACE, entity = Folder.class)
+    protected abstract void update(CategoryFolderTuple tuple);
+
     @Transaction
-    public void moveFolders(long targetId, long[] folderIds) {
-        ParentIdTuple[] folderParentIdTuples = this.getFolderParentIdTuplesByIds(folderIds);
-        Arrays.stream(folderParentIdTuples)
+    public void move(long targetId, long[] folderIds) {
+        ParentIdTuple[] parentIdTuples = this.getParentIdTuplesByIds(folderIds);
+        Arrays.stream(parentIdTuples)
                 .forEach(parentIdTuple -> parentIdTuple.setParentId(targetId));
-        this.updateParentIdTuples(folderParentIdTuples);
+        this.update(parentIdTuples);
     }
 
     @Query("SELECT id, parentId FROM Folder WHERE id IN (:ids)")
-    protected abstract ParentIdTuple[] getFolderParentIdTuplesByIds(long[] ids);
+    protected abstract ParentIdTuple[] getParentIdTuplesByIds(long[] ids);
 
+    @Update(onConflict = OnConflictStrategy.REPLACE, entity = Folder.class)
+    protected abstract void update(ParentIdTuple[] parentIdTuples);
 
     @Transaction
-    public void deleteOrRestoreFolders(long[] folderIds, boolean isDeleted) {
-        FolderDeletedRelation[] folderDeletedRelations = this.getFolderDeletedRelationsByIds(folderIds, !isDeleted);
-        DeletedTuple[] folderDeletedTuples = super.getFolderDeletedTuples(folderDeletedRelations);
-        super.setDeletedTuples(folderDeletedTuples, isDeleted);
-        this.updateDeletedTuples(folderDeletedTuples);
+    public void deleteOrRestore(long[] folderIds, boolean isDeleted) {
+        DeletedTuple[] deletedTuples = this.getDeletedTuplesByIds(folderIds);
+        super.setDeletedTuples(deletedTuples, isDeleted);
+        this.update(deletedTuples);
 
-        this.setChildrenParentDeleted(folderDeletedRelations, isDeleted);
+        super.setChildrenParentDeleted(folderIds, isDeleted);
     }
 
-    private void setChildrenParentDeleted(FolderDeletedRelation[] folderDeletedTupleWithChildren, boolean isDeleted) {
-        if (sizeDao == null) sizeDao = appDataBase.sizeDao();
-
-        LinkedList<ParentDeletedTuple> folderChildParentDeletedTuples = this.getFolderChildFolderParentDeletedTuples(folderDeletedTupleWithChildren);
-        long[] folderChildFolderIds = super.getParentTuplesIds(folderChildParentDeletedTuples);
-        //folderChildParentDeletedTuples -> allChildFolderParentDeletedTuples
-        this.addAllChildParentDeletedTuples(folderChildFolderIds, folderChildParentDeletedTuples, !isDeleted);
-
-        long[] allFolderIds = super.getParentTuplesIds(folderChildParentDeletedTuples);
-        List<ParentDeletedTuple> allChildSizeParentDeletedTuple = sizeDao.getSizeParentDeletedTuplesByParentIds(allFolderIds, !isDeleted);
-
-        super.setParentDeletedTuples(folderChildParentDeletedTuples, isDeleted);
-        super.setParentDeletedTuples(allChildSizeParentDeletedTuple, isDeleted);
-        this.updateParentDeletedTuples(folderChildParentDeletedTuples);
-        sizeDao.updateParentDeletedTuples(allChildSizeParentDeletedTuple);
-    }
-
-    private void addAllChildParentDeletedTuples(long[] folderChildFolderIds,
-                                                @NotNull LinkedList<ParentDeletedTuple> folderChildParentDeletedTuples,
-                                                boolean isDeletedDenial) {
-        FolderDeletedRelation[] folderChildFolderDeletedTupleWithChildren = this.getFolderDeleteRelationByParentIds(folderChildFolderIds, isDeletedDenial);
-
-        LinkedList<ParentDeletedTuple> folderChildFolderDeletedTuples = super.getFolderChildFolderParentDeletedTuples(folderChildFolderDeletedTupleWithChildren);
-        folderChildParentDeletedTuples.addAll(folderChildFolderDeletedTuples);
-
-        Arrays.stream(folderChildFolderDeletedTupleWithChildren)
-                .filter(FolderDeletedRelation::areChildFoldersNotEmpty)
-                .forEach(folderDeletedRelation -> addAllChildParentDeletedTuples(folderDeletedRelation.getChildFolderIds(),
-                        folderChildParentDeletedTuples, isDeletedDenial));
-    }
-
-    @Query("SELECT id, isDeleted FROM Folder WHERE id IN (:ids) AND isDeleted = :isDeleted AND isParentDeleted = 0")
-    protected abstract FolderDeletedRelation[] getFolderDeletedRelationsByIds(long[] ids, boolean isDeleted);
-
-    @Query("SELECT id, isDeleted FROM Folder WHERE parentId IN (:parentIds) AND isDeleted = 0 AND isParentDeleted = :isParentDeleted")
-    protected abstract FolderDeletedRelation[] getFolderDeleteRelationByParentIds(long[] parentIds, boolean isParentDeleted);
+    @Query("SELECT id, isDeleted FROM Folder WHERE id IN (:ids)")
+    protected abstract DeletedTuple[] getDeletedTuplesByIds(long[] ids);
 
     @Update(onConflict = OnConflictStrategy.REPLACE, entity = Folder.class)
-    protected abstract void updateTuple(CategoryFolderTuple folderTuple);
-
-    @Update(onConflict = OnConflictStrategy.REPLACE, entity = Folder.class)
-    public abstract void updateTuples(LinkedList<CategoryFolderTuple> folderTuple);
-
-    @Update(onConflict = OnConflictStrategy.REPLACE, entity = Folder.class)
-    protected abstract void updateDeletedTuples(DeletedTuple[] deletedTuples);
-
-    @Update(onConflict = OnConflictStrategy.REPLACE, entity = Folder.class)
-    protected abstract void updateParentDeletedTuples(List<ParentDeletedTuple> parentDeletedTuples);
-
-    @Update(onConflict = OnConflictStrategy.REPLACE, entity = Folder.class)
-    protected abstract void updateParentIdTuples(ParentIdTuple[] parentIdTuples);
+    protected abstract void update(DeletedTuple[] deletedTuples);
 
     //from addFolder dialog
     @Query("SELECT EXISTS(SELECT name, parentId FROM Folder WHERE name =:folderName AND parentId = :parentId AND isDeleted = 0 AND isParentDeleted = 0)")
