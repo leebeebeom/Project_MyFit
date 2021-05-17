6e35c87c2c4db35730cd95e95397fb414ea6142c
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-18 04:37:47 +0900

    최근검색 전부 삭제 다이얼로그 생성

---
+++ app/src/main/java/com/example/myfit/ui/dialog/DeleteAllRecentSearchDialog.java
@@ -0,0 +1,54 @@
+package com.example.myfit.ui.dialog;
+
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+
+import com.example.myfit.R;
+import com.example.myfit.data.repository.RecentSearchRepository;
+
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
+public class DeleteAllRecentSearchDialog extends BaseDialog {
+    @Inject
+    DialogBuilder dialogBuilder;
+    @Inject
+    DialogBindingBuilder dialogBindingBuilder;
+    @Inject
+    RecentSearchRepository recentSearchRepository;
+
+    @NonNull
+    @NotNull
+    @Override
+    public Dialog onCreateDialog(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        return getAlertDialog();
+    }
+
+    @Override
+    protected AlertDialog getAlertDialog() {
+        return dialogBuilder.makeConfirmDialog(getString(R.string.dialog_message_recent_search_delete_all))
+                .setPositiveClickListener(getPositiveClickListener())
+                .create();
+    }
+
+    @NotNull
+    @Contract(pure = true)
+    @Override
+    protected View.OnClickListener getPositiveClickListener() {
+        return v -> {
+            recentSearchRepository.deleteAll();
+            dismiss();
+        };
+    }
+}
