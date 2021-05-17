adf745ac16436b57bf1387a1f2c3d76e9b6b0511
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-17 11:32:17 +0900

    컴포넌트 변경

--- app/src/main/java/com/example/myfit/di/DataModule.java
+++ app/src/main/java/com/example/myfit/di/DataModule.java
@@ -9,36 +9,38 @@ import com.example.myfit.data.repository.SizeRepository;
 
 import org.jetbrains.annotations.NotNull;
 
-import javax.inject.Singleton;
-
 import dagger.Module;
 import dagger.Provides;
 import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.ViewModelComponent;
 import dagger.hilt.android.qualifiers.ApplicationContext;
-import dagger.hilt.components.SingletonComponent;
+import dagger.hilt.android.scopes.ViewModelScoped;
 
 @Module
-@InstallIn(SingletonComponent.class)
+@InstallIn(ViewModelComponent.class)
 public class DataModule {
+    @ViewModelScoped
     @NotNull
     @Provides
-    @Singleton
     public static CategoryRepository provideCategoryRepository(@ApplicationContext Context context) {
         return new CategoryRepository(context);
     }
 
+    @ViewModelScoped
     @NotNull
     @Provides
     public static FolderRepository provideFolderRepository(@ApplicationContext Context context) {
         return new FolderRepository(context);
     }
 
+    @ViewModelScoped
     @NotNull
     @Provides
     public static SizeRepository provideSizeRepository(@ApplicationContext Context context) {
         return new SizeRepository(context);
     }
 
+    @ViewModelScoped
     @NotNull
     @Provides
     public static RecentSearchRepository provideRecentSearchRepository(@ApplicationContext Context context) {
