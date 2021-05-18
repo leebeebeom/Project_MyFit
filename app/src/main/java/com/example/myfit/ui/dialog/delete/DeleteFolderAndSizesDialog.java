fbfb53b6075611b18dd50ea8b8fba3472511bdcf
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-18 12:11:30 +0900

    구현

--- app/src/main/java/com/example/myfit/ui/dialog/delete/DeleteFolderAndSizesDialog.java
+++ app/src/main/java/com/example/myfit/ui/dialog/delete/DeleteFolderAndSizesDialog.java
@@ -1,17 +1,29 @@
 package com.example.myfit.ui.dialog.delete;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
+import androidx.hilt.navigation.HiltViewModelFactory;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavBackStackEntry;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.myfit.R;
 import com.example.myfit.data.repository.FolderRepository;
 import com.example.myfit.data.repository.SizeRepository;
 import com.example.myfit.ui.dialog.BaseDialog;
+import com.example.myfit.ui.main.MainGraphViewModel;
+
+import org.jetbrains.annotations.NotNull;
 
 import javax.inject.Inject;
+
+import static com.example.myfit.ui.dialog.delete.BaseDeleteDialog.DELETE_SELECT_ITEMS;
 
 public class DeleteFolderAndSizesDialog extends BaseDialog {
     @Inject
@@ -20,12 +32,24 @@ public class DeleteFolderAndSizesDialog extends BaseDialog {
     SizeRepository sizeRepository;
     private long[] selectedFolderIds;
     private long[] selectedSizeIds;
+    private NavBackStackEntry navBackStackEntry;
 
     @Override
     public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         selectedFolderIds = DeleteFolderAndSizesDialogArgs.fromBundle(getArguments()).getSelectedFolderIds();
         selectedSizeIds = DeleteFolderAndSizesDialogArgs.fromBundle(getArguments()).getSelectedSizeIds();
+        NavController navController = NavHostFragment.findNavController(this);
+        navBackStackEntry = navController.getBackStackEntry(navController.getGraph().getId());
+        MainGraphViewModel mainGraphViewModel = new ViewModelProvider(navBackStackEntry, HiltViewModelFactory.create(requireContext(), navBackStackEntry)).get(MainGraphViewModel.class);
+        mainGraphViewModel.setBackStackEntryLive(navBackStackEntry);
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public Dialog onCreateDialog(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        return getAlertDialog();
     }
 
     @Override
@@ -41,6 +65,7 @@ public class DeleteFolderAndSizesDialog extends BaseDialog {
         return v -> {
             folderRepository.deleteOrRestore(selectedFolderIds, true);
             sizeRepository.deleteOrRestore(selectedSizeIds, true);
+            navBackStackEntry.getSavedStateHandle().set(DELETE_SELECT_ITEMS, null);
             dismiss();
         };
     }
