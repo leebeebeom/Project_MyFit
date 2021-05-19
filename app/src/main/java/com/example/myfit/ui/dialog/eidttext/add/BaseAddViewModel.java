ceb60d9c8fc0f72aa60b190b7e36e1d5f56a3126
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-20 06:22:22 +0900

    뷰모델 쪼개기

---
+++ app/src/main/java/com/example/myfit/ui/dialog/eidttext/add/BaseAddViewModel.java
@@ -0,0 +1,7 @@
+package com.example.myfit.ui.dialog.eidttext.add;
+
+import com.example.myfit.ui.dialog.eidttext.BaseEditTextViewModel;
+
+public abstract class BaseAddViewModel extends BaseEditTextViewModel {
+    public abstract void insert();
+}
---
+++ app/src/main/java/com/example/myfit/ui/dialog/eidttext/edit/BaseEditViewModel.java
@@ -0,0 +1,7 @@
+package com.example.myfit.ui.dialog.eidttext.edit;
+
+import com.example.myfit.ui.dialog.eidttext.BaseEditTextViewModel;
+
+public abstract class BaseEditViewModel extends BaseEditTextViewModel {
+    public abstract void update();
+}
