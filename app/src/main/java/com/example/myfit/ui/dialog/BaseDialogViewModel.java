a6b771cc7fe21dc6234a93c1a31366146a36a45a
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-20 02:54:24 +0900

    베이스 모델 통합

--- app/src/main/java/com/example/myfit/ui/dialog/BaseDialogViewModel.java
+++ app/src/main/java/com/example/myfit/ui/dialog/BaseDialogViewModel.java
@@ -1,14 +1,12 @@
-package com.example.myfit.ui.dialog.add;
+package com.example.myfit.ui.dialog;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public abstract class BaseAddViewModel extends ViewModel {
+public abstract class BaseDialogViewModel extends ViewModel {
     private final MediatorLiveData<Boolean> isExistingMutable = new MediatorLiveData<>();
-
-    public abstract void insert();
 
     public MutableLiveData<Boolean> getIsExistingMutable() {
         isExistingMutable.addSource(getIsExistingLive(), isExistingMutable::setValue);
@@ -20,4 +18,12 @@ public abstract class BaseAddViewModel extends ViewModel {
     public abstract void queryIsExistingName(String inputText, byte parentIndex);
 
     public abstract void queryIsExistingName(String inputText, long parentId, byte parentIndex);
+
+    public abstract void queryIsExistingName(long id, String inputText, byte parentIndex);
+
+    public abstract void queryIsExistingName(long id, String inputText, long parentId);
+
+    public abstract void insert();
+
+    public abstract void update();
 }
