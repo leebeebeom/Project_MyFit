79989833672508884ceb2a7cb3f5110ced709ad8
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-20 00:05:09 +0900

    getPositiveClickListener 자식구현으로 수정

--- app/src/main/java/com/example/myfit/ui/dialog/delete/BaseDeleteDialog.java
+++ app/src/main/java/com/example/myfit/ui/dialog/delete/BaseDeleteDialog.java
@@ -2,7 +2,6 @@ package com.example.myfit.ui.dialog.delete;
 
 import android.app.Dialog;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,21 +12,18 @@ import androidx.navigation.NavBackStackEntry;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.example.myfit.data.repository.BaseRepository;
+import com.example.myfit.R;
 import com.example.myfit.ui.dialog.BaseDialog;
 import com.example.myfit.ui.main.MainGraphViewModel;
 
 import org.jetbrains.annotations.NotNull;
 
 public abstract class BaseDeleteDialog extends BaseDialog {
-    public static final String DELETE_SELECT_ITEMS = "delete select items";
-    private long[] selectedItemIds;
-    private NavBackStackEntry navBackStackEntry;
+    protected NavBackStackEntry navBackStackEntry;
 
     @Override
     public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        selectedItemIds = getSelectedItemIds();
         NavController navController = NavHostFragment.findNavController(this);
         navBackStackEntry = getBackStackEntry(navController);
         setBackStackEntryLive(navController);
@@ -51,24 +47,11 @@ public abstract class BaseDeleteDialog extends BaseDialog {
 
     @Override
     protected AlertDialog getAlertDialog() {
-        String selectedItemSize = String.valueOf(selectedItemIds.length);
-        return dialogBuilder.makeConfirmDialog(selectedItemSize + getMessage())
+        return dialogBuilder.makeConfirmDialog(getSelectedItemsSize() + getString(R.string.dialog_message_selected_item_delete))
                 .setPositiveClickListener(getPositiveClickListener())
                 .create();
     }
 
-    protected abstract String getMessage();
+    protected abstract String getSelectedItemsSize();
 
-    @Override
-    protected View.OnClickListener getPositiveClickListener() {
-        return v -> {
-            getRepository().deleteOrRestore(selectedItemIds, true);
-            navBackStackEntry.getSavedStateHandle().set(DELETE_SELECT_ITEMS, null);
-            dismiss();
-        };
-    }
-
-    protected abstract long[] getSelectedItemIds();
-
-    protected abstract BaseRepository getRepository();
 }
