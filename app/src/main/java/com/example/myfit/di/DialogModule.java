470dbdf57f1dbb01b4f95dcc3938aa9ddf7044d1
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-19 23:21:00 +0900

    다이얼로그 모듈 추가

---
+++ app/src/main/java/com/example/myfit/di/DialogModule.java
@@ -0,0 +1,39 @@
+package com.example.myfit.di;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+
+import com.example.myfit.ui.dialog.DialogBindingBuilder;
+import com.example.myfit.ui.dialog.DialogBuilder;
+
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import dagger.Module;
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.FragmentComponent;
+import dagger.hilt.android.qualifiers.ActivityContext;
+import dagger.hilt.android.qualifiers.ApplicationContext;
+
+@Module
+@InstallIn(FragmentComponent.class)
+public class DialogModule {
+    @NotNull
+    @Provides
+    public static DialogBuilder provideDialogBuilder(@ApplicationContext Context context) {
+        return new DialogBuilder(context);
+    }
+
+    @NotNull
+    @Contract("_ -> new")
+    @Provides
+    public static DialogBindingBuilder provideDialogBinding(LayoutInflater inflater) {
+        return new DialogBindingBuilder(inflater);
+    }
+
+    @Provides
+    public static LayoutInflater provideLayoutInflater(@ActivityContext Context context){
+        return LayoutInflater.from(context);
+    }
+}
