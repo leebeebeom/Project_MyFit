63919f8bb51dbdb88089b66b781659fd9828d44c
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-20 18:22:02 +0900

    뷰모델 수정

--- app/src/main/java/com/example/myfit/ui/dialog/eidttext/add/BaseAddViewModel.java
+++ app/src/main/java/com/example/myfit/ui/dialog/eidttext/add/BaseAddViewModel.java
@@ -1,7 +1,20 @@
 package com.example.myfit.ui.dialog.eidttext.add;
 
+import androidx.lifecycle.SavedStateHandle;
+
 import com.example.myfit.ui.dialog.eidttext.BaseEditTextViewModel;
 
+import javax.inject.Inject;
+
+import dagger.hilt.android.lifecycle.HiltViewModel;
+
+@HiltViewModel
 public abstract class BaseAddViewModel extends BaseEditTextViewModel {
+
+    @Inject
+    public BaseAddViewModel(SavedStateHandle savedStateHandle) {
+        super(savedStateHandle);
+    }
+
     public abstract void insert();
 }
--- app/src/main/java/com/example/myfit/ui/dialog/eidttext/edit/BaseEditViewModel.java
+++ app/src/main/java/com/example/myfit/ui/dialog/eidttext/edit/BaseEditViewModel.java
@@ -1,7 +1,20 @@
 package com.example.myfit.ui.dialog.eidttext.edit;
 
+import androidx.lifecycle.SavedStateHandle;
+
 import com.example.myfit.ui.dialog.eidttext.BaseEditTextViewModel;
 
+import javax.inject.Inject;
+
+import dagger.hilt.android.lifecycle.HiltViewModel;
+
+@HiltViewModel
 public abstract class BaseEditViewModel extends BaseEditTextViewModel {
+
+    @Inject
+    public BaseEditViewModel(SavedStateHandle savedStateHandle) {
+        super(savedStateHandle);
+    }
+
     public abstract void update();
 }
