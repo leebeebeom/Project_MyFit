177e2ad647e0a255ed7f9702b4847aeec75ce867
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-24 03:43:19 +0900

    키보드 관련 코드 삭제

--- app/src/main/java/com/example/myfit/util/CommonUtil.java
+++ app/src/main/java/com/example/myfit/util/CommonUtil.java
@@ -1,11 +1,10 @@
-package com.example.project_myfit.util;
+package com.example.myfit.util;
 
 import android.content.Context;
 import android.graphics.Rect;
 import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 
 import androidx.navigation.NavController;
 import androidx.navigation.NavDirections;
@@ -17,10 +16,6 @@ import com.google.android.material.textfield.TextInputEditText;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 public class CommonUtil {
     public static void editTextLosableFocus(@NotNull MotionEvent ev, View getCurrentFocus, Context context) {
         if (ev.getAction() == MotionEvent.ACTION_DOWN) {
@@ -29,39 +24,10 @@ public class CommonUtil {
                 getCurrentFocus.getGlobalVisibleRect(outRect);
                 if (!outRect.contains((int) ev.getRawX(), (int) ev.getRawY())) {
                     getCurrentFocus.clearFocus();
-                    keyBoardHide(context, getCurrentFocus);
+                    KeyBoardUtil.hideKeyBoard(getCurrentFocus);
                 }
             }
         }
-    }
-
-    public static boolean isKeyboardShowing(@NotNull View rootView) {
-        Rect r = new Rect();
-        rootView.getWindowVisibleDisplayFrame(r);
-
-        int screenHeight = rootView.getRootView().getHeight();
-        int keypadHeight = screenHeight - r.bottom;
-        return keypadHeight > screenHeight * 0.15;
-    }
-
-    public static void keyBoardShow(@NotNull Context context, @NotNull View view) {
-        InputMethodManager inputMethodManager = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
-        inputMethodManager.showSoftInputFromInputMethod(view.getWindowToken(), 0);
-    }
-
-    public static void keyBoardHide(@NotNull Context context, @NotNull View view) {
-        InputMethodManager inputMethodManager = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
-        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
-    }
-
-    public static void keyBoardToggle(@NotNull Context context) {
-        InputMethodManager inputMethodManager = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
-        inputMethodManager.toggleSoftInput(0, 0);
-    }
-
-    public static long createId(){
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss", Locale.getDefault());
-        return Long.parseLong(dateFormat.format(new Date(System.currentTimeMillis())));
     }
 
     public static void setBadgeCount(TabLayout.Tab tab, int count, TypedValue colorControl){
