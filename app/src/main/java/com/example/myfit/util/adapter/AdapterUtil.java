9162e749e3fccf1a50d70fb496cb16234a0ac268
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-23 10:55:48 +0900

    gridActionMode 삭제

--- app/src/main/java/com/example/myfit/util/adapter/AdapterUtil.java
+++ app/src/main/java/com/example/myfit/util/adapter/AdapterUtil.java
@@ -1,13 +1,11 @@
 package com.example.myfit.util.adapter;
 
-import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
 import com.example.myfit.R;
 import com.example.myfit.data.model.tuple.BaseTuple;
 import com.google.android.material.card.MaterialCardView;
-import com.google.android.material.checkbox.MaterialCheckBox;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -20,22 +18,12 @@ public class AdapterUtil<T extends BaseTuple> {
     public void listActionModeOn(@NotNull MaterialCardView cardView) {
         if (openAnimation == null)
             openAnimation = AnimationUtils.loadAnimation(cardView.getContext(), R.anim.item_list_slide_right);
-        if (!openAnimation.hasStarted())
-            cardView.setAnimation(openAnimation);
+        cardView.setAnimation(openAnimation);
     }
 
     public void listActionModeOff(@NotNull MaterialCardView cardView) {
         cardView.setAnimation(AnimationUtils.loadAnimation(cardView.getContext(), R.anim.item_list_slide_left));
         openAnimation = null;
-    }
-
-    public void gridActionModeOn(@NotNull MaterialCheckBox checkBox) {
-        checkBox.setVisibility(View.VISIBLE);
-    }
-
-    public void gridActionModeOff(@NotNull MaterialCheckBox checkBox) {
-        checkBox.jumpDrawablesToCurrentState();
-        checkBox.setVisibility(View.GONE);
     }
 
     public void itemMove(int from, int to, List<T> list) {
