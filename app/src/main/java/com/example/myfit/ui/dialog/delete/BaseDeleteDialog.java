8240b92c70ebfcfa1469baa6afeea7e3426c8869
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-23 12:45:39 +0900

    onCreateDialog, getBackStack 제거, getPositiveClickListener 구현

--- app/src/main/java/com/example/myfit/ui/dialog/delete/BaseDeleteDialog.java
+++ app/src/main/java/com/example/myfit/ui/dialog/delete/BaseDeleteDialog.java
@@ -1,17 +1,13 @@
 package com.example.myfit.ui.dialog.delete;
 
-import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
-import androidx.navigation.NavBackStackEntry;
 
 import com.example.myfit.R;
 import com.example.myfit.ui.dialog.BaseDialog;
-
-import org.jetbrains.annotations.NotNull;
 
 public abstract class BaseDeleteDialog extends BaseDialog {
 
@@ -19,16 +15,6 @@ public abstract class BaseDeleteDialog extends BaseDialog {
     public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setBackStackLive();
-    }
-
-    @NotNull
-    protected abstract NavBackStackEntry getBackStack();
-
-    @NonNull
-    @NotNull
-    @Override
-    public Dialog onCreateDialog(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
-        return getAlertDialog();
     }
 
     @Override
@@ -39,4 +25,15 @@ public abstract class BaseDeleteDialog extends BaseDialog {
     }
 
     protected abstract String getSelectedItemsSize();
+
+    @Override
+    protected View.OnClickListener getPositiveClickListener() {
+        return v -> {
+            task();
+            actionModeOff();
+            dismiss();
+        };
+    }
+
+    protected abstract void task();
 }
