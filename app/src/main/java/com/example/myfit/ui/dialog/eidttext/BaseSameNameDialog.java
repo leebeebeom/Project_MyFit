029abd312a883e03220aae394e284da02cfaccc1
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-23 12:50:21 +0900

    onCreateDialog삭제

--- app/src/main/java/com/example/myfit/ui/dialog/eidttext/BaseSameNameDialog.java
+++ app/src/main/java/com/example/myfit/ui/dialog/eidttext/BaseSameNameDialog.java
@@ -1,25 +1,12 @@
 package com.example.myfit.ui.dialog.eidttext;
 
-import android.app.Dialog;
-import android.os.Bundle;
 import android.view.View;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 
 import com.example.myfit.ui.dialog.BaseDialog;
 
-import org.jetbrains.annotations.NotNull;
-
 public abstract class BaseSameNameDialog extends BaseDialog {
-    @NonNull
-    @NotNull
-    @Override
-    public Dialog onCreateDialog(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
-        return getAlertDialog();
-    }
-
     @Override
     protected AlertDialog getAlertDialog() {
         return dialogBuilder.makeConfirmDialog(getMessage())
