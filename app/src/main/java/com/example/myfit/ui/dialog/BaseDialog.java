2e5deae3ce97412afcc783ef9776e88a3d371ab9
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-23 12:44:53 +0900

    onCreateDialog 추가, getBackStack 변경

--- app/src/main/java/com/example/myfit/ui/dialog/BaseDialog.java
+++ app/src/main/java/com/example/myfit/ui/dialog/BaseDialog.java
@@ -1,7 +1,11 @@
 package com.example.myfit.ui.dialog;
 
+import android.app.Dialog;
+import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 import androidx.hilt.navigation.HiltViewModelFactory;
@@ -13,6 +17,8 @@ import androidx.navigation.fragment.NavHostFragment;
 import com.example.myfit.R;
 import com.example.myfit.ui.main.MainGraphViewModel;
 
+import org.jetbrains.annotations.NotNull;
+
 import javax.inject.Inject;
 
 import dagger.hilt.android.AndroidEntryPoint;
@@ -22,6 +28,13 @@ public abstract class BaseDialog extends DialogFragment {
     public static final String ACTION_MODE_OFF = "action mode off";
     @Inject
     protected DialogBuilder dialogBuilder;
+
+    @NonNull
+    @NotNull
+    @Override
+    public Dialog onCreateDialog(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        return getAlertDialog();
+    }
 
     protected abstract AlertDialog getAlertDialog();
 
@@ -37,7 +50,11 @@ public abstract class BaseDialog extends DialogFragment {
         return getNavController().getBackStackEntry(R.id.nav_graph_main);
     }
 
-    protected abstract NavBackStackEntry getBackStack();
+    private NavBackStackEntry getBackStack() {
+        return getNavController().getBackStackEntry(getResId());
+    }
+
+    protected abstract int getResId();
 
     protected NavBackStackEntry getGraphBackStack() {
         NavController navController = getNavController();
