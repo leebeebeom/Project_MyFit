f4a285499c99ea3c0bd2afdbd66c7dbf19165d17
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-24 16:41:10 +0900

    sort 상수 관련 코드 수정

--- app/src/main/java/com/example/myfit/util/constant/Sort.java
+++ app/src/main/java/com/example/myfit/util/constant/Sort.java
@@ -1,15 +1,22 @@
 package com.example.myfit.util.constant;
 
 public enum Sort {
-    SORT("sort"), SORT_LIST("sort list"), SORT_MAIN("sort main");
+    SORT_CUSTOM(0),
+    SORT_CREATE(1),
+    SORT_CREATE_REVERSE(2),
+    SORT_BRAND(3),
+    SORT_BRAND_REVERSE(4),
+    SORT_NAME(5),
+    SORT_NAME_REVERSE(6),
+    SORT_DELETED(7);
 
-    private final String text;
+    private final int value;
 
-    Sort(String text) {
-        this.text = text;
+    Sort(int value) {
+        this.value = value;
     }
 
-    public String getText() {
-        return text;
+    public int getValue() {
+        return value;
     }
 }
