99d9c16d5a66891cd00942d2e638c92ed45d5a19
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-30 23:52:11 +0900

    변수 명 변경

--- app/src/main/java/com/example/myfit/data/repository/RecentSearchRepository.java
+++ app/src/main/java/com/example/myfit/data/repository/RecentSearchRepository.java
@@ -8,40 +8,37 @@ import com.example.myfit.data.repository.dao.RecentSearchDao;
 import java.util.List;
 
 import javax.inject.Inject;
-import javax.inject.Singleton;
 
-@Singleton
+import dagger.hilt.android.scopes.ViewModelScoped;
+
+@ViewModelScoped
 public class RecentSearchRepository {
-    private final RecentSearchDao recentSearchDao;
-    private int type;
-    private LiveData<List<RecentSearch>> recentSearchLive;
+    private final RecentSearchDao mRecentSearchDao;
+    private int mType;
+    private LiveData<List<RecentSearch>> mRecentSearchLive;
 
     @Inject
     public RecentSearchRepository(RecentSearchDao recentSearchDao) {
-        this.recentSearchDao = recentSearchDao;
+        this.mRecentSearchDao = recentSearchDao;
     }
 
     //to searchView, recycleBin search
     public LiveData<List<RecentSearch>> getLiveByType(int type) {
-        if (this.type != type) {
-            this.type = type;
-            recentSearchLive = recentSearchDao.getLiveByType(type);
-        }
-        return recentSearchLive;
+        return mRecentSearchDao.getLiveByType(type);
     }
 
     //from searchView, recycleBin search
     public void insert(String word, int type) {
-        new Thread(() -> recentSearchDao.insert(word, type)).start();
+        new Thread(() -> mRecentSearchDao.insert(word, type)).start();
     }
 
     //from searchView, recycleBin search
     public void delete(RecentSearch recentSearch) {
-        new Thread(() -> recentSearchDao.delete(recentSearch)).start();
+        new Thread(() -> mRecentSearchDao.delete(recentSearch)).start();
     }
 
     //from searchView, recycleBin search
     public void deleteAll() {
-        new Thread(recentSearchDao::deleteAll).start();
+        new Thread(mRecentSearchDao::deleteAll).start();
     }
 }
