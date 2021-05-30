d1995632722a76d2d0d0ce94a21d2a58faa86052
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-30 09:33:08 +0900

    컬럼 명 수정

--- app/src/main/java/com/example/myfit/data/repository/dao/AutoCompleteDao.java
+++ app/src/main/java/com/example/myfit/data/repository/dao/AutoCompleteDao.java
@@ -9,16 +9,16 @@ import java.util.List;
 @Dao
 public abstract class AutoCompleteDao {
     //to searchView
-    @Query("SELECT(SELECT name FROM Category WHERE isDeleted = 0) + " +
-            "(SELECT name FROM Folder WHERE isDeleted = 0 AND isParentDeleted = 0) + " +
-            "(SELECT name FROM Size WHERE isDeleted = 0 AND isParentDeleted = 0) + " +
-            "(SELECT brand FROM Size WHERE isDeleted = 0 AND isParentDeleted = 0)")
+    @Query("SELECT(SELECT name FROM Category WHERE deleted = 0) + " +
+            "(SELECT name FROM Folder WHERE deleted = 0 AND parentDeleted = 0) + " +
+            "(SELECT name FROM Size WHERE deleted = 0 AND parentDeleted = 0) + " +
+            "(SELECT brand FROM Size WHERE deleted = 0 AND parentDeleted = 0)")
     public abstract LiveData<List<String>> getAutoCompleteWordsLive();
 
     //to recycleBin search
-    @Query("SELECT(SELECT name FROM Category WHERE isDeleted = 1) + " +
-            "(SELECT name FROM Folder WHERE isDeleted = 1) + " +
-            "(SELECT name FROM Size WHERE isDeleted = 1) + " +
-            "(SELECT brand FROM Size WHERE isDeleted = 1)")
+    @Query("SELECT(SELECT name FROM Category WHERE deleted = 1) + " +
+            "(SELECT name FROM Folder WHERE deleted = 1) + " +
+            "(SELECT name FROM Size WHERE deleted = 1) + " +
+            "(SELECT brand FROM Size WHERE deleted = 1)")
     public abstract LiveData<List<String>> getDeletedAutoCompleteWordsLive();
 }
