fdfc0f73490f50e000c9630271e1127f2d0f847b
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-30 09:52:13 +0900

    제네릭 생성, updateTuples 메소드 생성

--- app/src/main/java/com/example/myfit/data/repository/BaseRepository.java
+++ app/src/main/java/com/example/myfit/data/repository/BaseRepository.java
@@ -2,11 +2,14 @@ package com.example.myfit.data.repository;
 
 import android.content.SharedPreferences;
 
+import com.example.myfit.data.model.tuple.BaseTuple;
 import com.example.myfit.util.constant.Sort;
+
+import java.util.List;
 
 import static com.example.myfit.di.DataModule.SORT;
 
-public abstract class BaseRepository {
+public abstract class BaseRepository<T extends BaseTuple> {
     public abstract void deleteOrRestore(long[] ids, boolean isDeleted);
 
     public void changeSort(Sort sort) {
@@ -23,4 +26,6 @@ public abstract class BaseRepository {
     }
 
     protected abstract SharedPreferences getPreference();
+
+    public abstract void updateTuples(List<T> itemList);
 }
