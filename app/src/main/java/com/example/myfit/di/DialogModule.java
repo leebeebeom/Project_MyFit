00fdaec19f41572ce1e47d921dbf7dd1ffd3e741
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-05-31 00:23:16 +0900

    패키지 변경,

--- app/src/main/java/com/example/myfit/di/DialogModule.java
+++ app/src/main/java/com/example/myfit/di/DialogModule.java
@@ -3,17 +3,14 @@ package com.example.myfit.di;
 import android.content.Context;
 
 import androidx.appcompat.app.AlertDialog;
-import androidx.navigation.NavController;
 
 import com.example.myfit.R;
-import com.example.myfit.ui.dialog.tree.TreeNodeProvider;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import dagger.Module;
 import dagger.Provides;
 import dagger.hilt.InstallIn;
 import dagger.hilt.android.components.FragmentComponent;
-import dagger.hilt.android.qualifiers.ActivityContext;
 import dagger.hilt.android.qualifiers.ApplicationContext;
 
 @Module
@@ -26,10 +23,5 @@ public class DialogModule {
                 .setNegativeButton(R.string.dialog_cancel, null)
                 .setPositiveButton(R.string.dialog_confirm, null)
                 .create();
-    }
-
-    @Provides
-    public static TreeNodeProvider provideTreeNodeProvider(@ActivityContext Context context, NavController navController){
-        return new TreeNodeProvider(context, navController);
     }
 }
