2be5ecd63b7f26934452fb772c1702a968fa5eeb
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-01 23:22:01 +0900

    baseInfo 삭제

---
+++ app/src/main/java/com/example/myfit/data/model/ModelFactory.java
@@ -0,0 +1,45 @@
+package com.example.myfit.data.model;
+
+import com.example.myfit.data.model.model.Category;
+import com.example.myfit.data.model.model.Folder;
+import com.example.myfit.data.model.model.RecentSearch;
+import com.example.myfit.data.model.model.Size;
+
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class ModelFactory {
+    @NotNull
+    @Contract("_, _, _ -> new")
+    public static Category makeCategory(String categoryName, int parentIndex, int orderNumber) {
+        return new Category(parentIndex, orderNumber, categoryName);
+    }
+
+    @NotNull
+    @Contract("_, _, _, _ -> new")
+    public static Folder makeFolder(String folderName, long parentId, int parentIndex, int orderNumber) {
+        return new Folder(parentIndex, orderNumber, folderName, parentId);
+    }
+
+    @NotNull
+    @Contract("_, _, _ -> new")
+    public static Size makeSize(long parentId, int parentIndex, int orderNumber) {
+        return new Size(parentIndex, orderNumber, parentId);
+    }
+
+    @NotNull
+    public static RecentSearch makeRecentSearch(String word, int type) {
+        String date = getRecentSearchDate();
+        return new RecentSearch(word, date, type);
+    }
+
+    @NotNull
+    private static String getRecentSearchDate() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM월 dd일", Locale.getDefault());
+        return dateFormat.format(new Date(System.currentTimeMillis()));
+    }
+}
