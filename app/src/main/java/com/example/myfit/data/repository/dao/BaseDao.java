a20f82d8f7f501b349f2ac85746a4d5c2cce2dfe
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-02 00:53:26 +0900

    코드 정리

--- app/src/main/java/com/example/myfit/data/repository/dao/BaseDao.java
+++ app/src/main/java/com/example/myfit/data/repository/dao/BaseDao.java
@@ -1,7 +1,5 @@
 package com.example.myfit.data.repository.dao;
 
-import android.util.Log;
-
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Transformations;
 import androidx.room.Dao;
@@ -9,12 +7,12 @@ import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 import androidx.room.Update;
 
-import com.example.myfit.data.tuple.tuple.CategoryTuple;
 import com.example.myfit.data.model.model.Folder;
-import com.example.myfit.data.model.model.size.Size;
+import com.example.myfit.data.model.model.Size;
 import com.example.myfit.data.tuple.BaseTuple;
 import com.example.myfit.data.tuple.DeletedTuple;
 import com.example.myfit.data.tuple.ParentDeletedTuple;
+import com.example.myfit.data.tuple.tuple.CategoryTuple;
 import com.example.myfit.util.SortUtil;
 import com.example.myfit.util.constant.Sort;
 
@@ -32,14 +30,14 @@ import java.util.Locale;
 @Dao
 public abstract class BaseDao<T extends BaseTuple> {
     @NotNull
-    public LiveData<int[]> getContentSizesLive(LiveData<List<T>> tuplesLive, boolean parentDeleted) {
+    protected LiveData<int[]> getContentSizesLive(LiveData<List<T>> tuplesLive, boolean parentDeleted) {
         return Transformations.switchMap(tuplesLive, tuples -> {
-            long[] itemIds = getItemIds(tuples);
-            return getContentsSizesLiveByParentIds(itemIds, parentDeleted);
+            long[] itemIds = getTupleIds(tuples);
+            return getContentSizesLiveByParentIds(itemIds, parentDeleted);
         });
     }
 
-    protected long[] getItemIds(@NotNull List<T> tuples) {
+    protected long[] getTupleIds(@NotNull List<T> tuples) {
         return tuples.stream()
                 .mapToLong(BaseTuple::getId)
                 .toArray();
@@ -47,54 +45,38 @@ public abstract class BaseDao<T extends BaseTuple> {
 
     @Query("SELECT((SELECT COUNT(parentId) FROM Folder WHERE parentId IN (:parentIds) AND deleted = 0 AND parentDeleted = :parentDeleted) + " +
             "(SELECT COUNT(parentId) FROM Size WHERE parentId IN(:parentIds) AND deleted = 0 AND parentDeleted = :parentDeleted))")
-    protected abstract LiveData<int[]> getContentsSizesLiveByParentIds(long[] parentIds, boolean parentDeleted);
+    protected abstract LiveData<int[]> getContentSizesLiveByParentIds(long[] parentIds, boolean parentDeleted);
 
-
-    @NotNull
     protected <R extends CategoryTuple> LiveData<List<List<R>>> getClassifiedTuplesLive(LiveData<List<R>> tuplesLive,
-                                                                                        LiveData<int[]> contentsSizesLive) {
-        return Transformations.map(contentsSizesLive, contentsSizes -> {
+                                                                                        LiveData<int[]> contentSizesLive) {
+        return Transformations.map(contentSizesLive, contentSizes -> {
             List<R> tuples = tuplesLive.getValue();
-            setContentsSize(tuples, contentsSizes);
+            setContentSize(tuples, contentSizes);
             return getClassifiedTuplesByParentIndex(tuples);
         });
     }
 
-    protected <R extends CategoryTuple> void setContentsSize(List<R> items, int[] contentsSizes) {
-        try {
-            int itemsSize = items.size();
+    protected <R extends CategoryTuple> void setContentSize(List<R> tuples, int[] contentSizes) {
+        if (tuples != null) {
+            int itemsSize = tuples.size();
             for (int i = 0; i < itemsSize; i++)
-                items.get(i).setContentsSize(contentsSizes[i]);
-        } catch (NullPointerException | ArrayIndexOutOfBoundsException e) {
-            logE(e);
+                tuples.get(i).setContentSize(contentSizes[i]);
         }
     }
 
-    protected <R extends CategoryTuple> void orderTuples(Sort sort, List<R> tuples) {
-        try {
-            SortUtil.orderCategoryFolderTuples(sort, tuples);
-        } catch (NullPointerException e) {
-            logE(e);
-        }
-    }
-
-    @NotNull
     protected <R extends CategoryTuple> List<List<R>> getClassifiedTuplesByParentIndex(List<R> tuples) {
-        List<LinkedList<R>> classifiedLinkedList = Arrays.asList(new LinkedList<>(), new LinkedList<>(), new LinkedList<>(), new LinkedList<>());
-        List<List<R>> classifiedList = Arrays.asList(new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+        if (tuples != null) {
+            List<LinkedList<R>> classifiedLinkedList = Arrays.asList(new LinkedList<>(), new LinkedList<>(), new LinkedList<>(), new LinkedList<>());
+            List<List<R>> classifiedList = Arrays.asList(new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
 
-        try {
             tuples.forEach(tuple -> classifiedLinkedList.get(tuple.getParentIndex()).add(tuple));
             for (int i = 0; i < 4; i++) classifiedList.get(i).addAll(classifiedLinkedList.get(i));
-        } catch (NullPointerException e) {
-            logE(e);
-        }
-
-        return classifiedList;
+            return classifiedList;
+        } else return null;
     }
 
-    protected void logE(Exception e) {
-        Log.e("에러", "Exception " + e.getMessage(), e);
+    protected <R extends CategoryTuple> void sortTuples(Sort sort, List<R> tuples) {
+        if (tuples != null) SortUtil.sortCategoryFolderTuples(sort, tuples);
     }
 
     protected void setDeletedTuples(DeletedTuple[] deletedTuples, boolean deleted) {
@@ -135,7 +117,7 @@ public abstract class BaseDao<T extends BaseTuple> {
     }
 
     private void addAllChildFolderParentDeletedTuples(long[] parentIds, @NotNull LinkedList<ParentDeletedTuple> allParentDeletedTuples) {
-        List<ParentDeletedTuple> childFolderParentDeletedTuples = this.getFolderParentDeletedTuplesByParentIds(parentIds);
+        List<ParentDeletedTuple> childFolderParentDeletedTuples = getFolderParentDeletedTuplesByParentIds(parentIds);
         if (!childFolderParentDeletedTuples.isEmpty()) {
             allParentDeletedTuples.addAll(childFolderParentDeletedTuples);
             long[] childFolderIds = getParentDeletedTupleIds(childFolderParentDeletedTuples);
@@ -143,14 +125,14 @@ public abstract class BaseDao<T extends BaseTuple> {
         }
     }
 
+    @Query("SELECT id, deleted ,parentDeleted FROM Folder WHERE parentId IN (:parentIds)")
+    protected abstract List<ParentDeletedTuple> getFolderParentDeletedTuplesByParentIds(long[] parentIds);
+
     private long[] getParentDeletedTupleIds(@NotNull List<ParentDeletedTuple> childFolderParentDeletedTuples) {
         return childFolderParentDeletedTuples.stream()
                 .filter(parentDeletedTuple -> !parentDeletedTuple.isDeleted())
                 .mapToLong(ParentDeletedTuple::getId).toArray();
     }
-
-    @Query("SELECT id, deleted ,parentDeleted FROM Folder WHERE parentId IN (:parentIds)")
-    protected abstract List<ParentDeletedTuple> getFolderParentDeletedTuplesByParentIds(long[] parentIds);
 
     private void addAllChildSizeParentDeletedTuples(long[] parentIds, @NotNull LinkedList<ParentDeletedTuple> allParentDeletedTuples) {
         allParentDeletedTuples.addAll(getSizeParentDeletedTuplesByParentIds(parentIds));
@@ -171,9 +153,9 @@ public abstract class BaseDao<T extends BaseTuple> {
 
     @Query("SELECT((SELECT COUNT(parentId) FROM Folder WHERE parentId IN (:parentIds) AND deleted = 0 AND parentDeleted = 0) + " +
             "(SELECT COUNT(parentId) FROM Size WHERE parentId IN(:parentIds) AND deleted =0 AND parentDeleted = 0))")
-    protected abstract int[] getContentsSizesByParentIds(long[] parentIds);
+    protected abstract int[] getContentSizesByParentIds(long[] parentIds);
 
     @Query("SELECT((SELECT COUNT(parentId) FROM Folder WHERE parentId = :parentId AND deleted = 0 AND parentDeleted = 0) + " +
             "(SELECT COUNT(parentId) FROM Size WHERE parentId = :parentId AND deleted =0 AND parentDeleted = 0))")
-    protected abstract int getContentsSizeByParentId(long parentId);
+    protected abstract int getContentSizeByParentId(long parentId);
 }
