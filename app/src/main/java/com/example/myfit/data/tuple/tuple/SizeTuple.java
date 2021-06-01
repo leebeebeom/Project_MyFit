03b938e4f300906eeb531c89940e06f23b702149
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-01 23:24:32 +0900

    롬복 추가

--- app/src/main/java/com/example/myfit/data/tuple/tuple/SizeTuple.java
+++ app/src/main/java/com/example/myfit/data/tuple/tuple/SizeTuple.java
@@ -2,49 +2,14 @@ package com.example.myfit.data.tuple.tuple;
 
 import com.example.myfit.data.tuple.BaseTuple;
 
-import java.util.Objects;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@EqualsAndHashCode
 public class SizeTuple extends BaseTuple {
     private String brand, imageUri;
     private boolean favorite;
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public boolean isFavorite() {
-        return favorite;
-    }
-
-    public void setFavorite(boolean favorite) {
-        this.favorite = favorite;
-    }
-
-    public String getImageUri() {
-        return imageUri;
-    }
-
-    public void setImageUri(String imageUri) {
-        this.imageUri = imageUri;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof SizeTuple)) return false;
-        if (!super.equals(o)) return false;
-        SizeTuple sizeTuple = (SizeTuple) o;
-        return isFavorite() == sizeTuple.isFavorite() &&
-                getBrand().equals(sizeTuple.getBrand()) &&
-                Objects.equals(getImageUri(), sizeTuple.getImageUri());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), getBrand(), getImageUri(), isFavorite());
-    }
 }
