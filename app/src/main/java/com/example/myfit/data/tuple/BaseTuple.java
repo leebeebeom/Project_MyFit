086af5da695c277a7a5a16b704a3e1b249a96ea5
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-02 12:10:57 +0900

    생성자 접근제한자 변경

--- app/src/main/java/com/example/myfit/data/tuple/BaseTuple.java
+++ app/src/main/java/com/example/myfit/data/tuple/BaseTuple.java
@@ -1,12 +1,15 @@
 package com.example.myfit.data.tuple;
 
+import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @EqualsAndHashCode
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BaseTuple {
     private long id;
     private int parentIndex, sortNumber;
