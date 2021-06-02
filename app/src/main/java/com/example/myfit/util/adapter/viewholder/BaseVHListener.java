afe4f50ddae5d72eb54b858352fcfd4697271f0d
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-02 16:12:00 +0900

    인수 변경

--- app/src/main/java/com/example/myfit/util/adapter/viewholder/BaseVHListener.java
+++ app/src/main/java/com/example/myfit/util/adapter/viewholder/BaseVHListener.java
@@ -2,14 +2,14 @@ package com.example.myfit.util.adapter.viewholder;
 
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.myfit.data.model.tuple.BaseTuple;
+import com.example.myfit.data.tuple.BaseTuple;
 
 public interface BaseVHListener {
     void itemViewClick(BaseTuple tuple);
 
-    void itemViewLongClick(int position, int parentIndex);
+    void itemViewLongClick(int position, BaseTuple tuple);
 
-    void dragStart(RecyclerView.ViewHolder viewHolder, int parentIndex);
+    void dragStart(RecyclerView.ViewHolder viewHolder, BaseTuple tuple);
 
-    void dragStop(int parentIndex);
+    void dragStop(BaseTuple tuple);
 }
