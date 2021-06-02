086fdf55e53ffde230710d417de2d6078af5614e
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-02 16:11:46 +0900

    코드 수정

--- app/src/main/java/com/example/myfit/util/adapter/viewholder/BaseVH.java
+++ app/src/main/java/com/example/myfit/util/adapter/viewholder/BaseVH.java
@@ -8,35 +8,35 @@ import androidx.appcompat.widget.AppCompatImageView;
 import androidx.databinding.ViewDataBinding;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.myfit.data.model.size.SizeTuple;
-import com.example.myfit.data.model.tuple.BaseTuple;
+import com.example.myfit.data.tuple.BaseTuple;
+import com.example.myfit.data.tuple.tuple.SizeTuple;
 import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.checkbox.MaterialCheckBox;
 
 import java.util.Set;
 
-import static com.example.myfit.util.actionmodecallback.BaseActionModeCallBack.sActionMode;
+import static com.example.myfit.util.ActionModeImpl.sActionMode;
 
 public abstract class BaseVH<T extends BaseTuple, L extends BaseVHListener> extends RecyclerView.ViewHolder {
-    public static boolean sIsDragging = false;
+    public static boolean sDragging = false;
     private final L mListener;
-    private T mTuple;
+    protected T mTuple;
 
     @SuppressLint("ClickableViewAccessibility")
     public BaseVH(ViewDataBinding binding, L listener, Set<T> selectedItems) {
         super(binding.getRoot());
         this.mListener = listener;
 
-        setItemViewClickListener(listener, selectedItems);
+        setItemViewClickListener(selectedItems);
 
         itemView.setOnLongClickListener(v -> {
-            listener.itemViewLongClick(getLayoutPosition(), mTuple.getParentIndex());
+            listener.itemViewLongClick(getLayoutPosition(), mTuple);
             return false;
         });
 
         if (getDragHandleIcon() != null) {
             getDragHandleIcon().setOnTouchListener((v, event) -> {
-                if (event.getAction() == MotionEvent.ACTION_DOWN && !sIsDragging)
+                if (event.getAction() == MotionEvent.ACTION_DOWN && !sDragging)
                     dragStart();
                 return false;
             });
@@ -50,20 +50,15 @@ public abstract class BaseVH<T extends BaseTuple, L extends BaseVHListener> exte
 
     protected abstract void bind(T tuple);
 
-    private void setItemViewClickListener(L listener, Set<T> selectedItems) {
+    private void setItemViewClickListener(Set<T> selectedItems) {
         itemView.setOnClickListener(
                 v -> {
                     if (sActionMode != null) {
                         if (selectedItems.contains(mTuple))
                             removeItem(selectedItems);
                         else addItem(selectedItems);
-                    } else listener.itemViewClick(mTuple);
+                    } else mListener.itemViewClick(mTuple);
                 });
-    }
-
-    private void addItem(Set<T> selectedItemSet) {
-        getCheckBox().setChecked(true);
-        selectedItemSet.add(mTuple);
     }
 
     private void removeItem(Set<T> selectedItemSet) {
@@ -71,8 +66,9 @@ public abstract class BaseVH<T extends BaseTuple, L extends BaseVHListener> exte
         selectedItemSet.remove(mTuple);
     }
 
-    protected T getTuple() {
-        return mTuple;
+    private void addItem(Set<T> selectedItemSet) {
+        getCheckBox().setChecked(true);
+        selectedItemSet.add(mTuple);
     }
 
     public abstract MaterialCheckBox getCheckBox();
@@ -80,16 +76,16 @@ public abstract class BaseVH<T extends BaseTuple, L extends BaseVHListener> exte
     public abstract AppCompatImageView getDragHandleIcon();
 
     private void dragStart() {
-        sIsDragging = true;
-        mListener.dragStart(this, getTuple().getParentIndex());
+        sDragging = true;
+        mListener.dragStart(this, mTuple);
         setDraggingView();
     }
 
     protected abstract void setDraggingView();
 
     public void dragStop() {
-        sIsDragging = false;
-        mListener.dragStop(getTuple().getParentIndex());
+        sDragging = false;
+        mListener.dragStop(mTuple);
         setDropView();
     }
 
