89b829a5f09abefd9ef672e8b3422d438b61cf6b
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-05 02:14:39 +0900

    include 추가

--- app/src/main/java/com/example/myfit/util/adapter/viewholder/CategoryVH.java
+++ app/src/main/java/com/example/myfit/util/adapter/viewholder/CategoryVH.java
@@ -3,7 +3,7 @@ package com.example.myfit.util.adapter.viewholder;
 import android.view.View;
 
 import com.example.myfit.data.tuple.tuple.CategoryTuple;
-import com.example.myfit.databinding.ItemCategoryBinding;
+import com.example.myfit.databinding.ItemVhCategoryBinding;
 import com.example.myfit.util.adapter.BaseAdapter;
 import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.checkbox.MaterialCheckBox;
@@ -13,9 +13,9 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Set;
 
 public class CategoryVH extends BaseVH<CategoryTuple, BaseVHListener> {
-    private final ItemCategoryBinding mBinding;
+    private final ItemVhCategoryBinding mBinding;
 
-    public CategoryVH(@NotNull ItemCategoryBinding binding, BaseVHListener listener, Set<CategoryTuple> selectedItems) {
+    public CategoryVH(@NotNull ItemVhCategoryBinding binding, BaseVHListener listener, Set<CategoryTuple> selectedItems) {
         super(binding, listener, selectedItems);
         this.mBinding = binding;
     }
@@ -34,21 +34,21 @@ public class CategoryVH extends BaseVH<CategoryTuple, BaseVHListener> {
     protected void setDraggingView() {
         setItemViewTranslationZ(itemView, 10);
         mBinding.cb.setVisibility(View.INVISIBLE);
-        mBinding.tvCategoryName.setAlpha(0.5f);
-        mBinding.layoutContentsSize.setAlpha(0.5f);
+        mBinding.cardView.tvName.setAlpha(0.5f);
+        mBinding.cardView.tvContentSize.setAlpha(0.5f);
     }
 
     @Override
     protected void setDropView() {
         setItemViewTranslationZ(itemView, 10);
         mBinding.cb.setVisibility(View.VISIBLE);
-        mBinding.tvCategoryName.setAlpha(0.8f);
-        mBinding.layoutContentsSize.setAlpha(0.8f);
+        mBinding.cardView.tvName.setAlpha(0.8f);
+        mBinding.cardView.tvContentSize.setAlpha(0.8f);
     }
 
     @Override
     public MaterialCardView getCardView() {
-        return mBinding.cardView;
+        return mBinding.cardView.cardView;
     }
 
     @Override
--- app/src/main/java/com/example/myfit/util/adapter/viewholder/FolderListVH.java
+++ app/src/main/java/com/example/myfit/util/adapter/viewholder/FolderListVH.java
@@ -1,7 +1,7 @@
 package com.example.myfit.util.adapter.viewholder;
 
 import com.example.myfit.data.tuple.tuple.FolderTuple;
-import com.example.myfit.databinding.ItemFolderListBinding;
+import com.example.myfit.databinding.ItemVhFolderListBinding;
 import com.example.myfit.util.adapter.BaseAdapter;
 import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.checkbox.MaterialCheckBox;
@@ -9,9 +9,9 @@ import com.google.android.material.checkbox.MaterialCheckBox;
 import java.util.Set;
 
 public class FolderListVH extends BaseVH<FolderTuple, BaseVHListener> {
-    private final ItemFolderListBinding mBinding;
+    private final ItemVhFolderListBinding mBinding;
 
-    public FolderListVH(ItemFolderListBinding binding, BaseVHListener listener, Set<FolderTuple> selectedItems) {
+    public FolderListVH(ItemVhFolderListBinding binding, BaseVHListener listener, Set<FolderTuple> selectedItems) {
         super(binding, listener, selectedItems);
         this.mBinding = binding;
     }
@@ -35,7 +35,7 @@ public class FolderListVH extends BaseVH<FolderTuple, BaseVHListener> {
 
     @Override
     public MaterialCardView getCardView() {
-        return mBinding.cardView;
+        return mBinding.cardView.cardView;
     }
 
     @Override
