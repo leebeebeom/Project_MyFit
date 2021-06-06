83c9df120c88e6d7f02a67d80ac1a113d5abf396
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-07 01:01:27 +0900

    다이얼로그 프로바이드 수정

--- app/src/main/java/com/example/myfit/di/BindingModule.java
+++ app/src/main/java/com/example/myfit/di/BindingModule.java
@@ -1,7 +1,9 @@
 package com.example.myfit.di;
 
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 
+import com.example.myfit.R;
 import com.example.myfit.databinding.ItemDialogEditTextBinding;
 import com.example.myfit.databinding.LayoutDialogSortBinding;
 import com.example.myfit.databinding.LayoutDialogTreeBinding;
@@ -18,7 +20,23 @@ import dagger.hilt.android.components.FragmentComponent;
 public class BindingModule {
     @Provides
     public static ItemDialogEditTextBinding provideDialogBiding(LayoutInflater inflater) {
-        return ItemDialogEditTextBinding.inflate(inflater);
+        ItemDialogEditTextBinding binding = ItemDialogEditTextBinding.inflate(inflater);
+        binding.et.requestFocus();
+        showErrorIfMoreThan30Characters(binding);
+        return binding;
+    }
+
+    private static void showErrorIfMoreThan30Characters(ItemDialogEditTextBinding binding) {
+        binding.et.setOnKeyListener((v, keyCode, event) -> {
+            if (isTextLength30(binding) && keyCode != KeyEvent.KEYCODE_DEL && keyCode != KeyEvent.FLAG_EDITOR_ACTION)
+                binding.layout.setError(v.getContext().getString(R.string.dialog_et_max_length));
+            else binding.layout.setErrorEnabled(false);
+            return false;
+        });
+    }
+
+    private static boolean isTextLength30(ItemDialogEditTextBinding binding) {
+        return String.valueOf(binding.et.getText()).length() == 30;
     }
 
     @Provides
@@ -27,7 +45,7 @@ public class BindingModule {
     }
 
     @Provides
-    public static TitleActionModeBinding provideActionModeTitleBinding(LayoutInflater inflater){
+    public static TitleActionModeBinding provideActionModeTitleBinding(LayoutInflater inflater) {
         return TitleActionModeBinding.inflate(inflater);
     }
 
