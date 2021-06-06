58bac2d5fcb8ef16350745ea7f525496c5cd2903
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-07 01:20:48 +0900

    생성

---
+++ app/src/main/java/com/example/myfit/util/bindingadapter/ActivityBindingAdapter.java
@@ -0,0 +1,86 @@
+package com.example.myfit.util.bindingadapter;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.databinding.BindingAdapter;
+
+import com.example.myfit.R;
+import com.example.myfit.ui.main.MainActivity;
+import com.example.myfit.ui.main.MainActivityViewModel;
+import com.example.myfit.ui.search.adapter.AutoCompleteAdapter;
+import com.example.myfit.util.KeyBoardUtil;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.MaterialAutoCompleteTextView;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public class ActivityBindingAdapter {
+    @BindingAdapter("setContentView")
+    public static void setContentView(CoordinatorLayout root, MainActivity activity) {
+        activity.setContentView(root);
+    }
+
+    @BindingAdapter("setSupportActionBar")
+    public static void setSupportActionBar(MaterialToolbar toolbar, MainActivity activity) {
+        activity.setSupportActionBar(toolbar);
+    }
+
+    @BindingAdapter("removeBottomNavShadow")
+    public static void removeBottomNavShadow(BottomNavigationView bottomNavigationView, String dummy) {
+        bottomNavigationView.setBackgroundTintList(null);
+    }
+
+    @BindingAdapter("alignBottomNavIcons")
+    public static void alignBottomNavIcons(BottomNavigationView bottomNavigationView, String dummy) {
+        bottomNavigationView.getMenu().getItem(2).setVisible(false);
+    }
+
+    @BindingAdapter("setVisibility")
+    public static void setVisibility(TextView title, int destinationId) {
+        title.setVisibility(destinationId == R.id.mainFragment ? View.VISIBLE : View.GONE);
+    }
+
+    @BindingAdapter("change")
+    public static void change(FloatingActionButton fab, int destinationId) {
+        if (destinationId == R.id.mainFragment)
+            fabChange(fab, R.drawable.icon_search);
+        else if (destinationId == R.id.listFragment)
+            fabChange(fab, R.drawable.icon_add);
+        else if (destinationId == R.id.sizeFragment)
+            fabChange(fab, R.drawable.icon_save);
+    }
+
+    private static void fabChange(FloatingActionButton fab, int resId) {
+        fab.hide();
+        fab.setImageResource(resId);
+        fab.show();
+    }
+
+    @BindingAdapter("setVisibility")
+    public static void setVisibility(TextInputLayout layout, int destinationId) {
+        if (layout.getVisibility() == View.GONE && (destinationId == R.id.searchFragment || destinationId == R.id.recycleBinSearch))
+            layout.setVisibility(View.VISIBLE);
+        else if (layout.getVisibility() == View.VISIBLE && destinationId != R.id.searchFragment && destinationId != R.id.recycleBinSearch)
+            layout.setVisibility(View.GONE);
+    }
+
+    @BindingAdapter("setAutoCompleteItems")
+    public static void setAutoCompleteItems(MaterialAutoCompleteTextView autoCompleteTextView, Set<String> data) {
+        if (autoCompleteTextView.getAdapter() instanceof AutoCompleteAdapter && data != null) {
+            ArrayList<String> listData = new ArrayList<>(data);
+            ((AutoCompleteAdapter) autoCompleteTextView.getAdapter()).setItem(listData);
+        }
+    }
+
+    @BindingAdapter("keyboardShowingListener")
+    public static void keyboardShowingListener(CoordinatorLayout layout, MainActivityViewModel model) {
+        layout.getViewTreeObserver().addOnGlobalLayoutListener(() ->
+                model.getKeyboardShowingLive().setValue(KeyBoardUtil.isKeyboardShowing(layout)));
+    }
+}
