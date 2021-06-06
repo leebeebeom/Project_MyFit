f2007e4028d9fa49989bc20e82e72cecdf57abfa
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-07 04:26:48 +0900

    코드 정리

--- app/src/main/java/com/example/myfit/util/adapter/AdapterUtil.java
+++ app/src/main/java/com/example/myfit/util/adapter/AdapterUtil.java
@@ -1,15 +1,7 @@
 package com.example.myfit.util.adapter;
 
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-
-import com.example.myfit.R;
 import com.example.myfit.data.tuple.BaseTuple;
-import com.google.android.material.card.MaterialCardView;
 
-import org.jetbrains.annotations.NotNull;
-
-import java.util.Collections;
 import java.util.List;
 
 import lombok.AccessLevel;
@@ -22,12 +14,10 @@ public class AdapterUtil {
             for (int i = from; i < to; i++) {
                 if (list.get(i++).getId() != -1)
                     swapOrderNumber(list, i, i++);
-                Collections.swap(list, i, i++);
             }
         } else {//up
             for (int i = from; i > to; i--) {
                 swapOrderNumber(list, i, i--);
-                Collections.swap(list, i, i--);
             }
         }
     }
@@ -35,9 +25,7 @@ public class AdapterUtil {
     private static <T extends BaseTuple> void swapOrderNumber(List<T> list, int i, int i2) {
         BaseTuple baseTuple1 = list.get(i);
         BaseTuple baseTuple2 = list.get(i2);
-        int order1 = baseTuple1.getSortNumber();
-        int order2 = baseTuple2.getSortNumber();
-        baseTuple1.setSortNumber(order2);
-        baseTuple2.setSortNumber(order1);
+        baseTuple1.setSortNumber(baseTuple2.getSortNumber());
+        baseTuple2.setSortNumber(baseTuple1.getSortNumber());
     }
 }
