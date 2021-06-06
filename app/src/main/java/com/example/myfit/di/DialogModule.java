5f4670faa532377bd785d490d7ad7867c27f3cf6
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-07 01:01:44 +0900

    트리노드 프로바이더 추가

--- app/src/main/java/com/example/myfit/di/DialogModule.java
+++ app/src/main/java/com/example/myfit/di/DialogModule.java
@@ -3,14 +3,17 @@ package com.example.myfit.di;
 import android.content.Context;
 
 import androidx.appcompat.app.AlertDialog;
+import androidx.navigation.NavController;
 
 import com.example.myfit.R;
+import com.example.myfit.ui.dialog.tree.TreeNodeProvider;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import dagger.Module;
 import dagger.Provides;
 import dagger.hilt.InstallIn;
 import dagger.hilt.android.components.FragmentComponent;
+import dagger.hilt.android.qualifiers.ActivityContext;
 import dagger.hilt.android.qualifiers.ApplicationContext;
 
 @Module
@@ -23,5 +26,10 @@ public class DialogModule {
                 .setNegativeButton(R.string.dialog_cancel, null)
                 .setPositiveButton(R.string.dialog_confirm, null)
                 .create();
+    }
+
+    @Provides
+    public static TreeNodeProvider provideTreeNodeProvider(@ActivityContext Context context, NavController navController) {
+        return new TreeNodeProvider(context, navController);
     }
 }
