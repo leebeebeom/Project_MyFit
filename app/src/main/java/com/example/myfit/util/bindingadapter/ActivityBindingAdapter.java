8b921bc5c5abdffffb1e4a5d80b33588544202a1
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-08 00:36:43 +0900

    액티비티 로직 삭제

--- app/src/main/java/com/example/myfit/util/bindingadapter/ActivityBindingAdapter.java
+++ app/src/main/java/com/example/myfit/util/bindingadapter/ActivityBindingAdapter.java
@@ -21,41 +21,6 @@ import java.util.ArrayList;
 import java.util.Set;
 
 public class ActivityBindingAdapter {
-    @BindingAdapter("setContentView")
-    public static void setContentView(CoordinatorLayout root, MainActivity activity) {
-        activity.setContentView(root);
-    }
-
-    @BindingAdapter("setSupportActionBar")
-    public static void setSupportActionBar(MaterialToolbar toolbar, MainActivity activity) {
-        activity.setSupportActionBar(toolbar);
-    }
-
-    @BindingAdapter("removeBottomNavShadow")
-    public static void removeBottomNavShadow(BottomNavigationView bottomNavigationView, String dummy) {
-        bottomNavigationView.setBackgroundTintList(null);
-    }
-
-    @BindingAdapter("alignBottomNavIcons")
-    public static void alignBottomNavIcons(BottomNavigationView bottomNavigationView, String dummy) {
-        bottomNavigationView.getMenu().getItem(2).setVisible(false);
-    }
-
-    @BindingAdapter("change")
-    public static void change(FloatingActionButton fab, int destinationId) {
-        if (destinationId == R.id.mainFragment)
-            fabChange(fab, R.drawable.icon_search);
-        else if (destinationId == R.id.listFragment)
-            fabChange(fab, R.drawable.icon_add);
-        else if (destinationId == R.id.sizeFragment)
-            fabChange(fab, R.drawable.icon_save);
-    }
-
-    private static void fabChange(FloatingActionButton fab, int resId) {
-        fab.hide();
-        fab.setImageResource(resId);
-        fab.show();
-    }
 
     @BindingAdapter("setAutoCompleteItems")
     public static void setAutoCompleteItems(MaterialAutoCompleteTextView autoCompleteTextView, Set<String> data) {
@@ -63,11 +28,5 @@ public class ActivityBindingAdapter {
             ArrayList<String> listData = new ArrayList<>(data);
             ((AutoCompleteAdapter) autoCompleteTextView.getAdapter()).setItem(listData);
         }
-    }
-
-    @BindingAdapter("keyboardShowingListener")
-    public static void keyboardShowingListener(CoordinatorLayout layout, MainActivityViewModel model) {
-        layout.getViewTreeObserver().addOnGlobalLayoutListener(() ->
-                model.getKeyboardShowingLive().setValue(KeyBoardUtil.isKeyboardShowing(layout)));
     }
 }
