fbde422dbd8c7f6a97bc7f0644588c935db2257a
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-08 01:31:02 +0900

    onScrollChange, onTopFabClick 추가

--- app/src/main/java/com/example/myfit/ui/BaseFragment.java
+++ app/src/main/java/com/example/myfit/ui/BaseFragment.java
@@ -1,11 +1,17 @@
 package com.example.myfit.ui;
 
+import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.myfit.R;
 import com.example.myfit.databinding.ActivityMainBinding;
+import com.example.myfit.util.LockableScrollView;
+
+import org.jetbrains.annotations.NotNull;
 
 import javax.inject.Inject;
 
@@ -13,12 +19,28 @@ import dagger.hilt.android.AndroidEntryPoint;
 import lombok.Getter;
 import lombok.experimental.Accessors;
 
+import static com.example.myfit.util.ListenerUtil.sScrollEnable;
+import static com.example.myfit.util.adapter.viewholder.BaseVH.sDragging;
+import static com.example.myfit.util.dragselect.DragSelect.sDragSelecting;
+
 @AndroidEntryPoint
 @Accessors(prefix = "m")
 public abstract class BaseFragment extends Fragment {
     @Inject
     @Getter
     protected ActivityMainBinding mActivityBinding;
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        if (getScrollView().getScaleY() == 0) mActivityBinding.fabTop.hide();
+        else mActivityBinding.fabTop.show();
+
+        setOnScrollChange();
+        setOnTopFabClick();
+    }
+
+    protected abstract LockableScrollView getScrollView();
 
     protected void addBottomAppBar() {
         if (mActivityBinding.root.findViewById(R.id.bottomAppBar) == null)
@@ -54,5 +76,32 @@ public abstract class BaseFragment extends Fragment {
         mActivityBinding.fab.hide();
         mActivityBinding.fab.setImageResource(resId);
         mActivityBinding.fab.show();
+    }
+
+    private void setOnScrollChange() {
+        getScrollView().setOnScrollChangeListener((View.OnScrollChangeListener) (v, scrollX, scrollY, oldScrollX, oldScrollY) -> {
+            if (v.getScrollY() == 0) mActivityBinding.fabTop.hide();
+            else mActivityBinding.fabTop.show();
+
+            if ((sDragSelecting || sDragging) && sScrollEnable && scrollY > oldScrollY)
+                v.postDelayed(() -> v.scrollBy(0, 1), 50);
+            else if ((sDragSelecting || sDragging) && sScrollEnable && scrollY < oldScrollY)
+                v.postDelayed(() -> v.scrollBy(0, -1), 50);
+        });
+    }
+
+    private void setOnTopFabClick() {
+        LockableScrollView scrollView = getScrollView();
+        mActivityBinding.fabTop.setOnClickListener(v -> {
+            scrollView.scrollTo(0, 0);
+            scrollView.setOnScrollChangeListener((View.OnScrollChangeListener) (v1, scrollX, scrollY, oldScrollX, oldScrollY) -> {
+                if (scrollY != 0)
+                    getScrollView().scrollTo(0, 0);
+                else {
+                    getScrollView().setOnScrollChangeListener((View.OnScrollChangeListener) null);
+                    setOnScrollChange();
+                }
+            });
+        });
     }
 }
