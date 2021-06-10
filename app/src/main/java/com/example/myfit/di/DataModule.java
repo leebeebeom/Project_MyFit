48fa2fdd262858a533d4e2c95bacc086d1af63a5
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-11 07:32:05 +0900

    레포지토리 프로바이드 추가

--- app/src/main/java/com/example/myfit/di/DataModule.java
+++ app/src/main/java/com/example/myfit/di/DataModule.java
@@ -4,15 +4,16 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 import com.example.myfit.data.AppDataBase;
+import com.example.myfit.data.repository.AutoCompleteRepository;
+import com.example.myfit.data.repository.CategoryRepository;
+import com.example.myfit.data.repository.FolderRepository;
+import com.example.myfit.data.repository.RecentSearchRepository;
+import com.example.myfit.data.repository.SizeRepository;
 import com.example.myfit.data.repository.dao.AutoCompleteDao;
 import com.example.myfit.data.repository.dao.CategoryDao;
 import com.example.myfit.data.repository.dao.FolderDao;
 import com.example.myfit.data.repository.dao.RecentSearchDao;
 import com.example.myfit.data.repository.dao.SizeDao;
-import com.example.myfit.util.constant.Sort;
-import com.example.myfit.util.constant.ViewType;
-import com.example.myfit.util.sharedpreferencelive.BooleanSharePreferenceLiveData;
-import com.example.myfit.util.sharedpreferencelive.IntegerSharedPreferenceLiveData;
 
 import dagger.Module;
 import dagger.Provides;
@@ -24,101 +25,43 @@ import dagger.hilt.android.scopes.ViewModelScoped;
 @Module
 @InstallIn(ViewModelComponent.class)
 public class DataModule {
-    private static final String SORT_MAIN = "sort main";
-    private static final String SORT_LIST = "sort list";
-    public static final String SORT = "sort";
-    public static final String VIEW_TYPE = "view type";
-    public static final String FOLDER_TOGGLE = "folder toggle";
-
     @ViewModelScoped
     @Provides
-    public static CategoryDao provideCategoryDao(AppDataBase appDataBase) {
-        return appDataBase.categoryDao();
+    public static CategoryRepository provideCategoryRepository(@ApplicationContext Context context,
+                                                               @Qualifiers.MainSortPreference SharedPreferences mainSortPreference) {
+        CategoryDao categoryDao = AppDataBase.getsInstance(context).categoryDao();
+        return new CategoryRepository(categoryDao, mainSortPreference);
     }
 
     @ViewModelScoped
     @Provides
-    public static FolderDao provideFolderDao(AppDataBase appDataBase) {
-        return appDataBase.folderDao();
+    public static FolderRepository provideFolderRepository(@ApplicationContext Context context,
+                                                           @Qualifiers.ListSortPreference SharedPreferences listSortPreference,
+                                                           @Qualifiers.FolderTogglePreference SharedPreferences folderTogglePreference) {
+        FolderDao folderDao = AppDataBase.getsInstance(context).folderDao();
+        return new FolderRepository(folderDao, listSortPreference, folderTogglePreference);
     }
 
     @ViewModelScoped
     @Provides
-    public static SizeDao provideSizeDao(AppDataBase appDataBase) {
-        return appDataBase.sizeDao();
+    public static SizeRepository provideSizeRepository(@ApplicationContext Context context,
+                                                       @Qualifiers.ListSortPreference SharedPreferences listSortPreference,
+                                                       @Qualifiers.ViewTypePreference SharedPreferences viewTypePreferences) {
+        SizeDao sizeDao = AppDataBase.getsInstance(context).sizeDao();
+        return new SizeRepository(sizeDao, listSortPreference, viewTypePreferences);
     }
 
     @ViewModelScoped
     @Provides
-    public static RecentSearchDao provideRecentSearchDao(AppDataBase appDataBase) {
-        return appDataBase.recentSearchDao();
+    public static RecentSearchRepository provideRecentSearchRepository(@ApplicationContext Context context) {
+        RecentSearchDao recentSearchDao = AppDataBase.getsInstance(context).recentSearchDao();
+        return new RecentSearchRepository(recentSearchDao);
     }
 
     @ViewModelScoped
     @Provides
-    public static AutoCompleteDao provideAutoCompleteDao(AppDataBase appDataBase) {
-        return appDataBase.autoCompleteDao();
-    }
-
-    @ViewModelScoped
-    @Provides
-    public static AppDataBase provideAppDataBase(@ApplicationContext Context context) {
-        return AppDataBase.getsInstance(context);
-    }
-
-    @Qualifiers.MainSortPreference
-    @ViewModelScoped
-    @Provides
-    public static SharedPreferences provideMainSortPreference(@ApplicationContext Context context) {
-        return context.getSharedPreferences(SORT_MAIN, Context.MODE_PRIVATE);
-    }
-
-    @Qualifiers.MainSortPreferenceLive
-    @ViewModelScoped
-    @Provides
-    public static IntegerSharedPreferenceLiveData provideMainSorePreferenceList(@Qualifiers.MainSortPreference SharedPreferences mainSortPreference) {
-        return new IntegerSharedPreferenceLiveData(mainSortPreference, SORT, Sort.SORT_CUSTOM.getValue());
-    }
-
-    @Qualifiers.ListSortPreference
-    @ViewModelScoped
-    @Provides
-    public static SharedPreferences provideListSortPreference(@ApplicationContext Context context) {
-        return context.getSharedPreferences(SORT_LIST, Context.MODE_PRIVATE);
-    }
-
-    @Qualifiers.ListSortPreferenceLive
-    @ViewModelScoped
-    @Provides
-    public static IntegerSharedPreferenceLiveData provideListSortPreferenceLive(@Qualifiers.ListSortPreference SharedPreferences listSortPreference) {
-        return new IntegerSharedPreferenceLiveData(listSortPreference, SORT, Sort.SORT_CUSTOM.getValue());
-    }
-
-    @Qualifiers.ViewTypePreference
-    @ViewModelScoped
-    @Provides
-    public static SharedPreferences provideViewTypePreferences(@ApplicationContext Context context) {
-        return context.getSharedPreferences(VIEW_TYPE, Context.MODE_PRIVATE);
-    }
-
-    @Qualifiers.ViewTypePreferenceLive
-    @ViewModelScoped
-    @Provides
-    public static IntegerSharedPreferenceLiveData provideViewTypeReferenceLive(@Qualifiers.ViewTypePreference SharedPreferences viewTypePreference) {
-        return new IntegerSharedPreferenceLiveData(viewTypePreference, VIEW_TYPE, ViewType.LIST_VIEW.getValue());
-    }
-
-    @Qualifiers.FolderTogglePreference
-    @ViewModelScoped
-    @Provides
-    public static SharedPreferences provideFolderTogglePreference(@ApplicationContext Context context) {
-        return context.getSharedPreferences(FOLDER_TOGGLE, Context.MODE_PRIVATE);
-    }
-
-    @Qualifiers.FolderTogglePreferenceLive
-    @ViewModelScoped
-    @Provides
-    public static BooleanSharePreferenceLiveData provideFolderTogglePreferenceLive(@Qualifiers.FolderTogglePreference SharedPreferences folderTogglePreferences) {
-        return new BooleanSharePreferenceLiveData(folderTogglePreferences, FOLDER_TOGGLE, false);
+    public static AutoCompleteRepository provideAutoCompleteRepository(@ApplicationContext Context context) {
+        AutoCompleteDao autoCompleteDao = AppDataBase.getsInstance(context).autoCompleteDao();
+        return new AutoCompleteRepository(autoCompleteDao);
     }
 }
