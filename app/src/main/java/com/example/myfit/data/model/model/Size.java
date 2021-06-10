5a4478a9e83c8c8ab2469c8c7381de18cedc94d5
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-10 23:03:54 +0900

    EqualsAndHashCode callSuper 추가, isFavorite -> favorite 변수 명 변경, Cloneable 제거

--- app/src/main/java/com/example/myfit/data/model/model/Size.java
+++ app/src/main/java/com/example/myfit/data/model/model/Size.java
@@ -1,11 +1,8 @@
 package com.example.myfit.data.model.model;
 
-import androidx.annotation.NonNull;
 import androidx.room.Entity;
 
 import com.example.myfit.data.model.BaseModel;
-
-import org.jetbrains.annotations.NotNull;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -14,22 +11,15 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@EqualsAndHashCode(callSuper = false)
-public class Size extends BaseModel implements Cloneable {
+@EqualsAndHashCode(callSuper = true)
+public class Size extends BaseModel {
     private String createdTime, modifiedTime, imageUri, brand, name, size, link, memo,
             firstInfo, secondInfo, thirdInfo, fourthInfo, fifthInfo, sixthInfo;
-    private boolean isFavorite, parentDeleted;
+    private boolean favorite, parentDeleted;
     private long parentId;
 
     public Size(int parentIndex, int sortNumber, long parentId) {
         super(parentIndex, sortNumber);
         this.parentId = parentId;
-    }
-
-    @NonNull
-    @NotNull
-    @Override
-    public Size clone() throws CloneNotSupportedException {
-        return (Size) super.clone();
     }
 }
