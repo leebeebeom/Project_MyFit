ff4a0ed423f09032c33562d877987769585d9e5c
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-11 03:53:37 +0900

    ContentSizeTuple 생성

---
+++ app/src/main/java/com/example/myfit/data/tuple/ContentSizeTuple.java
@@ -0,0 +1,13 @@
+package com.example.myfit.data.tuple;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+public class ContentSizeTuple {
+    private long id;
+    private int contentSize;
+}
