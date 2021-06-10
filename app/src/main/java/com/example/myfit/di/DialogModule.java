f72b5cc01b7f8e08d4d654e227fbe113b51e80cd
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-11 07:34:01 +0900

    다이얼로그 빌더 프로바이드 추가

--- app/src/main/java/com/example/myfit/di/DialogModule.java
+++ app/src/main/java/com/example/myfit/di/DialogModule.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.navigation.NavController;
 
 import com.example.myfit.R;
+import com.example.myfit.ui.dialog.DialogBuilder;
 import com.example.myfit.ui.dialog.tree.TreeNodeProvider;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
@@ -14,14 +15,18 @@ import dagger.Provides;
 import dagger.hilt.InstallIn;
 import dagger.hilt.android.components.FragmentComponent;
 import dagger.hilt.android.qualifiers.ActivityContext;
-import dagger.hilt.android.qualifiers.ApplicationContext;
 
 @Module
 @InstallIn(FragmentComponent.class)
 public class DialogModule {
 
     @Provides
-    public static AlertDialog provideAlertDialog(@ApplicationContext Context context) {
+    public static DialogBuilder provideDialogBuilder(AlertDialog dialog){
+        return new DialogBuilder(dialog);
+    }
+
+    @Provides
+    public static AlertDialog provideAlertDialog(@ActivityContext Context context) {
         return new MaterialAlertDialogBuilder(context, R.style.myAlertDialogStyle)
                 .setNegativeButton(R.string.dialog_cancel, null)
                 .setPositiveButton(R.string.dialog_confirm, null)
