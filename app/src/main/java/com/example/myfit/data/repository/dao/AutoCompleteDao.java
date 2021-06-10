a8a23c3e3b95ff849816f562521b3897dc19f704
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-10 23:04:34 +0900

    쿼리 deleted 추가, AutoCompleteTuple 제거

--- app/src/main/java/com/example/myfit/data/repository/dao/AutoCompleteDao.java
+++ app/src/main/java/com/example/myfit/data/repository/dao/AutoCompleteDao.java
@@ -4,15 +4,14 @@ import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Query;
 
-import com.example.myfit.data.tuple.AutoCompleteTuple;
-
 import java.util.List;
 
 @Dao
 public abstract class AutoCompleteDao {
-    @Query("SELECT(SELECT id, name, deleted FROM Category) + " +
-            "(SELECT id, name, deleted FROM Folder WHERE parentDeleted = 0) + " +
-            "(SELECT id, name, deleted FROM Size WHERE parentDeleted = 0) + " +
-            "(SELECT id, brand, deleted FROM Size WHERE parentDeleted = 0)")
-    public abstract LiveData<List<AutoCompleteTuple>> getAutoCompleteWordsLive();
+
+    @Query("SELECT(SELECT name FROM Category WHERE deleted = :deleted) + " +
+            "(SELECT name FROM Folder WHERE deleted = :deleted AND parentDeleted = 0) + " +
+            "(SELECT name FROM Size WHERE deleted = :deleted AND parentDeleted = 0) + " +
+            "(SELECT brand FROM Size WHERE deleted = :deleted AND parentDeleted = 0)")
+    public abstract LiveData<List<String>> getAutoCompleteWordsLive(boolean deleted);
 }
