d6632d8f5b00f897ec5b5e4a67a89b7bafefe774
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-12 06:12:39 +0900

    equals 수정

--- app/src/main/java/com/example/myfit/data/tuple/BaseTuple.java
+++ app/src/main/java/com/example/myfit/data/tuple/BaseTuple.java
@@ -8,11 +8,32 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@EqualsAndHashCode
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BaseTuple {
     private long id;
     private int parentIndex, sortNumber;
     private String name;
     private long deletedTime;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BaseTuple)) return false;
+
+        BaseTuple baseTuple = (BaseTuple) o;
+
+        if (getId() != baseTuple.getId()) return false;
+        if (getParentIndex() != baseTuple.getParentIndex()) return false;
+        if (getDeletedTime() != baseTuple.getDeletedTime()) return false;
+        return getName() != null ? getName().equals(baseTuple.getName()) : baseTuple.getName() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (getId() ^ (getId() >>> 32));
+        result = 31 * result + getParentIndex();
+        result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+        result = 31 * result + (int) (getDeletedTime() ^ (getDeletedTime() >>> 32));
+        return result;
+    }
 }
