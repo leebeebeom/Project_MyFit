f17ff6cd918ecffcdb1e066d7b14444073912c8f
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-12 03:38:52 +0900

    프래그먼트 모듈 생성

---
+++ app/src/main/java/com/example/myfit/di/FragmentModule.java
@@ -0,0 +1,46 @@
+package com.example.myfit.di;
+
+import android.content.Context;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.myfit.R;
+import com.example.myfit.ui.dialog.tree.TreeNodeProvider;
+import com.example.myfit.util.adapter.AutoCompleteAdapter;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+import dagger.Module;
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.components.FragmentComponent;
+import dagger.hilt.android.qualifiers.ActivityContext;
+
+@Module
+@InstallIn(FragmentComponent.class)
+public class FragmentModule {
+    @Provides
+    public static AutoCompleteAdapter provideAutoCompleteAdapter(@ActivityContext Context context) {
+        return new AutoCompleteAdapter(context, R.layout.item_auto_complete_texv_view, R.id.tv_item_ac_tv);
+    }
+
+    @Provides
+    public static NavController provideNavController(Fragment fragment) {
+        return NavHostFragment.findNavController(fragment);
+    }
+
+    @Provides
+    public static AlertDialog provideAlertDialog(@ActivityContext Context context) {
+        return new MaterialAlertDialogBuilder(context, R.style.myAlertDialogStyle)
+                .setNegativeButton(R.string.dialog_cancel, null)
+                .setPositiveButton(R.string.dialog_confirm, null)
+                .create();
+    }
+
+    @Provides
+    public static TreeNodeProvider provideTreeNodeProvider(@ActivityContext Context context, NavController navController) {
+        return new TreeNodeProvider(context, navController);
+    }
+}
