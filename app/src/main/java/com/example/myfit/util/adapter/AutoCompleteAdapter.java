7438fe24fd14719881850d9b7927c65a5a0e35d6
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-12 06:16:30 +0900

    코드 정리

--- app/src/main/java/com/example/myfit/util/adapter/AutoCompleteAdapter.java
+++ app/src/main/java/com/example/myfit/util/adapter/AutoCompleteAdapter.java
@@ -1,4 +1,4 @@
-package com.example.project_myfit.search.adapter;
+package com.example.myfit.util.adapter;
 
 import android.content.Context;
 import android.widget.ArrayAdapter;
@@ -7,39 +7,32 @@ import android.widget.Filter;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import com.example.project_myfit.util.ktw.KoreanTextMatcher;
+import com.example.myfit.util.ktw.KoreanTextMatcher;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
 public class AutoCompleteAdapter extends ArrayAdapter<String> {
-    private List<String> mBindList;
+    private List<String> mBindList = new ArrayList<>();
     private List<String> mOriginList;
-
-    public AutoCompleteAdapter(@NonNull @NotNull Context context, int resource, int textViewResourceId, @NonNull @NotNull List<String> objects) {
-        super(context, resource, textViewResourceId, objects);
-        this.mBindList = objects;
-        this.mOriginList = objects;
-    }
 
     public AutoCompleteAdapter(@NonNull @NotNull Context context, int resource, int textViewResourceId) {
         super(context, resource, textViewResourceId);
     }
 
-    public void setItem(List<String> autoCompleteList){
+    public void setItem(List<String> autoCompleteList) {
         clear();
-        this.mBindList = autoCompleteList;
         this.mOriginList = autoCompleteList;
         notifyDataSetChanged();
     }
 
     @Override
     public int getCount() {
-        if (mBindList != null) return mBindList.size();
-        else return 0;
+        return mBindList.size();
     }
 
     @Nullable
@@ -57,12 +50,11 @@ public class AutoCompleteAdapter extends ArrayAdapter<String> {
         @Override
         protected FilterResults performFiltering(@NotNull CharSequence constraint) {
             String keyWord = constraint.toString().toLowerCase(Locale.getDefault()).replaceAll("\\p{Z}", "");
-            ArrayList<String> filteredList = new ArrayList<>();
-            for (String s : mOriginList)
-                if (KoreanTextMatcher.isMatch(s.toLowerCase(Locale.getDefault()).replaceAll("\\p{Z}", ""), keyWord))
-                    filteredList.add(s);
 
-            mBindList = filteredList;
+            mBindList = mOriginList.stream().filter(word -> {
+                String word2 = word.toLowerCase(Locale.getDefault()).replaceAll("\\p{Z}", "");
+                return KoreanTextMatcher.isMatch(word, keyWord);
+            }).collect(Collectors.toList());
             return null;
         }
 
