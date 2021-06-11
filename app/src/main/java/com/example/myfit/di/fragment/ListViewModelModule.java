370e9eeab9444a9b1ecb0b6a5749a04154149767
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-12 04:22:45 +0900

    selecteditems 추가, unused 삭제

--- app/src/main/java/com/example/myfit/di/fragment/ListViewModelModule.java
+++ app/src/main/java/com/example/myfit/di/fragment/ListViewModelModule.java
@@ -2,11 +2,14 @@ package com.example.myfit.di.fragment;
 
 import androidx.recyclerview.widget.ItemTouchHelper;
 
+import com.example.myfit.data.tuple.tuple.FolderTuple;
+import com.example.myfit.data.tuple.tuple.SizeTuple;
 import com.example.myfit.di.Qualifiers;
 import com.example.myfit.ui.main.list.adapter.folderadapter.FolderAdapter;
 import com.example.myfit.ui.main.list.adapter.folderadapter.FolderDragCallBack;
 import com.example.myfit.ui.main.list.adapter.sizeadapter.SizeAdapterGrid;
 import com.example.myfit.ui.main.list.adapter.sizeadapter.SizeAdapterList;
+import com.example.myfit.util.SizeLiveSet;
 import com.example.myfit.util.adapter.dragcallback.DragCallBackGrid;
 import com.example.myfit.util.adapter.dragcallback.DragCallBackList;
 
@@ -38,5 +41,17 @@ public class ListViewModelModule {
     @Provides
     public static ItemTouchHelper provideSizeItemTouchHelperGrid(SizeAdapterGrid sizeAdapterGrid) {
         return new ItemTouchHelper(new DragCallBackGrid(sizeAdapterGrid));
+    }
+
+    @ViewModelScoped
+    @Provides
+    public static SizeLiveSet<FolderTuple> provideSelectedFolderTuples() {
+        return new SizeLiveSet<>();
+    }
+
+    @ViewModelScoped
+    @Provides
+    public static SizeLiveSet<SizeTuple> provideSelectedSizeTuples() {
+        return new SizeLiveSet<>();
     }
 }
--- app/src/main/java/com/example/myfit/di/fragment/MainViewModelModule.java
+++ app/src/main/java/com/example/myfit/di/fragment/MainViewModelModule.java
@@ -1,13 +1,7 @@
 package com.example.myfit.di.fragment;
 
-import androidx.recyclerview.widget.ItemTouchHelper;
-
 import com.example.myfit.data.tuple.tuple.CategoryTuple;
-import com.example.myfit.ui.main.main.adapter.CategoryAdapter;
 import com.example.myfit.util.SizeLiveSet;
-import com.example.myfit.util.adapter.BaseViewPagerAdapter;
-import com.example.myfit.util.adapter.dragcallback.DragCallBackList;
-import com.example.myfit.util.dragselect.DragSelect;
 
 import dagger.Module;
 import dagger.Provides;
@@ -18,32 +12,9 @@ import dagger.hilt.android.scopes.ViewModelScoped;
 @Module
 @InstallIn(ViewModelComponent.class)
 public class MainViewModelModule {
-
     @ViewModelScoped
     @Provides
-    public static BaseViewPagerAdapter.MainViewPagerAdapter provideMainViewPagerAdapter(CategoryAdapter[] categoryAdapters,
-                                                                                        DragSelect[] dragSelects,
-                                                                                        ItemTouchHelper[] itemTouchHelpers) {
-        return new BaseViewPagerAdapter.MainViewPagerAdapter(categoryAdapters, dragSelects, itemTouchHelpers);
-    }
-
-    @ViewModelScoped
-    @Provides
-    public static CategoryAdapter[] provideCategoryAdapters(SizeLiveSet<CategoryTuple> selectedCategories) {
-        return new CategoryAdapter[]{new CategoryAdapter(selectedCategories), new CategoryAdapter(selectedCategories), new CategoryAdapter(selectedCategories), new CategoryAdapter(selectedCategories)};
-    }
-
-    @Provides
-    public static ItemTouchHelper[] provideMainItemTouchHelpers(CategoryAdapter[] categoryAdapters) {
-        return new ItemTouchHelper[]{new ItemTouchHelper(new DragCallBackList(categoryAdapters[0])),
-                new ItemTouchHelper(new DragCallBackList(categoryAdapters[1])),
-                new ItemTouchHelper(new DragCallBackList(categoryAdapters[2])),
-                new ItemTouchHelper(new DragCallBackList(categoryAdapters[3]))
-        };
-    }
-
-    @Provides
-    public static DragSelect[] provideDragSelects() {
-        return new DragSelect[]{new DragSelect(), new DragSelect(), new DragSelect(), new DragSelect()};
+    public static SizeLiveSet<CategoryTuple> provideSelectedCategoryTuples() {
+        return new SizeLiveSet<>();
     }
 }
