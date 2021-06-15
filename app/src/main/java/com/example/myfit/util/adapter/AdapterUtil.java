b5904492cb38973b84af63758cccbbce907b84c5
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-16 04:00:12 +0900

    코드정리

--- app/src/main/java/com/example/myfit/util/adapter/AdapterUtil.java
+++ app/src/main/java/com/example/myfit/util/adapter/AdapterUtil.java
@@ -11,23 +11,23 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AdapterUtil {
     public static <T extends BaseTuple> void itemMove(int from, int to, List<T> list) {
-        if (from < to) {//down
+        if (from < to) //down
             for (int i = from; i < to; i++)
-                if (list.get(i + 1).getId() != -1)
-                    swapOrderNumber(list, i, i + 1);
-        } else //up
+                swapOrderNumber(list, i, i + 1);
+        else //up
             for (int i = from; i > to; i--)
                 swapOrderNumber(list, i, i - 1);
     }
 
     private static <T extends BaseTuple> void swapOrderNumber(List<T> list, int i, int i2) {
         Collections.swap(list, i, i2);
-
-        BaseTuple baseTuple1 = list.get(i);
-        BaseTuple baseTuple2 = list.get(i2);
-        int sortNumber1 = baseTuple1.getSortNumber();
-        int sortNumber2 = baseTuple2.getSortNumber();
-        baseTuple1.setSortNumber(sortNumber2);
-        baseTuple2.setSortNumber(sortNumber1);
+        if (list.get(i).getId() != -1 && list.get(i2).getId() != -1) {
+            BaseTuple baseTuple1 = list.get(i);
+            BaseTuple baseTuple2 = list.get(i2);
+            int sortNumber1 = baseTuple1.getSortNumber();
+            int sortNumber2 = baseTuple2.getSortNumber();
+            baseTuple1.setSortNumber(sortNumber2);
+            baseTuple2.setSortNumber(sortNumber1);
+        }
     }
 }
