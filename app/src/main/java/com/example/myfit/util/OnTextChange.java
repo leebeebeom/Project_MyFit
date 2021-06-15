8e954a5420d3183374bcd997e8c56dbcf7e6b6c6
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-16 03:58:24 +0900

    코드정리

--- app/src/main/java/com/example/myfit/util/OnTextChange.java
+++ app/src/main/java/com/example/myfit/util/OnTextChange.java
@@ -6,14 +6,12 @@ import android.text.TextWatcher;
 public abstract class OnTextChange implements TextWatcher {
     @Override
     public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
     }
 
     @Override
     public abstract void onTextChanged(CharSequence s, int start, int before, int count);
 
     @Override
-    public void afterTextChanged(Editable s) {
-
+    public void afterTextChanged(Editable s) {ㅋ
     }
 }
