1b07e140c7d01054a9fbe3742798409de3ed27ac
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-17 03:22:51 +0900

    코드정리

--- app/src/main/java/com/example/myfit/util/adapter/viewholder/SizeGridVH.java
+++ app/src/main/java/com/example/myfit/util/adapter/viewholder/SizeGridVH.java
@@ -4,14 +4,10 @@ import com.example.myfit.data.tuple.tuple.SizeTuple;
 import com.example.myfit.databinding.ItemVhSizeGridBinding;
 import com.example.myfit.util.SizeLiveSet;
 import com.example.myfit.util.adapter.BaseAdapter;
-import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.checkbox.MaterialCheckBox;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Set;
-
-import lombok.Getter;
 import lombok.experimental.Accessors;
 
 @Accessors(prefix = "m")
@@ -34,19 +30,8 @@ public class SizeGridVH extends BaseVH.BaseSizeVH {
     }
 
     @Override
-    protected void setDraggingView() {
-        setItemViewTranslationZ(itemView, 10);
-        mBinding.iv.setAlpha(0.5f);
-        mBinding.layoutContents.setAlpha(0.7f);
-        mBinding.cb.setAlpha(0.5f);
-    }
-
-    @Override
-    protected void setDropView() {
-        setItemViewTranslationZ(itemView, 0);
-        mBinding.iv.setAlpha(1f);
-        mBinding.layoutContents.setAlpha(1);
-        mBinding.cb.setAlpha(0.8f);
+    protected void setDragging() {
+        mBinding.setDragging(sDragging);
     }
 
     @Override
