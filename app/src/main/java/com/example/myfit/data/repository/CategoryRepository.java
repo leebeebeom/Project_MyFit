101afb1d1bb4d514e5bd1fb5e9d4ab3d1a3eb420
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-18 03:03:31 +0900

    리팩토링

--- app/src/main/java/com/example/myfit/data/repository/CategoryRepository.java
+++ app/src/main/java/com/example/myfit/data/repository/CategoryRepository.java
@@ -4,19 +4,26 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
 
 import com.example.myfit.data.AppDataBase;
 import com.example.myfit.data.model.model.Category;
 import com.example.myfit.data.repository.dao.CategoryDao;
 import com.example.myfit.data.tuple.tuple.CategoryTuple;
+import com.example.myfit.util.SortUtil;
 import com.example.myfit.util.constant.SharedPreferenceKey;
 import com.example.myfit.util.constant.Sort;
 import com.example.myfit.util.sharedpreferencelive.IntegerSharedPreferenceLiveData;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -27,21 +34,25 @@ import lombok.experimental.Accessors;
 
 @Singleton
 @Accessors(prefix = "m")
-public class CategoryRepository extends BaseRepository<CategoryTuple> {
+public class CategoryRepository extends BaseRepository {
     private final CategoryDao mCategoryDao;
     private final SharedPreferences mMainSortPreference;
     @Getter
     private final IntegerSharedPreferenceLiveData mMainSortPreferenceLive;
     private MutableLiveData<Boolean> mExistingNameLive;
-    private LiveData<List<List<CategoryTuple>>> mClassifiedTuplesLive, mDeletedClassifiedTuplesLive, mDeletedSearchTuplesLive;
-    private MutableLiveData<CategoryTuple> mAddedTupleLive;
-    private MutableLiveData<List<CategoryTuple>> mAddedTuplesLive;
+    private final MediatorLiveData<List<List<CategoryTuple>>> mClassifiedTuplesLive = new MediatorLiveData<>();
+    private LiveData<List<List<CategoryTuple>>> mDeletedClassifiedTuplesLive, mDeletedSearchTuplesLive;
+    private LiveData<List<CategoryTuple>> mAddedTuplesLive;
+    private final LiveData<List<Category>> mAllCategoriesLive;
 
     @Inject
     public CategoryRepository(@ApplicationContext Context context) {
         this.mCategoryDao = AppDataBase.getsInstance(context).categoryDao();
         this.mMainSortPreference = context.getSharedPreferences(SharedPreferenceKey.SORT_MAIN.getValue(), Context.MODE_PRIVATE);
-        this.mMainSortPreferenceLive = new IntegerSharedPreferenceLiveData(mMainSortPreference, SharedPreferenceKey.SORT.getValue(), Sort.SORT_CUSTOM.getValue());
+        this.mMainSortPreferenceLive = new IntegerSharedPreferenceLiveData(
+                mMainSortPreference, SharedPreferenceKey.SORT.getValue(), Sort.SORT_CUSTOM.getValue());
+
+        this.mAllCategoriesLive = mCategoryDao.getAllCategoriesWithContentSize();
     }
 
     //from appDataBase
@@ -51,81 +62,102 @@ public class CategoryRepository extends BaseRepository<CategoryTuple> {
 
     //to main
     public LiveData<List<List<CategoryTuple>>> getClassifiedTuplesLive() {
-        if (mClassifiedTuplesLive == null)
-            mClassifiedTuplesLive = mCategoryDao.getClassifiedTuplesLive();
+        if (mClassifiedTuplesLive.getValue() == null) {
+            mClassifiedTuplesLive.addSource(mAllCategoriesLive, this::setValue);
+            mClassifiedTuplesLive.addSource(mMainSortPreferenceLive, sortInt -> {
+                if (mAllCategoriesLive.getValue() != null)
+                    setValue(mAllCategoriesLive.getValue());
+            });
+        }
         return mClassifiedTuplesLive;
+    }
+
+    private void setValue(List<Category> categories) {
+        Stream<Category> unDeletedCategoryStream = super.getUnDeletedStream(categories);
+        Stream<Category> sortedStream = SortUtil.sortCategoryStream(super.getSort(), unDeletedCategoryStream);
+        List<List<CategoryTuple>> classifiedTuples = super.getClassifiedTuplesByParentIndex(getCategoryTuples(sortedStream));
+        mClassifiedTuplesLive.setValue(classifiedTuples);
+    }
+
+    private List<CategoryTuple> getCategoryTuples(Stream<Category> stream) {
+        return stream.map(CategoryTuple::new).collect(Collectors.toList());
     }
 
     //to recycleBin
     public LiveData<List<List<CategoryTuple>>> getDeletedClassifiedTuplesLive() {
         if (mDeletedClassifiedTuplesLive == null)
-            mDeletedClassifiedTuplesLive = mCategoryDao.getDeletedClassifiedTuplesLive();
+            mDeletedClassifiedTuplesLive = Transformations.map(mAllCategoriesLive, categories -> {
+                Stream<Category> deletedCategoryStream = super.getDeletedCategoryStream(categories)
+                        .sorted((o1, o2) -> Long.compare(o1.getDeletedTime(), o2.getDeletedTime()));
+                return super.getClassifiedTuplesByParentIndex(getCategoryTuples(deletedCategoryStream));
+            });
         return mDeletedClassifiedTuplesLive;
     }
 
     //to recycleBin search
     public LiveData<List<List<CategoryTuple>>> getDeletedSearchTuplesLive() {
         if (mDeletedSearchTuplesLive == null)
-            mDeletedSearchTuplesLive = mCategoryDao.getDeletedSearchTuplesLive();
+            mDeletedSearchTuplesLive = Transformations.map(mAllCategoriesLive, categories -> {
+                Stream<Category> deletedCategoryStream = super.getDeletedCategoryStream(categories)
+                        .sorted((o1, o2) -> o1.getName().compareTo(o2.getName()));
+                return super.getClassifiedTuplesByParentIndex(getCategoryTuples(deletedCategoryStream));
+            });
         return mDeletedSearchTuplesLive;
     }
 
     //to listFragment
     public LiveData<Category> getSingleLiveById(long id) {
-        return mCategoryDao.getSingleLiveById(id);
-    }
-
-    //to treeView(disposable)
-    public LiveData<List<CategoryTuple>> getTuplesByParentIndex(int parentIndex) {
-        MutableLiveData<List<CategoryTuple>> tuplesLive = new MutableLiveData<>();
-        new Thread(() -> {
-            Sort sort = getSort();
-            List<CategoryTuple> tuples = mCategoryDao.getTuplesByParentIndex(parentIndex, sort);
-            tuplesLive.postValue(tuples);
-        }).start();
-        return tuplesLive;
+        return Transformations.map(mAllCategoriesLive, categories -> {
+            Optional<Category> categoryOptional = categories.stream().filter(category -> category.getId() == id).findAny();
+            return categoryOptional.orElse(null);
+        });
     }
 
     //to treeView
-    public MutableLiveData<CategoryTuple> getAddedTupleLive() {
-        mAddedTupleLive = new MutableLiveData<>();
-        return mAddedTupleLive;
+    public LiveData<List<CategoryTuple>> getTuplesLiveByParentIndex(int parentIndex) {
+        return Transformations.map(mAllCategoriesLive, categories -> {
+            Stream<Category> unDeletedCategoryStream = super.getUnDeletedStream(categories)
+                    .filter(category -> category.getParentIndex() == parentIndex);
+            Stream<Category> sortedStream = SortUtil.sortCategoryStream(super.getSort(), unDeletedCategoryStream);
+            return getCategoryTuples(sortedStream);
+        });
     }
 
     //from addCategory dialog
     public void insert(String name, int parentIndex) {
-        new Thread(() -> {
-            long insertId = mCategoryDao.insert(name, parentIndex);
-            if (mAddedTupleLive != null) {
-                CategoryTuple addedTuple = mCategoryDao.getTupleById(insertId);
-                mAddedTupleLive.postValue(addedTuple);
-            }
-        }).start();
+        new Thread(() -> mCategoryDao.insert(name, parentIndex)).start();
     }
 
     //to recycle bin
-    public MutableLiveData<List<CategoryTuple>> getAddedTuplesLive() {
+    public LiveData<List<CategoryTuple>> getAddedTuplesLive() {
         mAddedTuplesLive = new MutableLiveData<>();
         return mAddedTuplesLive;
     }
 
     //from restore dialog(disposable)
-    public LiveData<List<Long>> insertRestoreCategories(@NotNull List<Integer> parentIndex) {
-        MutableLiveData<List<Long>> insertIdsLive = new MutableLiveData<>();
+    public void insertRestoreCategories(@NotNull List<Integer> parentIndex) {
         new Thread(() -> {
             List<Long> insertIds = mCategoryDao.insertRestoreCategories(parentIndex);
-            List<CategoryTuple> addedTuples = mCategoryDao.getTuplesByIds(insertIds);
-            mAddedTuplesLive.postValue(addedTuples);
+            mAddedTuplesLive = Transformations.map(mAllCategoriesLive, categories -> {
+                Stream<Category> addedCategoriesStream =
+                        categories.stream().filter(category -> insertIds.stream().anyMatch(id -> id == category.getId()));
+                return getCategoryTuples(addedCategoriesStream);
+            });
         }).start();
-        return insertIdsLive;
     }
 
     //from editCategoryName dialog
     public void update(long id, String name) {
-        new Thread(() -> mCategoryDao.update(id, name)).start();
+        if (mAllCategoriesLive.getValue() != null) {
+            Optional<Category> categoryOptional = mAllCategoriesLive.getValue().stream().filter(category -> category.getId() == id).findFirst();
+            if (categoryOptional.isPresent()) {
+                Category category = categoryOptional.get();
+                category.setName(name);
+                new Thread(() -> mCategoryDao.update(category)).start();
+            }
+        }
     }
 
-    @Override
     //from adapter drag drop
     public void updateTuples(List<CategoryTuple> categoryTuples) {
         new Thread(() -> mCategoryDao.updateTuples(categoryTuples)).start();
@@ -134,7 +166,16 @@ public class CategoryRepository extends BaseRepository<CategoryTuple> {
     @Override
     //from delete dialog, restore dialog
     public void deleteOrRestore(long[] ids) {
-        new Thread(() -> mCategoryDao.deleteOrRestore(ids)).start();
+        if (mAllCategoriesLive.getValue() != null) {
+            List<Category> categories = mAllCategoriesLive.getValue().stream()
+                    .filter(category -> Arrays.stream(ids).anyMatch(id -> category.getId() == id))
+                    .collect(Collectors.toList());
+            super.setDeleted(categories);
+            new Thread(() -> {
+                mCategoryDao.update(categories);
+                mCategoryDao.setChildrenParentDeleted(ids);
+            }).start();
+        }
     }
 
     @Override
