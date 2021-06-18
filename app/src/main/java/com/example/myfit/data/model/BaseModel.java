f134a17958b22c51382573a4fc5c52a4c3f74d67
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-19 01:03:24 +0900

    리팩토링

--- app/src/main/java/com/example/myfit/data/model/BaseModel.java
+++ app/src/main/java/com/example/myfit/data/model/BaseModel.java
@@ -18,10 +18,12 @@ public class BaseModel {
     private int sortNumber;
     private boolean deleted;
     private long deletedTime;
+    private String name;
 
-    protected BaseModel(int parentIndex, int sortNumber) {
+    protected BaseModel(int parentIndex, int sortNumber, String name) {
         this.id = CommonUtil.createId();
         this.parentIndex = parentIndex;
         this.sortNumber = sortNumber;
+        this.name = name;
     }
 }
