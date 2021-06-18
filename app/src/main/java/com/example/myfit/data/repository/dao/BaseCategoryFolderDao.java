00f68f44d7b089cf40aab8a90d03258c30b41ade
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-19 02:39:24 +0900

    nullSafe

--- app/src/main/java/com/example/myfit/data/repository/dao/BaseCategoryFolderDao.java
+++ app/src/main/java/com/example/myfit/data/repository/dao/BaseCategoryFolderDao.java
@@ -13,6 +13,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 @Dao
 public abstract class BaseCategoryFolderDao<T extends Category, U extends BaseTuple> extends BaseDao<T, U> {
@@ -24,26 +25,20 @@ public abstract class BaseCategoryFolderDao<T extends Category, U extends BaseTu
         MediatorLiveData<List<T>> mediatorLive = new MediatorLiveData<>();
 
         mediatorLive.addSource(allModelsLive, models -> {
-            if (folderContentSizeTuplesLive.getValue() != null && sizeContentSizeTuplesLive.getValue() != null) {
-                setFolderContentSize(models, folderContentSizeTuplesLive.getValue());
-                setSizeContentSize(models, sizeContentSizeTuplesLive.getValue());
-            }
+            setFolderContentSize(models, folderContentSizeTuplesLive.getValue());
+            setSizeContentSize(models, sizeContentSizeTuplesLive.getValue());
             mediatorLive.setValue(models);
         });
 
         mediatorLive.addSource(folderContentSizeTuplesLive, folderContentSizeTuples -> {
-            if (allModelsLive.getValue() != null && sizeContentSizeTuplesLive.getValue() != null) {
-                setFolderContentSize(allModelsLive.getValue(), folderContentSizeTuples);
-                setSizeContentSize(allModelsLive.getValue(), sizeContentSizeTuplesLive.getValue());
-                mediatorLive.setValue(allModelsLive.getValue());
-            }
+            setFolderContentSize(allModelsLive.getValue(), folderContentSizeTuples);
+            setSizeContentSize(allModelsLive.getValue(), sizeContentSizeTuplesLive.getValue());
+            mediatorLive.setValue(allModelsLive.getValue());
         });
         mediatorLive.addSource(sizeContentSizeTuplesLive, sizeContentSizeTuples -> {
-            if (allModelsLive.getValue() != null && folderContentSizeTuplesLive.getValue() != null) {
-                setFolderContentSize(allModelsLive.getValue(), folderContentSizeTuplesLive.getValue());
-                setSizeContentSize(allModelsLive.getValue(), sizeContentSizeTuplesLive.getValue());
-                mediatorLive.setValue(allModelsLive.getValue());
-            }
+            setFolderContentSize(allModelsLive.getValue(), folderContentSizeTuplesLive.getValue());
+            setSizeContentSize(allModelsLive.getValue(), sizeContentSizeTuplesLive.getValue());
+            mediatorLive.setValue(allModelsLive.getValue());
         });
         return mediatorLive;
     }
@@ -55,22 +50,30 @@ public abstract class BaseCategoryFolderDao<T extends Category, U extends BaseTu
     protected abstract LiveData<List<ContentSizeTuple>> getSizeContentSizeTuplesLive();
 
     private void setFolderContentSize(List<T> models, List<ContentSizeTuple> folderContentSizeTuples) {
-        models.forEach(model ->
-                getContentSizeOptional(folderContentSizeTuples, model)
-                        .ifPresent(folderContentSizeTuple -> model.setFolderContentSize(folderContentSizeTuple.getSize())));
+        if (models != null)
+            models.forEach(model ->
+                    getContentSizeOptional(folderContentSizeTuples, model)
+                            .ifPresent(folderContentSizeTuple -> model.setFolderContentSize(folderContentSizeTuple.getSize())));
     }
 
     private void setSizeContentSize(List<T> models, List<ContentSizeTuple> sizeContentSizeTuples) {
-        models.forEach(model ->
-                getContentSizeOptional(sizeContentSizeTuples, model)
-                        .ifPresent(sizeContentSizeTuple -> model.setSizeContentSize(sizeContentSizeTuple.getSize())));
+        if (models != null)
+            models.forEach(model ->
+                    getContentSizeOptional(sizeContentSizeTuples, model)
+                            .ifPresent(sizeContentSizeTuple -> model.setSizeContentSize(sizeContentSizeTuple.getSize())));
     }
 
     @NotNull
     private Optional<ContentSizeTuple> getContentSizeOptional(List<ContentSizeTuple> contentSizeTuples, T model) {
-        return contentSizeTuples.stream()
-                .filter(contentSizeTuple -> contentSizeTuple.getSize() != 0
-                        && contentSizeTuple.getParentId() == model.getId())
+        return getContentSizeTuplesStream(contentSizeTuples)
+                .filter(contentSizeTuple ->
+                        contentSizeTuple.getSize() != 0
+                                && contentSizeTuple.getParentId() == model.getId())
                 .findAny();
+    }
+
+    @NotNull
+    private Stream<ContentSizeTuple> getContentSizeTuplesStream(List<ContentSizeTuple> contentSizeTuples) {
+        return contentSizeTuples != null ? contentSizeTuples.stream() : Stream.empty();
     }
 }
