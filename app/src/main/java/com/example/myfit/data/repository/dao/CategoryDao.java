6ac683a017c4ee649e82087b00d398bc5c440dbc
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-19 00:57:55 +0900

    리팩토링

--- app/src/main/java/com/example/myfit/data/repository/dao/CategoryDao.java
+++ app/src/main/java/com/example/myfit/data/repository/dao/CategoryDao.java
@@ -4,22 +4,16 @@ import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
-import androidx.room.Transaction;
 import androidx.room.Update;
 
 import com.example.myfit.data.model.model.Category;
 import com.example.myfit.data.tuple.ContentSizeTuple;
 import com.example.myfit.data.tuple.tuple.CategoryTuple;
-import com.example.myfit.util.CommonUtil;
 
-import org.jetbrains.annotations.NotNull;
-
-import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 @Dao
-public abstract class CategoryDao extends BaseDao<Category> {
+public abstract class CategoryDao extends BaseCategoryFolderDao<Category> {
     @Override
     protected LiveData<List<Category>> getAllModelsLive() {
         return getAllCategoriesLive();
@@ -48,37 +42,7 @@ public abstract class CategoryDao extends BaseDao<Category> {
             "GROUP BY category.id")
     protected abstract LiveData<List<ContentSizeTuple>> getSizeContentSizeTuplesLiveImpl();
 
-    @Transaction
-    //from addCategory dialog(disposable)
-    public void insert(String name, int parentIndex) {
-        int sortNumber = getLargestSortNumber() + 1;
-        Category category = new Category(parentIndex, sortNumber, name);
-        insert(category);
-    }
-
-    @Query("SELECT max(sortNumber) FROM Category")
-    protected abstract int getLargestSortNumber();
-
-    @Transaction
-    //from restore dialog(disposable)
-    public List<Long> insertRestoreCategories(@NotNull List<Integer> parentIndexes) {
-        String name = "복구됨";
-
-        List<Category> categories = new LinkedList<>();
-        int sortNumber = getLargestSortNumber();
-        for (int parentIndex : parentIndexes)
-            categories.add(new Category(parentIndex, ++sortNumber, name));
-
-        AtomicLong id = new AtomicLong(CommonUtil.createId());
-        categories.forEach(category -> category.setId(id.incrementAndGet()));
-        return insert(categories);
-    }
-
     //from adapter drag drop
     @Update(onConflict = OnConflictStrategy.REPLACE, entity = Category.class)
     public abstract void updateTuples(List<CategoryTuple> categoryTuples);
-
-    //from addCategory dialog
-    @Query("SELECT EXISTS(SELECT name, parentIndex FROM Category WHERE name =:name AND parentIndex=:parentIndex AND deleted = 0)")
-    public abstract boolean isExistingName(String name, int parentIndex);
 }
