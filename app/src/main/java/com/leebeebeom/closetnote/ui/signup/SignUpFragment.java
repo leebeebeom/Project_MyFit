b1bd5aa669e1b285896e27c4e19a99d76a648c54
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-24 11:27:53 +0900

    Sign Up 완성

---
+++ app/src/main/java/com/leebeebeom/closetnote/ui/signup/SignUpFragment.java
@@ -0,0 +1,128 @@
+package com.leebeebeom.closetnote.ui.signup;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseNetworkException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.leebeebeom.closetnote.R;
+import com.leebeebeom.closetnote.databinding.FragmentSignUpBinding;
+import com.leebeebeom.closetnote.ui.BaseFragment;
+import com.leebeebeom.closetnote.ui.view.LockableScrollView;
+import com.leebeebeom.closetnote.util.EditTextErrorKeyListener;
+
+import org.jetbrains.annotations.NotNull;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+import static com.leebeebeom.closetnote.ui.main.MainActivity.TAG;
+
+@AndroidEntryPoint
+public class SignUpFragment extends BaseFragment {
+    @Inject
+    FirebaseAuth mAuth;
+    private SignUpViewModel mModel;
+    private FragmentSignUpBinding mBinding;
+
+    @Override
+    public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mModel = new ViewModelProvider(this).get(SignUpViewModel.class);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        mBinding = FragmentSignUpBinding.inflate(inflater, container, false);
+        mBinding.setSignUpFragment(this);
+        mBinding.setModel(mModel);
+        mBinding.setLifecycleOwner(this);
+
+        mBinding.etUsername.addTextChangedListener(new EditTextErrorKeyListener(mBinding.usernameLayout));
+        mBinding.et.etEmail.addTextChangedListener(new EditTextErrorKeyListener(mBinding.et.emailLayout));
+        mBinding.et.etPassword.addTextChangedListener(new EditTextErrorKeyListener(mBinding.et.passwordLayout));
+        mBinding.etConfirmPassword.addTextChangedListener(new EditTextErrorKeyListener(mBinding.confirmPasswordLayout));
+        return mBinding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mBinding = null;
+    }
+
+    public void emailSignUp() {
+        if (mModel.isTextsNotEmpty()) {
+            if (mModel.isPasswordNotEquals())
+                mBinding.confirmPasswordLayout.setError(getString(R.string.sgin_in_password_not_equal));
+            else mAuth.createUserWithEmailAndPassword(mModel.getEmail(), mModel.getPassword())
+                    .addOnFailureListener(requireActivity(), getOnFailureListener())
+                    .addOnSuccessListener(requireActivity(), getOnSuccessListener())
+                    .addOnCanceledListener(requireActivity(), getOnCanceledListener());
+        } else setEmptyError();
+    }
+
+    @NotNull
+    private OnFailureListener getOnFailureListener() {
+        return e -> {
+            if (e instanceof FirebaseAuthWeakPasswordException)
+                mBinding.et.passwordLayout.setError(getString(R.string.sign_up_password_is_less_than_6));
+            else if (e instanceof FirebaseAuthInvalidCredentialsException)
+                mBinding.et.emailLayout.setError(getString(R.string.sgin_up_email_is_badly_formatted));
+            else if (e instanceof FirebaseAuthUserCollisionException)
+                mBinding.et.emailLayout.setError(getString(R.string.sign_up_email_is_used));
+            else if (e instanceof FirebaseNetworkException)
+                Toast.makeText(requireContext(), R.string.all_please_check_internet, Toast.LENGTH_SHORT).show();
+            else {
+                Toast.makeText(requireContext(), getString(R.string.sign_up_fail) + e.getMessage(), Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "getOnFailureListener: " + e);
+            }
+        };
+    }
+
+    @NotNull
+    private OnCanceledListener getOnCanceledListener() {
+        return () -> Toast.makeText(requireContext(), getString(R.string.sign_up_canceled), Toast.LENGTH_SHORT).show();
+    }
+
+    @NotNull
+    private OnSuccessListener<AuthResult> getOnSuccessListener() {
+        return authResult -> {
+            Toast.makeText(requireContext(), getString(R.string.sign_up_success), Toast.LENGTH_SHORT).show();
+            mNavController.popBackStack();
+        };
+    }
+
+    private void setEmptyError() {
+        if (mModel.isUsernameEmpty())
+            mBinding.usernameLayout.setError(getString(R.string.sing_up_username_is_empty));
+        if (mModel.isEmailEmpty())
+            mBinding.et.emailLayout.setError(getString(R.string.sign_in_email_is_empty));
+        if (mModel.isPasswordEmpty())
+            mBinding.et.passwordLayout.setError(getString(R.string.sign_in_password_is_empty));
+        if (mModel.isConfirmPasswordEmpty())
+            mBinding.confirmPasswordLayout.setError(getString(R.string.sign_in_password_is_empty));
+    }
+
+    @Override
+    public LockableScrollView getScrollView() {
+        return mBinding.sv;
+    }
+}
---
+++ app/src/main/java/com/leebeebeom/closetnote/ui/signup/SignUpViewModel.java
@@ -0,0 +1,51 @@
+package com.leebeebeom.closetnote.ui.signup;
+
+import android.text.TextUtils;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import dagger.hilt.android.lifecycle.HiltViewModel;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Accessors(prefix = "m")
+public class SignUpViewModel extends ViewModel {
+    private final MutableLiveData<String> mUsernameLive = new MutableLiveData<>("");
+    private final MutableLiveData<String> mEmailLive = new MutableLiveData<>("");
+    private final MutableLiveData<String> mPasswordLive = new MutableLiveData<>("");
+    private final MutableLiveData<String> mConfirmPasswordLive = new MutableLiveData<>("");
+
+    boolean isTextsNotEmpty() {
+        return !isUsernameEmpty() && !isEmailEmpty() && !isPasswordEmpty() && !isConfirmPasswordEmpty();
+    }
+
+    boolean isPasswordNotEquals() {
+        return !String.valueOf(mPasswordLive.getValue()).equals(mConfirmPasswordLive.getValue());
+    }
+
+    boolean isUsernameEmpty() {
+        return TextUtils.isEmpty(mUsernameLive.getValue());
+    }
+
+    boolean isEmailEmpty() {
+        return TextUtils.isEmpty(mEmailLive.getValue());
+    }
+
+    boolean isPasswordEmpty() {
+        return TextUtils.isEmpty(mPasswordLive.getValue());
+    }
+
+    boolean isConfirmPasswordEmpty() {
+        return TextUtils.isEmpty(mConfirmPasswordLive.getValue());
+    }
+
+    String getEmail() {
+        return String.valueOf(mEmailLive);
+    }
+
+    String getPassword() {
+        return String.valueOf(mPasswordLive);
+    }
+}
