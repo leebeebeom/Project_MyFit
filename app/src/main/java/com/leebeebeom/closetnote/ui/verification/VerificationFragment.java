d80f291e8f61654abb1a21e1caf0a782d0e2e6b2
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-24 13:38:00 +0900

    Verification 프래그먼트 완성

---
+++ app/src/main/java/com/leebeebeom/closetnote/ui/verification/VerificationFragment.java
@@ -0,0 +1,55 @@
+package com.leebeebeom.closetnote.ui.verification;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.leebeebeom.closetnote.databinding.FragmentVerificationBinding;
+
+import org.jetbrains.annotations.NotNull;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
+public class VerificationFragment extends Fragment {
+    @Inject
+    FirebaseAuth mAuth;
+    private FragmentVerificationBinding mBinding;
+
+    @Nullable
+    @org.jetbrains.annotations.Nullable
+    @Override
+    public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        mBinding = FragmentVerificationBinding.inflate(inflater, container, false);
+        mBinding.setVerificationFragment(this);
+        return mBinding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mBinding = null;
+    }
+
+    public void openBrowser() {
+        if (mAuth.getCurrentUser() != null && mAuth.getCurrentUser().getEmail() != null) {
+            String email = mAuth.getCurrentUser().getEmail();
+            int index = email.indexOf("@");
+            String domain = "https://" + email.substring(index + 1);
+
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(domain));
+            if (intent.resolveActivity(requireContext().getPackageManager()) != null)
+                startActivity(intent);
+        }
+    }
+}
