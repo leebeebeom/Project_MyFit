480b96771a4a407b16ec8c76caf67bca56911fb9
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-25 11:23:46 +0900

    ReVerificationFragment 완성

---
+++ app/src/main/java/com/leebeebeom/closetnote/ui/signin/reverification/ReVerificationFragment.java
@@ -0,0 +1,58 @@
+package com.leebeebeom.closetnote.ui.signin.reverification;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.firebase.auth.ActionCodeSettings;
+import com.google.firebase.auth.FirebaseAuth;
+import com.leebeebeom.closetnote.databinding.FragmentReVerificationBinding;
+import com.leebeebeom.closetnote.ui.BaseFragment;
+import com.leebeebeom.closetnote.ui.view.LockableScrollView;
+import com.leebeebeom.closetnote.util.CommonUtil;
+
+import org.jetbrains.annotations.NotNull;
+
+import javax.inject.Inject;
+
+public class ReVerificationFragment extends BaseFragment {
+    @Inject
+    FirebaseAuth mAuth;
+    @Inject
+    ActionCodeSettings mActionCodeSettings;
+    private FragmentReVerificationBinding mBinding;
+
+    @Nullable
+    @org.jetbrains.annotations.Nullable
+    @Override
+    public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        removeAppBar();
+        removeBottomAppBar();
+
+        mBinding = FragmentReVerificationBinding.inflate(inflater, container, false);
+        return mBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        if (mAuth.getCurrentUser() != null)
+            mAuth.getCurrentUser().sendEmailVerification(mActionCodeSettings);
+    }
+
+    public void openEmail() {
+        if (mAuth.getCurrentUser() != null && mAuth.getCurrentUser().getEmail() != null) {
+            String email = mAuth.getCurrentUser().getEmail();
+            openBrowser(CommonUtil.getDomain(email));
+        }
+    }
+
+    @Override
+    public LockableScrollView getScrollView() {
+        return mBinding.sv;
+    }
+}
