f596fa6788fd06091f8374955bc1fa211fc55a65
leebeebeom <75092455+leebeebeom@users.noreply.github.com>
2021-06-30 03:14:37 +0900

    AuthUtil 생성

--- app/src/main/java/com/leebeebeom/closetnote/ui/signin/SignInFragment.java
+++ app/src/main/java/com/leebeebeom/closetnote/ui/signin/SignInFragment.java
@@ -2,7 +2,6 @@ package com.leebeebeom.closetnote.ui.signin;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -20,55 +19,33 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.ActionCodeSettings;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthInvalidUserException;
-import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.auth.UserProfileChangeRequest;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-import com.kakao.sdk.auth.model.OAuthToken;
-import com.kakao.sdk.user.UserApiClient;
-import com.kakao.sdk.user.model.Account;
-import com.kakao.sdk.user.model.User;
 import com.leebeebeom.closetnote.R;
 import com.leebeebeom.closetnote.databinding.FragmentSignInBinding;
 import com.leebeebeom.closetnote.di.Qualifiers;
+import com.leebeebeom.closetnote.util.AuthUtil;
 import com.leebeebeom.closetnote.util.CommonUtil;
 import com.leebeebeom.closetnote.util.EditTextErrorKeyListener;
 import com.leebeebeom.closetnote.util.ToastUtil;
-import com.nhn.android.naverlogin.OAuthLogin;
-import com.nhn.android.naverlogin.OAuthLoginHandler;
-
-import org.jetbrains.annotations.NotNull;
 
 import javax.inject.Inject;
 
 import dagger.hilt.android.AndroidEntryPoint;
-import kotlin.Unit;
-import kotlin.jvm.functions.Function2;
 
 import static com.leebeebeom.closetnote.ui.MainActivity.TAG;
 
 @AndroidEntryPoint
-public class SignInFragment extends BaseSignInFragment {
+public class SignInFragment extends BaseSignInFragment implements AuthUtil.EmailSignInListener {
     @Inject
     NavController mNavController;
-    @Inject
-    FirebaseAuth mAuth;
-    @Inject
-    OAuthLogin mOAuthLogin;
-    @Inject
-    ActionCodeSettings mActionCodeSettings;
     @Qualifiers.GoogleSignInIntent
     @Inject
     Intent mGoogleSignInIntent;
     @Inject
-    UserApiClient mUserApiClient;
-    @Inject
     ToastUtil mToastUtil;
+    @Inject
+    AuthUtil mAuthUtil;
 
     private SignInViewModel mModel;
     private FragmentSignInBinding mBinding;
@@ -77,7 +54,7 @@ public class SignInFragment extends BaseSignInFragment {
         Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(result.getData());
         try {
             GoogleSignInAccount account = task.getResult(ApiException.class);
-            signInWithGoogle(account);
+            mAuthUtil.signInWithGoogle(account);
         } catch (ApiException e) {
             mToastUtil.showGoogleSignInFail();
             Log.d(TAG, "SignInFragment: mGoogleSignInLauncher: ApiException" + e.getMessage());
@@ -121,202 +98,20 @@ public class SignInFragment extends BaseSignInFragment {
     }
 
     public void emailSignIn() {
-        showIndicator();
-        mAuth.signInWithEmailAndPassword(mModel.getEmail(), mModel.getPassword())
-                .addOnCompleteListener(requireActivity(), task -> {
-                    if (task.isSuccessful()) emailSignInSuccess();
-                    else emailSignInFail(task.getException());
-                    hideIndicator();
-                });
-    }
-
-    private void emailSignInSuccess() {
-        if (mAuth.getCurrentUser() != null && !mAuth.getCurrentUser().isEmailVerified())
-            CommonUtil.navigate(mNavController, R.id.signInFragment, SignInFragmentDirections.toReVerificationFragment());
-        else mNavController.popBackStack();
-    }
-
-    private void emailSignInFail(Exception e) {
-        Log.d(TAG, "SignInFragment: emailSignInFail: " + e);
-        if (e instanceof FirebaseAuthInvalidCredentialsException) {
-            mBinding.et.emailLayout.setError(getString(R.string.sign_in_check_email_or_password));
-            mBinding.et.passwordLayout.setError(getString(R.string.sign_in_check_email_or_password));
-        } else if (e instanceof FirebaseAuthInvalidUserException)
-            mBinding.et.emailLayout.setError(getString(R.string.sign_in_not_exist_email));
-        else mToastUtil.showUnknownError();
+        mAuthUtil.emailSignIn(mModel.getEmail(), mModel.getPassword(), this);
     }
 
     public void googleSignIn() {
-        showIndicator();
         mGoogleSignInLauncher.launch(mGoogleSignInIntent);
-    }
-
-    private void signInWithGoogle(GoogleSignInAccount account) {
-        if (account != null) {
-            AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
-            mAuth.signInWithCredential(credential)
-                    .addOnCompleteListener(requireActivity(), task -> {
-                        if (task.isSuccessful()) {
-                            mToastUtil.showGoogleSignInSuccess();
-                            mNavController.popBackStack();
-                        } else {
-                            mToastUtil.showGoogleSignInFail();
-                            Log.d(TAG, "SignInFragment : signInWithGoogle : !task.isSuccessful" + task.getException());
-                        }
-                        hideIndicator();
-                    });
-        }
     }
 
     @SuppressLint("HandlerLeak")
     public void naverSignIn() {
-        showIndicator();
-        mOAuthLogin.startOauthLoginActivity(requireActivity(), new OAuthLoginHandler() {
-            @Override
-            public void run(boolean success) {
-                if (success)
-                    new Thread(() -> {
-                        JsonElement userInfo = getNaverUserInfo();
-                        String email = "naver_" + userInfo.getAsJsonObject().get("email").getAsString();
-                        //TODO 암호 만들어야함
-                        String id = userInfo.getAsJsonObject().get("id").getAsString();
-                        mNickname = userInfo.getAsJsonObject().get("nickname").getAsString();
-                        mProfileUrl = userInfo.getAsJsonObject().get("profile_image").getAsString();
-                        signWithAnotherEmail(email, id);
-                    }).start();
-                else {
-                    mToastUtil.showNaverSignInFail();
-                    String code = mOAuthLogin.getLastErrorCode(requireContext()).getCode();
-                    String desc = mOAuthLogin.getLastErrorDesc(requireContext());
-                    Log.d(TAG, "SignFragment : naverSignIn : errorCode: " + code + "\n errorDesc: " + desc);
-                    hideIndicator();
-                }
-            }
-        });
-    }
-
-    private JsonElement getNaverUserInfo() {
-        String url = getString(R.string.naver_profile_api_url);
-        String userInfo = mOAuthLogin.requestApi(requireContext(), mOAuthLogin.getAccessToken(requireContext()), url);
-        return JsonParser.parseString(userInfo).getAsJsonObject().get("response");
-    }
-
-    private void signWithAnotherEmail(String email, String id) {
-        mAuth.signInWithEmailAndPassword(email, id).addOnCompleteListener(requireActivity(), task -> {
-            if (task.isSuccessful()) {
-                showAnotherEmailSignInSuccessToast(email);
-                mNavController.popBackStack();
-                hideIndicatorInThread();
-            } else if (task.getException() instanceof FirebaseAuthInvalidUserException) {
-                createUserAnotherEmail(email, id);
-            } else {
-                mToastUtil.showUnknownError();
-                Log.d(TAG, "SignInFragment : signWithAnotherEmail: " + task.getException());
-            }
-        });
-    }
-
-    private void hideIndicatorInThread() {
-        mActivityBinding.indicator.post(() -> mActivityBinding.indicator.hide());
-    }
-
-    private void showAnotherEmailSignInSuccessToast(String email) {
-        if (isNaver(email))
-            mToastUtil.showNaverSignInSuccess();
-        else if (isKaKao(email))
-            mToastUtil.showKakaoSignInSuccess();
-    }
-
-    private void showAnotherEmailSignInFailToast(String email) {
-        if (isNaver(email))
-            mToastUtil.showNaverSignInFail();
-        else if (isKaKao(email))
-            mToastUtil.showKakaoSignInFail();
-    }
-
-    private boolean isKaKao(String email) {
-        return email.startsWith("kakao");
-    }
-
-    private boolean isNaver(String email) {
-        return email.startsWith("naver");
-    }
-
-    private void createUserAnotherEmail(String email, String id) {
-        mAuth.createUserWithEmailAndPassword(email, id)
-                .addOnCompleteListener(requireActivity(), task -> {
-                    if (task.isSuccessful()) {
-                        updateUserProfile();
-                        showAnotherEmailSignInSuccessToast(email);
-                    } else {
-                        showAnotherEmailSignInFailToast(email);
-                        Log.d(TAG, "SignInFragment : createUserAnotherEmail : " + task.getException());
-                    }
-                    hideIndicatorInThread();
-                });
-    }
-
-    private void updateUserProfile() {
-        if (mAuth.getCurrentUser() != null)
-            mAuth.getCurrentUser().updateProfile(getUserProfileChangeRequest())
-                    .addOnCompleteListener(requireActivity(), task -> {
-                        if (!task.isSuccessful()) mToastUtil.showProfileUpdateFail();
-                    });
-    }
-
-    private UserProfileChangeRequest getUserProfileChangeRequest() {
-        return new UserProfileChangeRequest.Builder()
-                .setDisplayName(mNickname)
-                .setPhotoUri(Uri.parse(mProfileUrl))
-                .build();
+        mAuthUtil.naverSignIn();
     }
 
     public void kakaoSignIn() {
-        showIndicator();
-        if (mUserApiClient.isKakaoTalkLoginAvailable(requireContext()))
-            mUserApiClient.loginWithKakaoTalk(requireContext(), getKakaoCallBack());
-        else mUserApiClient.loginWithKakaoAccount(requireContext(), getKakaoCallBack());
-    }
-
-    @NotNull
-    private Function2<OAuthToken, Throwable, Unit> getKakaoCallBack() {
-        return (oAuthToken, throwable) -> {
-            if (oAuthToken != null)
-                callKakaoUserInfo();
-            else if (throwable != null) {
-                mToastUtil.showKakaoSignInFail();
-                Log.d(TAG, "SignInFragment : kakaoSignIn: " + throwable);
-            }
-            return null;
-        };
-    }
-
-    private void callKakaoUserInfo() {
-        mUserApiClient.me((user, throwable) -> {
-            if (user != null)
-                signWithKaKaoEmail(user);
-            else {
-                mToastUtil.showKakaoSignInFailCausePermission();
-                Log.d(TAG, "SignInFragment : createUserWithKaKaoEmail: " + throwable);
-                hideIndicator();
-            }
-            return null;
-        });
-    }
-
-    private void signWithKaKaoEmail(User user) {
-        Account kakaoAccount = user.getKakaoAccount();
-        if (kakaoAccount != null && kakaoAccount.getProfile() != null) {
-            String email = "kakao_" + kakaoAccount.getEmail();
-            //TODO 암호 만들어야함
-            String id = String.valueOf(user.getId());
-            mNickname = kakaoAccount.getProfile().getNickname();
-            mProfileUrl = kakaoAccount.getProfile().getProfileImageUrl();
-            signWithAnotherEmail(email, id);
-        } else {
-            mToastUtil.showKakaoSignInFailCausePermission();
-            hideIndicator();
-        }
+        mAuthUtil.kakaoSignIn();
     }
 
     public void navigateSignUpFragment() {
@@ -328,17 +123,22 @@ public class SignInFragment extends BaseSignInFragment {
     }
 
     public void signInAnonymously() {
-        showIndicator();
-        mAuth.signInAnonymously()
-                .addOnCompleteListener(requireActivity(), task -> {
-                    if (task.isSuccessful()) {
-                        mToastUtil.showAnonymouslySignInSuccess();
-                        mNavController.popBackStack();
-                    } else {
-                        mToastUtil.showAnonymouslySignInFail();
-                        Log.d(TAG, "SignInFragment : signInAnonymously: " + task.getException());
-                    }
-                    hideIndicator();
-                });
+        mAuthUtil.signInAnonymously();
+    }
+
+    @Override
+    public void emailSignInNotVerification() {
+        CommonUtil.navigate(mNavController, R.id.signInFragment, SignInFragmentDirections.toReVerificationFragment());
+    }
+
+    @Override
+    public void emailSignInFail(Exception e) {
+        if (e instanceof FirebaseAuthInvalidCredentialsException) {
+            mBinding.et.emailLayout.setError(getString(R.string.sign_in_check_email_or_password));
+            mBinding.et.passwordLayout.setError(getString(R.string.sign_in_check_email_or_password));
+        } else if (e instanceof FirebaseAuthInvalidUserException)
+            mBinding.et.emailLayout.setError(getString(R.string.sign_in_not_exist_email));
+        else mToastUtil.showUnknownError();
+        Log.d(TAG, "SignInFragment: emailSignInFail: " + e);
     }
 }
